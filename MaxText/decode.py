e7a1b2a0c31cbed06a60ad712b867254dab25505
Rafi Witten <rwitten@google.com>
2023-02-07 05:01:56 +0000

    Added support for decode.py for running decodings on your NN!

---
+++ MaxText/decode.py
@@ -0,0 +1,177 @@
+# pylint: disable=g-bad-todo, abstract-method
+"""Training loop and Decoding of the model."""
+import functools
+from typing import Sequence
+
+import os
+from absl import app
+from flax.linen import partitioning as nn_partitioning
+import numpy as np
+import optax
+
+from layers import Transformer
+import pyconfig
+from input_pipeline import get_datasets
+from input_pipeline import preprocess_dataset
+import max_utils
+import temperature_sampler
+
+import checkpointing
+
+import jax
+import jax.numpy as jnp
+from jax import random
+from jax.experimental.pjit import pjit
+from jax.experimental.pjit import PartitionSpec as P
+from jax.experimental.maps import Mesh
+
+from jax.experimental.compilation_cache import compilation_cache as cc
+
+cc.initialize_cache(os.path.expanduser("~/jax_cache"))
+
+os.environ["TFDS_DATA_DIR"] = "gs://tensorflow-datasets/datasets"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
+
+def decode_tokens(toks, tokenizer, eos_id):
+  if np.argmax(toks == eos_id) > 0:
+    valid_toks = toks[:np.argmax(toks == eos_id)]
+  else:
+    valid_toks = toks
+    valid_toks[-1] = eos_id
+
+  valid_toks = valid_toks.astype(np.int32)
+  return tokenizer.detokenize(valid_toks).numpy().decode("utf-8"), len(valid_toks)
+
+
+def encode_strings(strs, max_len, tokenizer):
+  tokenized_batch = np.zeros((len(strs), max_len), np.int32)
+  for i, s in enumerate(strs):
+    toks = tokenizer.tokenize(s).numpy()
+    # Remove EOS token in prompt.
+    tokenized_batch[i, :toks.shape[0]-1] = toks[:-1]
+  return tokenized_batch
+
+def predict_step(inputs,
+                 state,
+                 rngkey,
+                 model,
+                 config):
+  """Predict language model on a batch."""
+  # NOTE: wtf are we adding inputs.shape[2:] here?  it's almost always empty??
+  target_shape = (inputs.shape[0], config.max_predict_length) + inputs.shape[2:]
+
+  initial_variables = model.init(
+      jax.random.PRNGKey(0),
+      jnp.ones(target_shape, config.dtype),
+      None,
+      enable_dropout=False,
+      decode=True,
+      max_decode_length=config.max_predict_length
+  )
+  cache = initial_variables["cache"]
+
+  def tokens_ids_to_logits(flat_ids, flat_cache):
+    """Token slice to logits from decoder model."""
+    # --> [batch * beam, 1, vocab]
+    flat_logits, new_vars = model.apply(
+        {
+            "params": state.params,
+            "cache": flat_cache
+        },
+        flat_ids,
+        None,
+        enable_dropout=False,
+        decode=True,
+        max_decode_length=config.max_predict_length,
+        mutable=["cache"])
+    new_flat_cache = new_vars["cache"]
+    # Remove singleton sequence-length dimension:
+    # [batch, 1, vocab] --> [batch, vocab]
+    flat_logits = flat_logits.squeeze(axis=1)
+    return flat_logits, new_flat_cache
+
+  # Using the above-defined single-step decoder function, run a
+  # search over possible sequences given input encoding.
+  seqs = temperature_sampler.temperature_sample(
+      inputs,
+      cache,
+      tokens_ids_to_logits,
+      rngkey,
+      temperature=config.sampling_temperature,
+      topk=config.sampling_top_k,
+      eos_token=config.eos_id)
+
+  return seqs
+
+def create_learning_rate_schedule(learning_rate: float, warmup_steps: int):
+  return optax.linear_schedule(
+          init_value=0,
+          end_value=learning_rate,
+          transition_steps=warmup_steps
+          )
+
+
+def decode_loop(config, state=None):
+  """Decoding loop for the Transformer model."""
+  checkpoint_manager = checkpointing.create_orbax_checkpoint_manager(config.checkpoint_dir, config.enable_checkpointing)
+  rng = random.PRNGKey(0)
+
+  # Model and Optimizer definition
+  model = Transformer(config)
+
+  tx = optax.adam(
+    max_utils.create_learning_rate_schedule(
+      learning_rate=config.learning_rate, warmup_steps=config.warmup_steps
+    )
+  ) # TODO: we need an optax.GradientTransformation to form a TrainState, but we don't use it when decoding
+
+  # Mesh definition
+  devices_array = max_utils.create_device_mesh(config)
+  mesh = Mesh(devices_array, config.mesh_axes)
+
+  # Set up datasets.
+  train_ds, eval_ds = get_datasets(
+      config=config,
+  )
+
+  _, _, _, sp_tokenizer = preprocess_dataset(
+    config,
+    mesh,
+    train_ds, eval_ds,
+    vocab_path=config.vocab_path,
+  )
+
+  state, state_mesh_annotations = max_utils.setup_initial_state(model, tx, config, rng, mesh, checkpoint_manager)
+
+  p_predict_step = pjit(
+      functools.partial(predict_step, model=model, config=config),
+      in_axis_resources=(P(None, None),
+                        state_mesh_annotations,
+                        None),
+      out_axis_resources=None
+  )
+
+  # Encode the demo prompt.
+  tokenized_prompts = encode_strings(
+      [config.prompt], config.max_predict_length, sp_tokenizer)
+
+  max_utils.activate_profiler(config)
+  for step in np.arange(100):
+    rng, rng_to_use = jax.random.split(rng)
+    with mesh, nn_partitioning.axis_rules(config.logical_axis_rules):
+      seqs = p_predict_step(tokenized_prompts, state, rng_to_use)
+      decoded_string, num_tokens_decoded = decode_tokens(np.array(seqs)[0], sp_tokenizer, config.eos_id)
+      print(f"Decoding #{step} (num tokens {num_tokens_decoded}):\n\t{decoded_string}")
+  max_utils.deactivate_profiler(config)
+
+
+
+def main(argv: Sequence[str]) -> None:
+  pyconfig.initialize(argv)
+  os.environ["JAX_USE_PJRT_C_API_ON_TPU"] = pyconfig.config.use_pjrt
+
+  decode_loop(pyconfig.config)
+
+
+if __name__ == "__main__":
+  app.run(main)
--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -1,6 +1,21 @@
 """ Common Max Utils needed by multiple modules"""
+import checkpointing
+import functools
 
+import numpy as np
 import jax
+import jax.numpy as jnp
+from jax.experimental import mesh_utils
+
+from jax.experimental.pjit import pjit
+
+import flax
+from flax.training import train_state
+from flax import linen as nn
+from flax.linen import partitioning as nn_partitioning
+
+import optax
+
 
 def l2norm_pytree(x):
   """L2 norm of a pytree of arrays."""
@@ -8,3 +23,137 @@ def l2norm_pytree(x):
       lambda x, y: x + jax.numpy.sum(y ** 2), x, initializer=0.0
   ) ** 0.5
 
+def activate_profiler(config):
+  if config.enable_profiler:
+    jax.profiler.start_trace(config.tensorboard_dir)
+
+def deactivate_profiler(config):
+  if config.enable_profiler:
+    jax.profiler.stop_trace()
+
+
+def create_device_mesh(config):
+  """Creates a device mesh with each slice in its own data parallel group. If there is only one slice, uses two replicas """
+  devices = jax.devices()
+  num_devices = len(devices)
+  print(f"Devices: {devices} (num_devices: {num_devices})", flush = True)
+  assert len(devices) > 1, "You must have at least two devices"
+
+  multi_slice_env = hasattr(jax.devices()[0], 'slice_index')
+
+  dcn_parallelism = [config.dcn_data_parallelism, config.dcn_fsdp_parallelism, config.dcn_tensor_parallelism]
+  ici_parallelism = [config.ici_data_parallelism, config.ici_fsdp_parallelism, config.ici_tensor_parallelism]
+
+  assert np.product(dcn_parallelism) * np.product(ici_parallelism) == num_devices, f"Number of devices {num_devices} \
+        does not match the product of the parallelism {np.product(dcn_parallelism) * np.product(ici_parallelism)}"
+
+  if multi_slice_env:
+    mesh = mesh_utils.create_hybrid_device_mesh(ici_parallelism, dcn_parallelism)
+  else:
+    mesh = mesh_utils.create_device_mesh(ici_parallelism)
+
+  print(f"Decided on mesh: {mesh}")
+
+  return mesh
+
+def unbox_logicallypartioned_trainstate(
+    boxed_train_state: train_state.TrainState):
+  """ Unboxes the flax.LogicallyPartitioned pieces in a train state.
+
+    Args:
+      boxed_train_state: a train state that includes LogicallyPartitioned
+        leaves.
+    Returns:
+      a TrainState where all all LogicallyPartitioned leaves have been unboxed.
+  """
+  return jax.tree_util.tree_map(lambda x: x.unbox() if \
+        isinstance(x, flax.linen.spmd.LogicallyPartitioned) \
+        else x, boxed_train_state, \
+        is_leaf=lambda k: isinstance(k, flax.linen.spmd.LogicallyPartitioned))
+
+def init_train_state(model, tx, config, key):
+  """
+  We pass in "static" objects like model, tx, config as JAX compares them by
+  object hash, and instantiating them inside causes pjit top-level annotations
+  to fail to match as pytree prefixes if we re-instantiate.
+
+  Args: model, tx, config, key
+  """
+  input_shape = (
+      len(jax.devices()) * config.per_device_batch_size,
+      config.max_target_length
+  )
+  model_vars = model.init({'params': key, 'dropout': key},
+                          jnp.ones(input_shape),
+                          jnp.ones(input_shape))
+  state = train_state.TrainState.create(
+      apply_fn=model.apply,
+      params=model_vars['params'],
+      tx=tx)
+  return state
+
+
+def setup_initial_state(model, tx, config, rng, mesh, checkpoint_manager):
+  """ We initialize the model and optimizer state, and optionally load from a
+  checkpoint as necessary.
+
+  Args:
+    model: the flax model to initialize
+    tx: the optax.GradientTransformation
+    config: config object
+    rng: jax.prng key
+    mesh: jax.devices() mesh
+    checkpoint_manager: an Orbax checkpointing.CheckpointManager object
+
+  Returns:
+    state: the initialized train state
+    state_mesh_annotations: the mesh annotations for the train state
+  """
+  init_train_state_partial = functools.partial(init_train_state, model, tx,
+                                               config)
+  abstract_state = jax.eval_shape(init_train_state_partial, rng)
+  state_logical_annotations = nn.get_partition_spec(abstract_state)
+  unboxed_abstract_state = unbox_logicallypartioned_trainstate(abstract_state)
+
+  # Initialization
+  with mesh, nn_partitioning.axis_rules(config.logical_axis_rules):
+    state_mesh_annotations = nn.logical_to_mesh(state_logical_annotations)
+    state, raw_params = checkpointing.load_state_if_possible(checkpoint_manager,
+                                                config.load_parameters_path,
+                                                unboxed_abstract_state,
+                                                mesh,
+                                                state_mesh_annotations)
+
+    if not state:
+      state = pjit(
+          init_train_state_partial,
+          in_axis_resources=None,
+          out_axis_resources=state_mesh_annotations
+      )(rng)
+      if raw_params: # If we loaded a partial state, we need to merge it.
+        state = state.replace(params = raw_params)
+    raw_params = None
+
+  state = unbox_logicallypartioned_trainstate(state)
+  return state, state_mesh_annotations
+
+
+# Learning Rate Schedule
+# -----------------------------------------------------------------------------
+# learning rate scheduling
+def rsqrt_schedule(init_value: float, shift: int = 0):
+  def schedule(count):
+    return init_value * (1 + count + shift)**-.5 * shift**.5
+  return schedule
+
+def create_learning_rate_schedule(learning_rate: float, warmup_steps: int):
+  """Creates a rsqrt schedule with linear warmup."""
+  return optax.join_schedules([
+      optax.linear_schedule(
+          init_value=0,
+          end_value=learning_rate,
+          transition_steps=warmup_steps
+          ),
+      rsqrt_schedule(init_value=learning_rate, shift=warmup_steps),
+      ], boundaries=[warmup_steps],
+      )
