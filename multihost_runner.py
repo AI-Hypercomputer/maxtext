a54f6eaa7bf21c7a15511b8fb35ec0945864dd13
Matthew Davidow <51136315+gobbleturk@users.noreply.github.com>
2023-02-24 07:55:19 -0800

    fix-kill (#56)

--- multihost_runner.py
+++ multihost_runner.py
@@ -107,13 +107,17 @@ device_name="accel0"
 if [[ "${_TPU_VERSION_NAME}" =~ ^v5.* ]]; then
   device_name="vfio/0"
 fi
-echo -e "Device Name: ${device_name}"
+echo -e "Searching for existing processes on device ${device_name}..."
 pid=$(sudo lsof -w /dev/${device_name} | awk 'END{print $2}')
-echo -e "PID: ${pid}"
 if [[ ! -z "${pid}" ]]
 then
- echo -e "Killing process with pid: ${pid}"
+ echo -e "Existing process found with pid ${pid}"
+ echo -e "Killing process ${pid}..."
  kill -9 "${pid}"
+ tail --pid=$pid -f /dev/null
+ echo -e "Orphaned process ${pid} on your TPU was killed successfully."
+else
+ echo -e "No existing processes found."
 fi
 sudo rm -f /tmp/libtpu_lockfile"""
 
