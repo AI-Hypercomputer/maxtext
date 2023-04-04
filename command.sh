17f28b49face7fb819f1329cc4207e65ff4673ed
Rafi Witten <rwitten@google.com>
2023-04-04 18:56:01 +0000

    Fixed libtpu version

--- command.sh
+++ command.sh
@@ -5,8 +5,9 @@ export TF_CPP_MIN_LOG_LEVEL=0
 export TPU_STDERR_LOG_LEVEL=0
 export TPU_MIN_LOG_LEVEL=0
 export TPU_PREMAPPED_BUFFER_SIZE=4294967296 
-#export XLA_FLAGS="--xla_dump_to=/tmp/hlo_${RUN}_${EMBED}"
-#export LIBTPU_INIT_ARGS="--xla_jf_dump_to=/tmp/llo_${RUN}_${EMBED}"
+export XLA_FLAGS="--xla_dump_to=/tmp/hlo_${RUN}_${EMBED}"
+export LIBTPU_INIT_ARGS="--xla_jf_dump_to=/tmp/llo_${RUN}_${EMBED}"
 export TPU_VMODULE=3
+export TPU_LIBRARY_PATH=/lib/libtpu.so
 python3 pedagogical_examples/shardings.py --ici_fsdp_parallelism=256 --batch_size=64  --embedding_dimension=$EMBED
 #python3 pedagogical_examples/shardings.py --ici_fsdp_parallelism=16 --ici_tensor_parallelism=16 --batch_size=64  --embedding_dimension=$EMBED
