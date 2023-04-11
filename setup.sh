da5aa3b9354855895299bbae88fcfd4540c77980
Rafi Witten <rwitten@google.com>
2023-04-11 18:46:38 +0000

    Moved Flax to a normal install

--- setup.sh
+++ setup.sh
@@ -15,8 +15,3 @@
 # limitations under the License.
 
 pip3 install -r requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-
-# install flax
-cd
-git clone --branch=main https://github.com/google/flax.git
-pip3 install -e flax
