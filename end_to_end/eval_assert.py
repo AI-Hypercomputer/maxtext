511bf1c2856f2cc44d2c24ac092c13125a167a3e
Priyanka Ganesha <prii@google.com>
2023-04-20 17:15:38 +0000

    checkpointing tests and gcs bucket fix

--- end_to_end/eval_assert.py
+++ end_to_end/eval_assert.py
@@ -1,6 +1,7 @@
 """Reads and asserts over target values"""
 from absl import app
 from typing import Sequence
+from math import isclose
 import json
 
 
@@ -20,13 +21,32 @@ def read(metrics_file, target):
   return avg
 
 
+def test_tflops(metrics_file, target, threshold):
+  """Asserts over tflops values"""
+  avg_tflops = read(metrics_file, target)
+  assert avg_tflops >= threshold
+
+
+def test_checkpointing(metrics_file, target):
+  """Asserts over loss values from loaded checkpoint"""
+  metrics_file_saved = 'saved_' + metrics_file
+  metrics_file_restored = 'restored_' + metrics_file
+
+  with open(metrics_file_saved, 'r', encoding='utf8') as saved,\
+    open(metrics_file_restored, 'r', encoding='utf8') as restored:
+    saved_loss = json.loads(saved.readlines()[-1])[target]
+    restored_loss = json.loads(restored.readlines()[0])[target]
+    assert isclose(saved_loss, restored_loss, rel_tol=0.5)
+
+
 def main(argv: Sequence[str]) -> None:
 
   _, metrics_file, threshold, target = argv
 
-  avg_val = read(metrics_file, target)
-
-  assert avg_val >= float(threshold)
+  if target == 'perf/per_device_tflops_per_sec':
+    test_tflops(metrics_file, target, float(threshold))
+  elif target == 'learning/loss':
+    test_checkpointing(metrics_file, target)
 
 
 if __name__ == "__main__":
