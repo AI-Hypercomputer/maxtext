deda831b6b50b4cf859cc78d2484ff2cdb8ae729
Mohit Khatwani <mohitkhatwani@google.com>
2023-04-20 17:46:52 -0700

    fun1

--- MaxText/tests/input_pipeline_test.py
+++ MaxText/tests/input_pipeline_test.py
@@ -14,68 +14,68 @@
 #  limitations under the License.
 #  """
 
-# # pylint: disable=missing-module-docstring, missing-function-docstring
-# import os
-# import sys
-# import jax
-# from jax.experimental.maps import Mesh
-# from jax.experimental import mesh_utils
+# pylint: disable=missing-module-docstring, missing-function-docstring
+import os
+import sys
+import jax
+from jax.experimental.maps import Mesh
+from jax.experimental import mesh_utils
 
-# import unittest
-# import tensorflow_datasets as tfds
+import unittest
+import tensorflow_datasets as tfds
 
-# import pyconfig
-# import input_pipeline
+import pyconfig
+import input_pipeline
 
-# # By default, XLA presents all the CPU cores as one device. This flag splits up cores in 2 CPU devices.
-# os.environ["TFDS_DATA_DIR"] = "gs://maxtext-dataset/"
-# os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=2'
-# jax.config.update('jax_platform_name', 'cpu')
+# By default, XLA presents all the CPU cores as one device. This flag splits up cores in 2 CPU devices.
+os.environ["TFDS_DATA_DIR"] = "gs://maxtext-dataset/"
+os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=2'
+jax.config.update('jax_platform_name', 'cpu')
 
 
-# class InputPipelineTest(unittest.TestCase):
+class InputPipelineTest(unittest.TestCase):
 
-#   def setUp(self):
-#     super().setUp()
-#     pyconfig.initialize(sys.argv + ['configs/base.yml'], per_device_batch_size=1, run_name='test', mesh_axes = ['data'],
-#                         logical_axis_rules = [['batch', 'data']],
-#                         data_sharding = ['data'])
-#     self.config = pyconfig.config
-#     self.read_config = tfds.ReadConfig()
-#     self.read_config.add_tfds_id = True
-#     self.train_ds, self.eval_ds = self._get_datasets()
-#     self.train_iter, self.eval_iter, self.predict_iter = self._get_preprocessed_datasets()
+  def setUp(self):
+    super().setUp()
+    pyconfig.initialize(sys.argv + ['configs/base.yml'], per_device_batch_size=1, run_name='test', mesh_axes = ['data'],
+                        logical_axis_rules = [['batch', 'data']],
+                        data_sharding = ['data'])
+    self.config = pyconfig.config
+    self.read_config = tfds.ReadConfig()
+    self.read_config.add_tfds_id = True
+    self.train_ds, self.eval_ds = self._get_datasets()
+    self.train_iter, self.eval_iter, self.predict_iter = self._get_preprocessed_datasets()
 
-#   def _get_datasets(self):
-#     print("Sharding dataset in ", jax.process_count(), " shards")
-#     train_ds, eval_ds = input_pipeline.get_datasets(
-#             config=self.config, read_config = self.read_config)
-#     return train_ds, eval_ds
+  def _get_datasets(self):
+    print("Sharding dataset in ", jax.process_count(), " shards")
+    train_ds, eval_ds = input_pipeline.get_datasets(
+            config=self.config, read_config = self.read_config)
+    return train_ds, eval_ds
 
-#   def _get_preprocessed_datasets(self):
-#     mesh_shape_1d = (len(jax.devices()),)
-#     mesh = Mesh(mesh_utils.create_device_mesh(mesh_shape_1d), self.config.mesh_axes)
+  def _get_preprocessed_datasets(self):
+    mesh_shape_1d = (len(jax.devices()),)
+    mesh = Mesh(mesh_utils.create_device_mesh(mesh_shape_1d), self.config.mesh_axes)
 
-#     train_iter, eval_iter, test_iter, _ = input_pipeline.preprocess_dataset(
-#               self.config,
-#               mesh,
-#               self.train_ds, self.eval_ds,
-#               vocab_path=os.path.join(self.config.base_output_directory, self.config.vocab_relative_path))
-#     return train_iter, eval_iter, test_iter
+    train_iter, eval_iter, test_iter, _ = input_pipeline.preprocess_dataset(
+              self.config,
+              mesh,
+              self.train_ds, self.eval_ds,
+              vocab_path=os.path.join(self.config.base_output_directory, self.config.vocab_relative_path))
+    return train_iter, eval_iter, test_iter
 
-#   def test_train_ds(self):
-#     expected_shape = [2, self.config.max_target_length]
-#     # For training we pack multiple short examples in one example.
-#     # *_position and *_segmentation indicate the boundaries.
-#     batch = self.train_iter()
-#     self.assertEqual({k: list(v.shape) for k, v in batch.items()}, {
-#         'inputs': expected_shape,
-#         'inputs_position': expected_shape,
-#         'inputs_segmentation': expected_shape,
-#         'targets': expected_shape,
-#         'targets_position': expected_shape,
-#         'targets_segmentation': expected_shape,
-#     })
+  def test_train_ds(self):
+    expected_shape = [2, self.config.max_target_length]
+    # For training we pack multiple short examples in one example.
+    # *_position and *_segmentation indicate the boundaries.
+    batch = self.train_iter()
+    self.assertEqual({k: list(v.shape) for k, v in batch.items()}, {
+        'inputs': expected_shape,
+        'inputs_position': expected_shape,
+        'inputs_segmentation': expected_shape,
+        'targets': expected_shape,
+        'targets_position': expected_shape,
+        'targets_segmentation': expected_shape,
+    })
 
 
 #   def test_eval_ds(self):
