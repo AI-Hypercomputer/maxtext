e35ee7e78867c996469f2d51e736c0d155cf0cef
gobbleturk <mattdavidow@google.com>
2023-04-21 21:57:53 +0000

    Add retry to data loading

--- MaxText/multihost_dataloading.py
+++ MaxText/multihost_dataloading.py
@@ -26,6 +26,7 @@ from functools import partial  # pylint: disable=g-importing-member
 import os
 from typing import Callable, Any, Dict, List, Tuple, Optional
 import tensorflow as tf  # pylint: disable=g-import-not-at-top
+import time
 import numpy as np
 
 import jax
@@ -107,7 +108,15 @@ def get_next_batch_sharded(local_dataset: tf.data.Dataset,
                            global_mesh: Mesh) -> jax.Array:
   """Splits the host loaded data equally over all devices."""
 
-  local_data = local_dataset.next()
+  loaded_next, load_attempts, max_load_attempts = False, 0, 10
+  while not loaded_next and load_attempts < max_load_attempts:
+    load_attempts = load_attempts + 1
+    try:
+      local_data = local_dataset.next()
+      loaded_next = True
+    except:
+      max_logging.log("Failed to get next data batch, retrying")
+      time.sleep(10)
 
   # local_devices = jax.local_devices()
   local_devices = global_mesh.local_devices
