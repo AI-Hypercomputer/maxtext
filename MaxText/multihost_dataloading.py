fd444261306135d2a8778713232393cdddf939c6
gobbleturk <mattdavidow@google.com>
2023-04-21 23:13:30 +0000

    Add retry to data loading

--- MaxText/multihost_dataloading.py
+++ MaxText/multihost_dataloading.py
@@ -110,9 +110,8 @@ def get_next_batch_sharded(local_dataset: tf.data.Dataset,
 
   # pylint: disable=bare-except
   try:
-    #local_data = local_dataset.next()
-    raise tf.errors.FailedPreconditionError("go me")
-  except tf.errors.FailedPreconditionError:
+    local_data = local_dataset.next()
+  except:
     max_logging.log("Failed to get next data batch, retrying")
     time.sleep(10)
     local_data = local_dataset.next()
