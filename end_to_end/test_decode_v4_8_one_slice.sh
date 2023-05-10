4bd696d05b352581af2a352cfea07d1579213fe3
gobbleturk <mattdavidow@google.com>
2023-05-10 17:43:36 +0000

    Decode.py XLML test

--- end_to_end/test_decode_v4_8_one_slice.sh
+++ end_to_end/test_decode_v4_8_one_slice.sh
@@ -3,20 +3,21 @@ set -e
 
 NUM_TOKEN_THRESHOLD=${1}
 OUTPUT_PATH=${2}
+DATASET_PATH=${3}
 # Run name is optional 3rd input - our daily XLML tests will use one.
 
 
-if [ -z ${3} ]
+if [ -z ${4} ]
 then 
     RUN_NAME=${USER}_$(date +%Y-%m-%d-%H-%M-%S)
 else
-    RUN_NAME=${3}_$(date +%Y-%m-%d-%H)
+    RUN_NAME=${4}_$(date +%Y-%m-%d-%H)
 fi
 
 #Setup and Train
 bash setup.sh
 python3 MaxText/decode.py MaxText/configs/base.yml run_name=$RUN_NAME\
-    steps=20 enable_checkpointing=False metrics_file='metrics.txt'\
-    base_output_directory=$OUTPUT_PATH
+    steps=50 enable_checkpointing=False metrics_file='metrics.txt'\
+    base_output_directory=$OUTPUT_PATH dataset_path=$DATASET_PATH
 
 python3 end_to_end/eval_assert.py metrics.txt $NUM_TOKEN_THRESHOLD num_tokens metrics_average
