5ed22fee6c321edae337ca144d4b6322f807bba2
Raymone Zou <raymondzou@google.com>
2023-06-14 23:23:16 +0000

    Remove an XLA flag from 16B and 32B test

--- end_to_end/test_tflops_32b_params.sh
+++ end_to_end/test_tflops_32b_params.sh
@@ -19,7 +19,7 @@ export LIBTPU_INIT_ARGS="--xla_tpu_enable_async_collective_fusion=true --xla_tpu
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
     steps=150 per_device_batch_size=1 enable_checkpointing=false\
     enable_profiler=true remat_policy=proj base_emb_dim=8192 base_mlp_dim=32768\
-    base_num_heads=32 base_num_decoder_layers=40 head_dim=256 \
+    base_num_heads=32 base_num_decoder_layers=40 head_dim=256\
     max_target_length=2048 metrics_file='metrics.txt' base_output_directory=$OUTPUT_PATH\
     dataset_path=$DATASET_PATH log_period=150
 
