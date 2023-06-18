a96ecdc3f4fb9de0a06de64dd1b1a94f762446bb
gobbleturk <mattdavidow@google.com>
2023-06-18 18:18:15 +0000

    yay

---
+++ MaxText/export_mxla_coordinator.sh
@@ -0,0 +1,4 @@
+curl -s 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/tpu-env' -H 'Metadata-Flavor: Google' > /tmp/tpu-env # store the metadata
+X_MEGASCALE_COORDINATOR_ADDRESS=$(grep '^MEGASCALE_COORDINATOR_ADDRESS' /tmp/tpu-env | cut -d "'" -f 2)
+echo $X_MEGASCALE_COORDINATOR_ADDRESS
+export X_MEGASCALE_COORDINATOR_ADDRESS=$X_MEGASCALE_COORDINATOR_ADDRESS
