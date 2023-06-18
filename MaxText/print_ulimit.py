9fae7b65704fbbf441fc31d10cbbdfaf8d81b47b
gobbleturk <mattdavidow@google.com>
2023-06-18 14:50:17 +0000

    gogo

--- MaxText/print_ulimit.py
+++ MaxText/print_ulimit.py
@@ -6,3 +6,10 @@ output = subprocess.run("ulimit -n", stdout=subprocess.PIPE, stderr=subprocess.P
 # Decode the output and print the result
 ulimit_value = output.stdout.decode().strip()
 print("Current ulimit -n value:", ulimit_value)
+
+# Execute the 'ulimit -n' command and capture the output
+output_hn = subprocess.run("ulimit -Hn", stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+
+# Decode the output and print the result
+ulimit_value_hn = output_hn.stdout.decode().strip()
+print("Current ulimit -Hn value:", ulimit_value_hn)
