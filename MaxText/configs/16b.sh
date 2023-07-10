0c6af8912928d10689106cf2cd132b2b5e1bdb8c
Matthew Davidow <51136315+gobbleturk@users.noreply.github.com>
2023-07-10 13:12:23 -0700

    Update 16b 32b tests to use full remat and larger batch size (#84)
    Update 16b and 32b configs to use full remat and larger batch size
    
    Also:
    * Add a config that runs these tests with additional optimizied rto
    * Remove datestring from the RUN_NAME so checkpointing works.

---
+++ MaxText/configs/16b.sh
@@ -0,0 +1,10 @@
+echo "Running 16b.sh"
+
+RUN_NAME=${1}
+OUTPUT_PATH=${2}
+DATASET_PATH=${3}
+
+bash rto_setup.sh
+
+TFLOP_THRESHOLD=0 # set to 0 since we are not actually running as a test.
+bash end_to_end/test_tflops_16b_params.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
