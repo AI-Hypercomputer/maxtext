2a393c853c63cf6e6a38d12a0f2bb128b3363f9e
gobbleturk <mattdavidow@google.com>
2023-07-10 18:53:07 +0000

    Add both 16b and 32b full remat configs

--- MaxText/configs/16b.sh
+++ MaxText/configs/16b.sh
@@ -8,4 +8,4 @@ bash rto_setup.sh
 export TPU_LIBRARY_PATH='/lib/libtpu.so'
 
 TFLOP_THRESHOLD=0 # set to 0 since we are not actually running as a test.
-bash end_to_end/test_tflops_16b_parmams.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
+bash end_to_end/test_tflops_16b_params.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
