fb4a422f872879ca442536e30f8b6d714197bd3e
gobbleturk <mattdavidow@google.com>
2023-07-10 18:30:52 +0000

    add 16b.sh

---
+++ MaxText/configs/16b.sh
@@ -0,0 +1,11 @@
+echo "Running 16b.sh"
+
+RUN_NAME=${1}
+OUTPUT_PATH=${2}
+DATASET_PATH=${3}
+
+bash rto_setup.sh
+export TPU_LIBRARY_PATH='/lib/libtpu.so'
+
+TFLOP_THRESHOLD=0 # set to 0 since we are not actually running as a test.
+bash end_to_end/test_tflops_16b_parmams.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
---
+++ MaxText/configs/small.sh
@@ -0,0 +1,11 @@
+echo "Running 16b.sh"
+RUN_NAME=${1}
+
+export TPU_LIBRARY_PATH='/lib/libtpu.so'
+bash rto_setup.sh && export LIBTPU_INIT_ARGS='--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true'
+echo "train.py"
+python3 MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME}\
+    steps=5 per_device_batch_size=1 enable_checkpointing=false\
+    enable_profiler=true remat_policy=full base_emb_dim=1024 base_mlp_dim=1024\
+    base_num_heads=24 base_num_decoder_layers=36 head_dim=256\
+    max_target_length=1024 metrics_file='metrics.txt' log_period=8
