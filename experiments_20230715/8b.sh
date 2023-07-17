2435628c3aad623109ed1277819a26cf10634394
Rafi Witten <rwitten@google.com>
2023-07-17 16:35:18 +0000

    Added experiments

---
+++ experiments_20230715/8b.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+STEPS=40000
+
+USE_INT8_TRAINING=$1
+USE_FWD_QUANT=$2
+
+RUN_NAME=rwitten-20230716_8b_${USE_INT8_TRAINING}_usefwd_${USE_FWD_QUANT}
+OUTPUT_FILE=gs://mattdavidow-maxtext-br/${RUN_NAME}.txt
+
+
+export LIBTPU_INIT_ARGS="--xla_tpu_spmd_rng_bit_generator_unsafe=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
+
+command="python3 MaxText/train.py MaxText/configs/base.yml \
+    steps=${STEPS} per_device_batch_size=4 learning_rate=0.001 warmup_steps=2000 enable_profiler=false \
+    enable_dropout=false enable_data_shuffling=false run_name=${RUN_NAME} scale=2\
+    use_fwd_quant=${USE_FWD_QUANT} use_int8_training=${USE_INT8_TRAINING} metrics_file=metrics.txt"
+
+echo "Starting run (${RUN_NAME}) with command: ${command}"
+eval ${command}
+echo "Finished command"
+echo "Now writing to ${OUTPUT_FILE}"
+if [[ ${SLICE_ID} -eq 0 && ${WORKER_ID} -eq 0 ]]; then
+    gsutil cp metrics.txt ${OUTPUT_FILE}
+fi
+echo "Done writing to ${OUTPUT_FILE}"
+
