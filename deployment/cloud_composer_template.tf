3fd4124bd1853ac94f4fdb25bfedfac26274bf41
Ran Ran <ranran@google.com>
2023-07-18 07:06:24 +0000

    Add more required IAM roles
    Change-Id: Ibbbab78ae4e9609e789b71dd0da9ac8a3624abd8

--- deployment/cloud_composer_template.tf
+++ deployment/cloud_composer_template.tf
@@ -21,11 +21,6 @@ locals {
   }
 }
 
-provider "google-beta" {
-  project = var.project_config.project_name
-  region  = var.project_config.project_region
-}
-
 resource "google_project_service" "composer_api" {
   provider           = google-beta
   project            = var.project_config.project_name
@@ -40,12 +35,52 @@ resource "google_service_account" "custom_service_account" {
   display_name = "ML Automation Solutions Service Account"
 }
 
-resource "google_project_iam_member" "custom_service_account" {
+resource "google_project_iam_member" "composer_worker_role" {
   provider = google-beta
   for_each = local.environment_config_dict
   project  = var.project_config.project_name
   member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
   role     = "roles/composer.worker"
+}
+
+resource "google_project_iam_member" "tpu_admin_role" {
+  provider = google-beta
+  for_each = local.environment_config_dict
+  project  = var.project_config.project_name
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
+  role     = "roles/tpu.admin"
+}
+
+resource "google_project_iam_member" "service_account_user_role" {
+  provider = google-beta
+  for_each = local.environment_config_dict
+  project  = var.project_config.project_name
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
+  role     = "roles/iam.serviceAccountUser"
+}
+
+resource "google_project_iam_member" "big_query_admin_role" {
+  provider = google-beta
+  for_each = local.environment_config_dict
+  project  = var.project_config.project_name
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
+  role     = "roles/bigquery.admin"
+}
+
+resource "google_project_iam_member" "storage_admin_role" {
+  provider = google-beta
+  for_each = local.environment_config_dict
+  project  = var.project_config.project_name
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
+  role     = "roles/storage.admin"
+}
+
+resource "google_project_iam_member" "vertex_ai_admin_role" {
+  provider = google-beta
+  for_each = local.environment_config_dict
+  project  = var.project_config.project_name
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account[each.key].email)
+  role     = "roles/aiplatform.admin"
 }
 
 resource "google_service_account_iam_member" "custom_service_account" {
@@ -54,13 +89,6 @@ resource "google_service_account_iam_member" "custom_service_account" {
   service_account_id = google_service_account.custom_service_account[each.key].name
   role               = "roles/composer.ServiceAgentV2Ext"
   member             = "serviceAccount:service-${var.project_config.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
-}
-
-terraform {
-  backend "gcs" {
-    bucket = "composer-ml-auto-solutions-tfstate"
-    prefix = "terraform/state"
-  }
 }
 
 resource "google_composer_environment" "example_environment" {
