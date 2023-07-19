8e1c1526bb41d19cf7b93dbf4e733fea8b07240f
gobbleturk <mattdavidow@google.com>
2023-07-19 05:00:21 +0000

    int8 7-18-23

--- MaxText/configs/1b-experiments-to-cns.sh
+++ MaxText/configs/1b-experiments-to-cns.sh
@@ -1,3 +1,8 @@
+
+CNS_PATH=/cns/pi-d/home/${USER}/int8-metrics/
+fileutil mkdir -p ${CNS_PATH}
+
+
 REMAT_ARRAY=("minimal" "full")
 USE_INT8_TRAINING_ARRAY=("false" "true")
 DTYPE_ARRAY=("bfloat16" "float32")
@@ -7,13 +12,10 @@ for REMAT_POLICY in ${REMAT_ARRAY[@]}; do
     for USE_INT8_TRAINING in ${USE_INT8_TRAINING_ARRAY[@]}; do
         for DTYPE in ${DTYPE_ARRAY[@]}; do
             for PRNG_KEY in ${PRNG_KEY_ARRAY[@]}; do
-
                 echo "Remat: ${REMAT_POLICY}, int8: ${USE_INT8_TRAINING}, dtype: ${DTYPE}, PRNG_KEY: ${PRNG_KEY}"
                 RUN_NAME=mattdavidow-20230716_remat_${REMAT_POLICY}_useint8_${USE_INT8_TRAINING}_dtype_${DTYPE}_PRNGKey_${PRNG_KEY}
-                echo "Running the above setting with RUN_NAME ${RUN_NAME}"
-                python3 multihost_job.py --BUCKET_NAME="mattdavidow-maxtext-br" --RUN_NAME=${RUN_NAME} --TPU_TYPE=v5litepod-256 --NUM_SLICES=1 --VERSION=v2-alpha-tpuv5-lite --COMMAND="bash setup.sh && bash MaxText/configs/1b-experiments-7-17-23.sh ${REMAT_POLICY} ${USE_INT8_TRAINING} ${DTYPE} ${PRNG_KEY}" --CQR_EXTRA_ARGS="--reserved" --ZONE=us-west4-a
-                
-                
+                OUTPUT_FILE=mattdavidow-maxtext-br/${RUN_NAME}.txt
+                /google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs cp /bigstore/${OUTPUT_FILE} ${CNS_PATH}/           
             done        
         done
     done
