2763c1e06db71a27196029334ceb83d2d69849d1
gobbleturk <mattdavidow@google.com>
2023-07-20 21:17:25 +0000

    Change behavior of scale to match intuition

--- MaxText/configs/2xv4-384.sh
+++ MaxText/configs/2xv4-384.sh
@@ -14,5 +14,5 @@
 
 export LIBTPU_INIT_ARGS="--xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE"
 RUN_NAME=2x_v4-384_0012
-python3 MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME} enable_profiler=true enable_checkpointing=false steps=10 dcn_data_parallelism=2 ici_tensor_parallelism=1 base_num_decoder_layers=32 per_device_batch_size=10 remat_policy=full base_emb_dim=12288 base_mlp_dim=49152 base_num_heads=32 learning_rate=1e-8
+python3 MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME} enable_profiler=true enable_checkpointing=false steps=10 dcn_data_parallelism=2 ici_fsdp_parallelism=192 ici_tensor_parallelism=1 base_num_decoder_layers=32 per_device_batch_size=10 remat_policy=full base_emb_dim=12288 base_mlp_dim=49152 base_num_heads=32 learning_rate=1e-8
 # achieves 162 TFLOP/s, 52B
