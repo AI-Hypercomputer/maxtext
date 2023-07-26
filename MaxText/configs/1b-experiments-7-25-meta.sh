712a6cf7ac33f13d0152474fd931e5a970669d76
gobbleturk <mattdavidow@google.com>
2023-07-26 17:21:26 +0000

    matt adds the new settings

--- MaxText/configs/1b-experiments-7-25-meta.sh
+++ MaxText/configs/1b-experiments-7-25-meta.sh
@@ -2,14 +2,14 @@
 echo "Starting conv_gap_test"
 SAVE_ACCUMULATOR_ARRAY=("false" "true")
 USE_RHS_NOISE_FUNCTION_ARRAY=("false" "true")
-PRNG_KEY_ARRAY=(6 7 8 9 10 11)
+PRNG_KEY_ARRAY=(0)
 
 echo "starting loops"
 for SAVE_ACCUMULATOR in ${SAVE_ACCUMULATOR_ARRAY[@]}; do
     for USE_RHS_NOISE_FUNCTION in ${USE_RHS_NOISE_FUNCTION_ARRAY[@]}; do
         for PRNG_KEY in ${PRNG_KEY_ARRAY[@]}; do
             echo "SAVE_ACCUMULATOR: ${SAVE_ACCUMULATOR}, USE_RHS_NOISE_FUNCTION_ARRAY: ${USE_RHS_NOISE_FUNCTION_ARRAY} PRNG_KEY: ${PRNG_KEY}"
-            RUN_NAME=mattdavidow-20230725_ACCUMULATOR_${SAVE_ACCUMULATOR}_${USE_RHS_NOISE_FUNCTION}_PRNGKey_${PRNG_KEY}
+            RUN_NAME=mattdavidow-20230725-a2_ACCUMULATOR_${SAVE_ACCUMULATOR}_${USE_RHS_NOISE_FUNCTION}_PRNGKey_${PRNG_KEY}
             echo "Running the above setting with RUN_NAME ${RUN_NAME}"
             python3 multihost_job.py --BUCKET_NAME="mattdavidow-maxtext-br" --RUN_NAME=${RUN_NAME} --TPU_TYPE=v5litepod-256 --NUM_SLICES=1 --VERSION=v2-alpha-tpuv5-lite --COMMAND="bash setup.sh && bash MaxText/configs/1b-experiments-7-25.sh ${SAVE_ACCUMULATOR} ${USE_RHS_NOISE_FUNCTION} ${PRNG_KEY} ${RUN_NAME}" --CQR_EXTRA_ARGS="--reserved" --ZONE=us-west4-a      
         done
--- MaxText/configs/1b-experiments-7-25.sh
+++ MaxText/configs/1b-experiments-7-25.sh
@@ -13,12 +13,15 @@ OUTPUT_FILE=gs://mattdavidow-maxtext-br/${RUN_NAME}.txt
 
 export LIBTPU_INIT_ARGS="--xla_tpu_spmd_rng_bit_generator_unsafe=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 
+# On night on 7-25 forgot to add SAVE_ACCUMULATOR and USE_RHS_NOISE_FUNCTION inputs
+
 command="python3 MaxText/train.py MaxText/configs/base.yml \
     steps=${STEPS} per_device_batch_size=4 learning_rate=0.001 warmup_steps=2000 enable_profiler=false enable_checkpointing=false \
     enable_dropout=false enable_data_shuffling=false run_name=${RUN_NAME}\
     use_int8_training=true metrics_file=metrics.txt\
     remat_policy=full init_prng_key=${PRNG_KEY}\
-    gcs_metrics_directory=gs://mattdavidow-maxtext-br/metrics/${RUN_NAME}"
+    gcs_metrics_directory=gs://mattdavidow-maxtext-br/metrics/${RUN_NAME}\
+    save_accumulator=${SAVE_ACCUMULATOR} use_rhs_noise_function=${USE_RHS_NOISE_FUNCTION}"
 
 echo "Starting run (${RUN_NAME}) with command: ${command}"
 eval ${command}
