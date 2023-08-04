d90804340e9e7037fdf84f796b0000f4bbcbd6d5
gobbleturk <mattdavidow@google.com>
2023-08-04 21:57:43 +0000

    this branch has been deemed messy

--- MaxText/configs/sweep3-meta-2b.sh
+++ MaxText/configs/sweep3-meta-2b.sh
@@ -1,5 +1,5 @@
 
-lr_array=(.0008 .0006 .0004 .0003 .0002 .00015 .0001 .000075) 
+lr_array=(.0008 .0006 .0004 .0003 .0002 .0001 .000075) # missing 0.00015
 scale_array=(2)
 num_slice_array=(1 2)
 key_array=(1)
@@ -14,8 +14,8 @@ for lr in ${lr_array[@]}; do
             for key in ${key_array[@]}; do
                 run_name=mattdavidow-sweep3-a4-params_lr_${lr_name}_scale_${scale}_slice_${num_slice}_PRNGKey_${key}
                 echo ${run_name}
-                echo ${run_name} >> mattdavidow-sweep3-1b-even-a1.txt
-                python3 multihost_job.py --BUCKET_NAME="mattdavidow-maxtext-br" --RUN_NAME=${run_name} --TPU_TYPE=v5litepod-256 --NUM_SLICES=${num_slice} --VERSION=v2-alpha-tpuv5-lite --COMMAND="bash setup.sh && bash MaxText/configs/sweep3-individual.sh ${lr} ${scale} ${num_slice} ${key} ${run_name}" --ZONE=us-east5-b      
+                echo ${run_name} >> mattdavidow-run-names-sweep3-2b.txt
+                #python3 multihost_job.py --BUCKET_NAME="mattdavidow-maxtext-br" --RUN_NAME=${run_name} --TPU_TYPE=v5litepod-256 --NUM_SLICES=${num_slice} --VERSION=v2-alpha-tpuv5-lite --COMMAND="bash setup.sh && bash MaxText/configs/sweep3-individual.sh ${lr} ${scale} ${num_slice} ${key} ${run_name}" --ZONE=us-east5-b      
             done
         done
     done
