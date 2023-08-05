ada57fb969c2ca1c32018e16f001e3ef36b467ec
gobbleturk <mattdavidow@google.com>
2023-08-05 08:22:55 +0000

    Even turkeys have to sleep

--- MaxText/configs/sweep5-baseline-meta.sh
+++ MaxText/configs/sweep5-baseline-meta.sh
@@ -7,7 +7,7 @@ int8_name="F"
 scale=32
 num_slice=8
 init_key=1
-run_name=mattdavidow-canary-a5-sweep5-lr_${lr_name}_int8${int8_name}_scale${scale}_slice${num_slice}_key${key}
+run_name=mattdavidow-sweep5-lr${lr_name}_int8${int8_name}_size${scale}_pods${num_slice}_key${key}
 echo ${run_name}
                 
 python3 multihost_job.py --BUCKET_NAME="mattdavidow-maxtext-br" --RUN_NAME=${run_name} --TPU_TYPE=v5litepod-256 --NUM_SLICES=${num_slice} --VERSION=v2-alpha-tpuv5-lite --COMMAND="bash setup.sh && bash MaxText/configs/sweep5-individual.sh ${lr} ${int8} ${scale} ${num_slice} ${key} ${run_name}" --ZONE=us-east5-b      
