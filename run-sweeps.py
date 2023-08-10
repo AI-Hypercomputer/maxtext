399c869faf4b56a834f40ebcd603778e95252844
Lukasz Lew <lukasz.lew@gmail.com>
2023-08-10 11:38:13 -0700

    Added V4 support and argparse to run-sweep.py

--- run-sweeps.py
+++ run-sweeps.py
@@ -3,6 +3,11 @@ import yaml
 import copy
 import os
 
+args = {
+    'dryrun': True,
+    'tpu': 'v4', # 'v4' 'v5'
+}
+
 ###################    Common Code    ###################
 def calc_chinchilla_step_count(num_params_billions, num_slices):
     base_steps = 20000 # number of Chinchilla steps for 1B model on 1 pod for per_device_batch of 4 seq length 1k
@@ -25,26 +30,44 @@ def write_yml(yml_filename, experiment_yml):
         yaml.dump(experiment_yml, file)
 
 # Common multihost_job arguments (although slice is likely to change)
-BASE_MHJ_DICT={
-    '--BUCKET_NAME':'mattdavidow-maxtext-br',
-    '--NUM_SLICE':1,
-    '--TPU_TYPE':'v5litepod-256',
-    '--VERSION':'v2-alpha-tpuv5-lite',
-    '--PROJECT':'tpu-prod-env-multipod',
-    '--ZONE':'us-east5-b'
+V5_MHJ_DICT={
+    '--BUCKET_NAME': 'mattdavidow-maxtext-br',
+    '--NUM_SLICE': 1,
+    '--TPU_TYPE': 'v5litepod-256',  # v5litepod-16
+    '--VERSION': 'v2-alpha-tpuv5-lite',
+    '--PROJECT': 'tpu-prod-env-multipod',
+    '--ZONE': 'us-east5-b'
+}
+
+V4_MHJ_DICT={
+    '--BUCKET_NAME': 'mattdavidow-br',  # for cloud-tpu-multipod-dev
+    '--NUM_SLICE': 1,
+    '--TPU_TYPE': 'v4-128',  # v4-8
+    '--VERSION': 'tpu-ubuntu2204-base',
+    '--PROJECT': 'cloud-tpu-multipod-dev',
+    '--ZONE': 'us-central2-b',
+    '--CQR_EXTRA_ARGS': ' --best-effort',
 }
 
 base_yml_file = 'MaxText/configs/base.yml'
 BASE_YML_DATA=read_yaml_file(base_yml_file)
+
 # This is a common base among all (or nearly all) sweeps
-base_yml_updates = {
+v5_base_yml_updates = {
     'per_device_batch_size':4,
     'enable_dropout':False,
     'base_output_directory':'gs://maxtext-experiments-multipod',
     'dataset_path':'gs://max-datasets-rogue',
     'remat_policy':'full'
 }
-BASE_YML_DATA=update_yaml_fields(BASE_YML_DATA, base_yml_updates)
+
+v4_base_yml_updates = {
+    'per_device_batch_size':4,
+    'enable_dropout':False,
+    'base_output_directory':'gs://max-experiments',
+    'dataset_path':'gs://maxtext-dataset',
+    'remat_policy':'full'
+}
 
 BASE_MHJ_CMD="""export LIBTPU_INIT_ARGS="--xla_tpu_spmd_rng_bit_generator_unsafe=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true" && \
 bash setup_with_retries.sh && \
@@ -56,7 +79,12 @@ def run_experiment(experiment_mhj):
         mhj_args.append(key)
         mhj_args.append(str(experiment_mhj[key]))
     # print('MHJ final args: ', mhj_args)
-    multihost_job_main(mhj_args)
+    if args['dryrun']:
+        import pprint
+        pprint.pprint(mhj_args)
+        print()
+    else:
+        multihost_job_main(mhj_args)
 
 def get_mhj_command(base_cmd, mhj_yml):
     return base_cmd + mhj_yml
@@ -65,7 +93,7 @@ def run_sweep(sweep_base_yml, sweep_base_mhj_dict, experiment_list, base_run_nam
     for experiment in experiment_list:
         experiment_mhj = update_yaml_fields(sweep_base_mhj_dict, experiment['mhj'])
         experiment_yml = update_yaml_fields(sweep_base_yml, experiment['maxtext'])
-        
+
         # Calculate steps:
         if not steps:
             steps = calc_chinchilla_step_count(experiment_yml['global_parameter_scale'], experiment_mhj['--NUM_SLICE'])
@@ -84,7 +112,7 @@ def run_sweep(sweep_base_yml, sweep_base_mhj_dict, experiment_list, base_run_nam
 
             print("Running experiment: ",run_name)
             run_experiment(experiment_mhj)
-            
+
             # Cleanup - delete writen yml
             os.remove(experiment_yml_file)
 
@@ -100,8 +128,22 @@ def run_sweep_8(base_run_name, attempt_number, only_print_run_names=False):
         'adam_eps':1e-3,
         'int8_training': True
     }
-    sweep8_base_yml = update_yaml_fields(BASE_YML_DATA, sweep8_base_yml_update)
-    sweep8_base_mhj = BASE_MHJ_DICT
+
+    if args['tpu'] == 'v4':
+        base_yml_updates = v4_base_yml_updates
+    elif args['tpu'] == 'v5':
+        base_yml_updates = v5_base_yml_updates
+    else:
+        assert False
+
+    BASE_YML_DATA2=update_yaml_fields(BASE_YML_DATA, base_yml_updates)
+    sweep8_base_yml = update_yaml_fields(BASE_YML_DATA2, sweep8_base_yml_update)
+    if args['tpu'] == 'v4':
+        sweep8_base_mhj = V4_MHJ_DICT
+    elif args['tpu'] == 'v5':
+        sweep8_base_mhj = V5_MHJ_DICT
+    else:
+        assert False
     sweep8_steps = 15000
 
     # Experiment Axes
@@ -115,14 +157,14 @@ def run_sweep_8(base_run_name, attempt_number, only_print_run_names=False):
     for fwd_int8, fwd_int8_name in zip(fwd_int8_array, fwd_int8_name_array):
         for bwd_int8, bwd_int8_name in zip(bwd_int8_array, bwd_int8_name_array):
             experiment_name = f"fwd{fwd_int8_name}_bwd{bwd_int8_name}"
-            maxtext_config={'fwd_int8':fwd_int8, 'bwd_int8':bwd_int8} 
+            maxtext_config={'fwd_int8':fwd_int8, 'bwd_int8':bwd_int8}
             experiment_list.append({'name':experiment_name, 'maxtext':maxtext_config,'mhj':{}})
 
     run_sweep(sweep8_base_yml, sweep8_base_mhj, experiment_list, base_run_name, attempt_number,steps=sweep8_steps, only_print_run_names=only_print_run_names)
 
 ###################    Sweep 7    ###################
 def run_sweep_7(base_run_name, attempt_number, only_print_run_names=False):
- 
+
     # All experiments inherit:
     sweep7_base_yml_update={
         'global_parameter_scale':8,
@@ -142,10 +184,24 @@ def run_sweep_7(base_run_name, attempt_number, only_print_run_names=False):
         {'name':"adam_b2_93", 'maxtext':{'adam_b2':0.93}, 'mhj':{}},
         {'name':"adam_b2_90", 'maxtext':{'adam_b2':0.90}, 'mhj':{}},
         {'name':"adam_b2_85", 'maxtext':{'adam_b2':0.85}, 'mhj':{}},
-        ] 
+        ]
 
-    sweep7_base_yml = update_yaml_fields(BASE_YML_DATA, sweep7_base_yml_update)
-    sweep7_base_mhj = BASE_MHJ_DICT
+    if args['tpu'] == 'v4':
+        base_yml_updates = v4_base_yml_updates
+    elif args['tpu'] == 'v5':
+        base_yml_updates = v5_base_yml_updates
+    else:
+        assert False
+
+    BASE_YML_DATA2=update_yaml_fields(BASE_YML_DATA, base_yml_updates)
+    sweep7_base_yml = update_yaml_fields(BASE_YML_DATA2, sweep7_base_yml_update)
+
+    if args['tpu'] == 'v4':
+        sweep7_base_mhj = V4_MHJ_DICT
+    elif args['tpu'] == 'v5':
+        sweep7_base_mhj = V5_MHJ_DICT
+    else:
+        assert False
 
     for experiment in experiment_list:
         # print("Starting experiment: ", experiment['name']) # DEBUG
@@ -154,7 +210,7 @@ def run_sweep_7(base_run_name, attempt_number, only_print_run_names=False):
 
         # Define "indepedent" maxtext configs (all except step count and run_name)
         experiment_yml = update_yaml_fields(sweep7_base_yml, experiment['maxtext'])
-        
+
         # Calculate steps:
         # We don't have compute/time to run to Chinchilla steps on 1 pod = 160k
         #steps = calc_chinchilla_step_count(experiment_yml['global_parameter_scale'], experiment_mhj['--NUM_SLICE'])
@@ -181,10 +237,19 @@ def run_sweep_7(base_run_name, attempt_number, only_print_run_names=False):
         print(run_name)
         if not only_print_run_names:
             run_experiment(experiment_mhj)
-        
+
         # Cleanup - delete writen yml
         os.remove(experiment_yml_file)
 
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='TPU configuration options')
+    parser.add_argument('--dryrun', type=bool, default=True)
+    parser.add_argument('--tpu', type=str, default='v5')
+    pargs = parser.parse_args()
+    global args
+    args = pargs.__dict__
+    run_sweep_8('mattdavidow-sweep8', 2, only_print_run_names=False)
+    #run_sweep_7('mattdavidow-sweep7',2, only_print_run_names=True)
 
-run_sweep_8('mattdavidow-sweep8', 2, only_print_run_names=True)
-#run_sweep_7('mattdavidow-sweep7',2, only_print_run_names=True)
+main()
