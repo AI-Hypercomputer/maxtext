acea423c305cfbd157095e190fe0f81e1c99787a
michelle-yooh <yooh@google.com>
2023-08-11 17:47:16 +0000

    Add end to end tests for 64B and 128B params configs

---
+++ MaxText/configs/128b_small_seq_len.sh
@@ -0,0 +1,10 @@
+echo "Running 128b.sh"
+
+RUN_NAME=${1}
+OUTPUT_PATH=${2}
+DATASET_PATH=${3}
+
+bash rto_setup.sh
+
+TFLOP_THRESHOLD=0 # set to 0 since we are not actually running as a test.
+bash end_to_end/test_tflops_128b_params_small_seq_len.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
---
+++ end_to_end/test_tflops_128b_params_medium_seq_len.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+USER=${1}
+TFLOP_THRESHOLD=${2}
+OUTPUT_PATH=${3}
+DATASET_PATH=${4}
+
+
+if [ -z ${5} ]
+then 
+    RUN_NAME=${USER}
+else
+    RUN_NAME=${5}
+fi
+
+# Train
+export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
+python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
+    steps=150 per_device_batch_size=2 enable_checkpointing=false\
+    enable_profiler=false remat_policy=full base_emb_dim=10240 base_mlp_dim=40960\
+    base_num_heads=64 base_num_decoder_layers=84 head_dim=256\
+    max_target_length=1024 metrics_file='metrics.txt' base_output_directory=$OUTPUT_PATH\
+    ici_fsdp_parallelism=16 ici_tensor_parallelism=16\
+    dataset_path=$DATASET_PATH log_period=150
+
+# Assert TFLOP/s
+python3 end_to_end/eval_assert.py metrics_average metrics.txt $TFLOP_THRESHOLD perf/per_device_tflops_per_sec
