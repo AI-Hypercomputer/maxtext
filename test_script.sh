cd4f947147c68d5501337eae9015a333add2e915
tonyjohnchen <tonyjohnchen@google.com>
2023-08-14 16:46:58 +0000

    save current work.

--- test_script.sh
+++ test_script.sh
@@ -45,29 +45,39 @@ done
 # GCS_PATH=wenqicao-prod-multipods
 # gsutil cp blaze-bin/learning/brain/tfrc/executor/_libtpu.so gs://${GCS_PATH}/
 
-gsutil cp gs://wenqicao-prod-multipods/_libtpu.so /tmp/libtpu.so
+# gsutil cp gs://wenqicao-prod-multipods/_libtpu.so /tmp/libtpu.so
 
-# network rto optimization
-first_line_res=$(ip route show | head -n 1)
-if [[ "$(echo "$first_line_res" | grep "rto_min lock 5ms" | wc -l)" -eq 0 ]]; then
-  sudo ip route change "${first_line_res}" rto_min 5ms
-fi
+# # gke download exp config
+# gsutil cp gs://"${BUCKET_NAME}"/mlperf_test_script/src/*.py /usr/local/lib/python3.10/site-packages/paxml/tasks/lm/params/
 
-# network nocache copy optimization
-sudo ethtool -K ens9 tx-nocache-copy on
+# gce download exp config
+gsutil cp gs://"${BUCKET_NAME}"/mlperf_test_script/src/*.py $HOME/.local/lib/python3.10/site-packages/paxml/tasks/lm/params/
 
-# download exp config
-gsutil cp gs://"${BUCKET_NAME}"/mlperf_test_script/src/*.py ./venv/lib/python3.10/site-packages/paxml/tasks/lm/params/
 #gsutil cp gs://"${BUCKET_NAME}"/mlperf_test_script/patch_src/opt/*.py ./venv/lib/python3.10/site-packages/paxml/
 #gsutil cp gs://${BUCKET_NAME}/mlperf_test_script/patch_src/profile/*.py ./venv/lib/python3.10/site-packages/paxml/
 
 
-# experiment command
-XLA_FLAGS="--xla_dump_to=/tmp/xla_dump_file --xla_dump_hlo_as_proto" TF_CPP_MIN_LOG_LEVEL=0 LIBTPU_INIT_ARGS="--xla_tpu_enable_megascale_barrier=true --xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_enable_async_collective_permute=true --xla_jf_rematerialization_percent_shared_memory_limit=97 --xla_tpu_decompose_all_gather_einsum=false --xla_tpu_spmd_threshold_for_allgather_cse=10 --xla_tpu_prefuse_self_attention=false --xla_tpu_rwb_fusion=false --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_dcn_max_overlap_estimation=32.0 --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_vf_vmem_max_overlap_to_mem_size_async_copy_ratio=10 --megascale_enable_async_host_commands=true --xla_tpu_dot_dot_fusion_duplicated=true --xla_tpu_enable_flash_attention=true --xla_tpu_scavenge_vmem_for_fusions=true" TPU_PREMAPPED_BUFFER_SIZE=4294967296 TPU_LIBRARY_PATH=/tmp/libtpu.so TPU_NAME=local TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 JAX_USE_PJRT_C_API_ON_TPU=1 python3 ./venv/lib/python3.10/site-packages/paxml/main.py \
+# # gke experiment command
+# XLA_FLAGS="--xla_dump_to=/tmp/xla_dump_file --xla_dump_hlo_as_proto" TF_CPP_MIN_LOG_LEVEL=0 \
+# LIBTPU_INIT_ARGS="--xla_tpu_enable_megascale_barrier=true --xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_enable_async_collective_permute=true --xla_jf_rematerialization_percent_shared_memory_limit=97 --xla_tpu_decompose_all_gather_einsum=false --xla_tpu_spmd_threshold_for_allgather_cse=10 --xla_tpu_prefuse_self_attention=false --xla_tpu_rwb_fusion=false --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_dcn_max_overlap_estimation=32.0 --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_vf_vmem_max_overlap_to_mem_size_async_copy_ratio=10 --megascale_enable_async_host_commands=true --xla_tpu_dot_dot_fusion_duplicated=true --xla_tpu_enable_flash_attention=true --xla_tpu_scavenge_vmem_for_fusions=true" TPU_PREMAPPED_BUFFER_SIZE=4294967296 TPU_NAME=local TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 JAX_USE_PJRT_C_API_ON_TPU=1 \
+#     python3 /usr/local/lib/python3.10/site-packages/paxml/main.py \
+#     --exp=tasks.lm.params.c4_mlperf_test."${exp_name}" \
+#     --job_log_dir="${job_dir}" \
+#     --jax_profiler_port=9999 \
+#     --enable_checkpoint_saving=false \
+#     --mode=train --eval_on_test 2>&1 | tee /tmp/large_scale_multislice_test_log
+
+# gce experiment command
+XLA_FLAGS="--xla_dump_to=/tmp/xla_dump_file --xla_dump_hlo_as_proto" TF_CPP_MIN_LOG_LEVEL=0 LIBTPU_INIT_ARGS="--xla_tpu_enable_megascale_barrier=true --xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_enable_async_collective_permute=true --xla_jf_rematerialization_percent_shared_memory_limit=97 --xla_tpu_decompose_all_gather_einsum=false --xla_tpu_spmd_threshold_for_allgather_cse=10 --xla_tpu_prefuse_self_attention=false --xla_tpu_rwb_fusion=false --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_dcn_max_overlap_estimation=32.0 --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_vf_vmem_max_overlap_to_mem_size_async_copy_ratio=10 --megascale_enable_async_host_commands=true --xla_tpu_dot_dot_fusion_duplicated=true --xla_tpu_enable_flash_attention=true --xla_tpu_scavenge_vmem_for_fusions=true" TPU_PREMAPPED_BUFFER_SIZE=4294967296 TPU_NAME=local TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 JAX_USE_PJRT_C_API_ON_TPU=1 \
+multiprocess_gpu=True num_hosts=2 \
+python3 $HOME/.local/lib/python3.10/site-packages/paxml/main.py \
     --exp=tasks.lm.params.c4_mlperf_test."${exp_name}" \
     --job_log_dir="${job_dir}" \
     --jax_profiler_port=9999 \
     --enable_checkpoint_saving=false \
-    --mode=train --eval_on_test 2>&1 | tee /tmp/large_scale_multislice_test_log
+    --mode=train --eval_on_test 2>&1 | sudo tee /tmp/large_scale_multislice_test_log
 
 source /tmp/mlperf_test_script/parser_metrics.sh
+
+
+# /tmp/2023-08-12-05-29-59/
