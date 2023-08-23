a34b3fc470ec2a909f23a076374fb893f4cb0801
gobbleturk <mattdavidow@google.com>
2023-08-23 23:00:09 +0000

    gcloud updated return get

--- multihost_runner.py
+++ multihost_runner.py
@@ -47,17 +47,17 @@ import os
 import re
 
 ##### Define flags #####
-def default_project():
+def get_project():
   completed_command = subprocess.run(["gcloud", "config", "get", "project"], check=True, capture_output=True)
   project_outputs = completed_command.stdout.decode().strip().split('\n')
-  if len(project_outputs) < 1:
+  if len(project_outputs) < 1 or project_outputs[-1]=='':
     sys.exit("You must specify the project in the PROJECT flag or set it with 'gcloud config set project <project>'")
   return project_outputs[-1] # The project name lives on the last line of the output
 
-def default_zone():
+def get_zone():
   completed_command = subprocess.run(["gcloud", "config", "get", "compute/zone"], check=True, capture_output=True)
   zone_outputs = completed_command.stdout.decode().strip().split('\n')
-  if len(zone_outputs) < 1 or zone_outputs[0]=='':
+  if len(zone_outputs) < 1 or zone_outputs[-1]=='':
     sys.exit("You must specify the zone in the ZONE flag or set it with 'gcloud config set compute/zone <zone>'")
   return zone_outputs[-1] # The zone name lives on the last line of the output
 
@@ -359,9 +359,9 @@ def main() -> None:
 
   ##### Validate arguments #####
   if not args.PROJECT:
-    args.PROJECT = default_project()
+    args.PROJECT = get_project()
   if not args.ZONE:
-    args.ZONE = default_zone()
+    args.ZONE = get_zone()
 
   assert_script_dir_exists(args.SCRIPT_DIR)
 
