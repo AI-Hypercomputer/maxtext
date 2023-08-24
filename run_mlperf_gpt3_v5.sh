9f4ea5898ffe635787c5c79be29cc90ceb076f37
tonyjohnchen <tonyjohnchen@google.com>
2023-08-24 18:38:08 +0000

    update

--- run_mlperf_gpt3_v5.sh
+++ run_mlperf_gpt3_v5.sh
@@ -34,18 +34,3 @@ gsutil cp "${SCRIPTS_DIR}"/test_script.sh gs://"${BUCKET_NAME}"/mlperf_test_scri
 gsutil cp "${SCRIPTS_DIR}"/parser_metrics.sh gs://"${BUCKET_NAME}"/mlperf_test_script
 
 gsutil cp -r "${SCRIPTS_DIR}"/src gs://"${BUCKET_NAME}"/mlperf_test_script/
-
-# python3 multihost_job.py --NUM_SLICES=$NODE_COUNT --RUN_NAME="$RUN_NAME"_"$TIMESTAMP" --BUCKET_NAME="$BUCKET_NAME" --TPU_TYPE=$TPU_TYPE --VERSION=$VERSION --CQR_EXTRA_ARGS="--network=mtu9k" \
-# --COMMAND="bash setup.sh MODE=stable && sudo gsutil cp -r gs://${BUCKET_NAME}/mlperf_test_script/ /tmp/ && pip install protobuf==3.15 && pip install orbax-checkpoint==0.2.7 && pip install fiddle==0.2.8 \
-# && git clone https://github.com/mlperf/logging.git mlperf-logging && pip install -e mlperf-logging \
-# && bash /tmp/mlperf_test_script/test_script.sh ${PAX_DATE} ${EXP} ${BUCKET_NAME}"
-
-# TPU_PREFIX=tonyjohnchen-mlperf-gpt3-v2-alpha-tpuv5-lite-2_v4_2023-08-12-03-28-43_230812_032843
-
-# python3 multihost_runner.py --TPU_PREFIX=$TPU_PREFIX \
-# --COMMAND="bash setup.sh MODE=stable && sudo gsutil cp -r gs://${BUCKET_NAME}/mlperf_test_script/ /tmp/ && pip install protobuf==3.15 && pip install orbax-checkpoint==0.2.7 && pip install fiddle==0.2.8 \
-# && git clone https://github.com/mlperf/logging.git mlperf-logging && pip install -e mlperf-logging \
-# && bash /tmp/mlperf_test_script/test_script.sh ${PAX_DATE} ${EXP} ${BUCKET_NAME}"
-
-# python3 multihost_runner.py --TPU_PREFIX=$TPU_PREFIX \
-# --COMMAND="printenv;"
