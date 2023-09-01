b05707a323e2cbd1769093c3cd7a28c43ca6bbae
Lukasz Lew <lukasz.lew@gmail.com>
2023-09-01 22:50:18 +0000

    Fix cleanup script

--- clean_gloud_qrs.sh
+++ clean_gloud_qrs.sh
@@ -4,8 +4,10 @@ function deljobs() {
   echo "JOBS  $1 / $2"
   jobs=$(gcloud alpha compute tpus queued-resources list --zone=$2 --project=$1)
   echo "$jobs"
-  jobs=$(echo $jobs | grep "FAILED\|SUSPENDED" | sed "s/ .*//g")
-  for job in ${jobs}; do
+  grepped=$(echo "$jobs" | grep "FAILED\|SUSPENDED" | sed "s/ .*//g")
+  echo
+  echo "run these:"
+  for job in ${grepped}; do
     cmd="yes | gcloud alpha compute tpus queued-resources delete --zone=$2 --project=$1 $job"
     echo "$cmd"
     # eval "$cmd" &
@@ -22,96 +24,3 @@ deljobs tpu-prod-env-vlp-2nic us-east5-b
 wait
 
 exit
-
-shopt -s expand_aliases
-
-alias tpugcloud_test='CLOUDSDK_API_ENDPOINT_OVERRIDES_TPU=https://test-tpu.sandbox.googleapis.com/ CLOUDSDK_API_ENDPOINT_OVERRIDES_COMPUTE=https://www.googleapis.com/compute/staging_v1/  CLOUDSDK_API_CLIENT_OVERRIDES_COMPUTE=staging_v1 /google/data/ro/teams/cloud-sdk/gcloud'
-alias tpugcloud_staging='CLOUDSDK_API_ENDPOINT_OVERRIDES_TPU=https://staging-tpu.sandbox.googleapis.com/ CLOUDSDK_API_ENDPOINT_OVERRIDES_COMPUTE=https://www.googleapis.com/compute/staging_v1/ CLOUDSDK_API_CLIENT_OVERRIDES_COMPUTE=staging_v1 /google/data/ro/teams/cloud-sdk/gcloud'
-
-
-# Multipod Tests in TPU-TEST (https://testgrid.corp.google.com/TPU-Test#Multipod%20Tests)
-function clear_test_env(){
-  PROJECT_ID=tpu-test-env-one-vm
-  ZONE=us-central1-iw1
-  delete_test_qr
-}
-
-# Multipod Tests in TPU-STAGING (https://testgrid.corp.google.com/TPU-Staging#Multipod%20Tests)
-function clear_staging_env(){
-  PROJECT_ID=tpu-staging-env-one-vm
-  ZONE=us-central1-iw1
-  delete_staging_qr
-}
-
-# Multipod Tests in TPU-PROD (https://testgrid.corp.google.com/TPU-Prod#Multipod%20Tests)
-function clear_prod_env(){
-  PROJECT_ID=cloud-tpu-multipod-dev
-  ZONE=us-central2-b
-  delete_prod_qr
-}
-
-# Multipod Vlp Tests in TPU-PROD (https://testgrid.corp.google.com/TPU-Prod#Multipod%20Vlp%20Tests)
-function clear_vlp_in_prod_env(){
-  PROJECT_ID=tpu-prod-env-automated
-  ZONE=us-east1-c
-  delete_prod_qr
-}
-
-# Delete v4 QR in prod env
-function clear_v4_prod_env(){
-  PROJECT_ID=tpu-prod-env-multipod
-  ZONE=us-central2-b
-  delete_prod_qr
-}
-
-# Delete VLP QR in prod env
-function clear_vlp_prod_env(){
-  PROJECT_ID=tpu-prod-env-multipod
-  ZONE=us-west4-a
-  delete_prod_qr
-}
-
-# Delete VLP burn-in QR in prod env
-function clear_vlp_burn_in_prod_env(){
-  PROJECT_ID=tpu-prod-env-multipod
-  ZONE=us-east5-b
-  delete_prod_qr
-}
-
-function delete_test_qr(){
-  echo "Deleting in test env at PROJECT: $PROJECT_ID ZONE: $ZONE"
-  tpugcloud_test alpha compute tpus queued-resources list --zone=$ZONE --project=$PROJECT_ID | grep -E "FAILED|SUSPENDED" | awk '{print $1}' | tail -n +1 | while read -r qr_id
-  do
-    echo "Deleting $qr_id"
-    yes | tpugcloud_test alpha compute tpus queued-resources delete "$qr_id" --async --force --zone=$ZONE --project=$PROJECT_ID
-  done
-}
-
-function delete_staging_qr(){
-  echo "Deleting in staging env at PROJECT: $PROJECT_ID ZONE: $ZONE"
-  tpugcloud_staging alpha compute tpus queued-resources list --zone=$ZONE --project=$PROJECT_ID | grep -E "FAILED|SUSPENDED" | awk '{print $1}' | tail -n +1 | while read -r qr_id
-  do
-    echo "Deleting $qr_id"
-    yes | tpugcloud_staging alpha compute tpus queued-resources delete "$qr_id" --async --force --zone=$ZONE --project=$PROJECT_ID
-  done
-}
-
-function delete_prod_qr(){
-  echo "Deleting in prod env at PROJECT: $PROJECT_ID ZONE: $ZONE"
-  gcloud alpha compute tpus queued-resources list --zone=$ZONE --project=$PROJECT_ID | grep -E "FAILED|SUSPENDED" | awk '{print $1}' | tail -n +1 | while read -r qr_id
-  do
-    echo "Deleting $qr_id"
-    yes | gcloud alpha compute tpus queued-resources delete "$qr_id" --async --force --zone=$ZONE --project=$PROJECT_ID
-  done
-}
-
-# Delete All QR for Multipod Tests
-clear_test_env
-clear_staging_env
-clear_prod_env
-clear_vlp_in_prod_env
-
-# Delete all v4 and v5 QRs in project `tpu-prod-env-multipod`
-clear_v4_prod_env
-clear_vlp_prod_env
-clear_vlp_burn_in_prod_env
