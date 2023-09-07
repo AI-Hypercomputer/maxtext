4401ae1573407c29ecf254a8c903bde13a336131
Raymond Zou <raymondzou@google.com>
2023-09-07 21:08:43 +0000

    Add stand alone scripts for largest job

--- MaxText/configs/64b.sh
+++ MaxText/configs/64b.sh
@@ -1,22 +1,10 @@
 echo "Running 64b.sh"
-# Example command to invoke this script
-# bash 64b.sh ${RUN_NAME} ${OUTPUT_PATH} ${DATASET_PATH} ${PLATFORM}
 
 RUN_NAME=${1}
 OUTPUT_PATH=${2}
 DATASET_PATH=${3}
-PLATFORM=${4} # can be 'gke' or 'gce', default is 'gce'
 
-if [ "${PLATFORM}" == "gke" ]
-then 
-    bash gke_rto_setup.sh
-else
-    bash rto_setup.sh
-fi
-
-# For DNS lookup when running on large number of VMs
-echo '142.250.123.95 www.googleapis.com' | tee -a /etc/hosts
-echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
+bash rto_setup.sh
 
 TFLOP_THRESHOLD=0 # set to 0 since we are not actually running as a test.
 bash end_to_end/test_tflops_64b_params.sh ${RUN_NAME} ${TFLOP_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
--- end_to_end/test_tflops_32b_params.sh
+++ end_to_end/test_tflops_32b_params.sh
@@ -21,7 +21,7 @@ python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
     enable_profiler=false remat_policy=full base_emb_dim=8192 base_mlp_dim=32768\
     base_num_heads=32 base_num_decoder_layers=40 head_dim=256\
     max_target_length=2048 metrics_file='metrics.txt' base_output_directory=$OUTPUT_PATH\
-    dataset_path=$DATASET_PATH log_period=150 use_iota_embed=true reuse_example_batch=1
+    dataset_path=$DATASET_PATH log_period=150
 
 # Assert TFLOP/s
 python3 end_to_end/eval_assert.py metrics_average metrics.txt $TFLOP_THRESHOLD perf/per_device_tflops_per_sec
