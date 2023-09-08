e38c5395ace21cc70ba3312fcf37e9171daa3f47
gobbleturk <mattdavidow@google.com>
2023-09-08 23:03:23 +0000

    Four part cosine lr schedule

--- MaxText/train.py
+++ MaxText/train.py
@@ -216,14 +216,10 @@ def train_loop(config, state=None):
 
   # Model and Optimizer definition
   model = Transformer(config)
-  learning_rate_schedule = max_utils.create_learning_rate_schedule(
-      learning_rate=config.learning_rate, warmup_steps=config.warmup_steps
-  )
+  learning_rate_schedule = max_utils.create_learning_rate_schedule(config)
 
   tx = optax.adam(
-      max_utils.create_learning_rate_schedule(
-          learning_rate=config.learning_rate, warmup_steps=config.warmup_steps
-      ),
+      max_utils.create_learning_rate_schedule(config),
       b1=config.adam_b1,
       b2=config.adam_b2,
       eps=config.adam_eps,
