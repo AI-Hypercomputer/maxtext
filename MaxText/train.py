57343bafecadd807cf6c8ab5471dc82b7806a54a
Matthew Davidow <51136315+gobbleturk@users.noreply.github.com>
2023-09-15 09:19:56 -0700

    Global gradient clipping (#138)
    * Add global gradient clipping option, default is on with value 1.0

--- MaxText/train.py
+++ MaxText/train.py
@@ -185,9 +185,14 @@ def train_step(model, config, state, data, dropout_rng):
     return jnp.sum(xent)/jnp.size(xent), intermediate_outputs
 
   grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-  (loss, intermediate_outputs), grads = grad_fn(state.params)
+  (loss, intermediate_outputs), raw_grads = grad_fn(state.params)
+  if config.gradient_clipping_threshold > 0:
+    grads, _ = optax.clip_by_global_norm(config.gradient_clipping_threshold).update(raw_grads, None, None)
+  else:
+    grads = raw_grads
   new_state = state.apply_gradients(grads=grads)
   metrics = {'scalar': {'learning/loss': loss, 'learning/grad_norm' : max_utils.l2norm_pytree(grads),
+             'learning/raw_grad_norm' : max_utils.l2norm_pytree(raw_grads), 
              'learning/param_norm' : max_utils.l2norm_pytree(new_state.params)}, 'scalars': {}}
   if config.record_internal_nn_metrics:
     record_activation_metrics(metrics, intermediate_outputs, config)
