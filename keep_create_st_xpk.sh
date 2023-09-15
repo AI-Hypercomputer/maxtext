c4f35d4db208e2d9dd39f4bbea1c17d0b97a1ffc
tonyjohnchen <tonyjohnchen@google.com>
2023-09-15 17:50:47 +0000

    save prog

--- keep_create_st_xpk.sh
+++ keep_create_st_xpk.sh
@@ -6,7 +6,7 @@ gcloud config set compute/zone $ZONE
 NUM_SLICES=2
 TPU_TYPE=v5litepod-256
 
-limit=20
+limit=10
 
 workloads_num=$(python3 ../experimental/users/vbarr/multipod/xpk/xpk.py workload list --cluster bodaborgprivate5 | grep -E "${USER}-st*" | awk '{print $1}' | wc -l)
 echo "workloads_num: $workloads_num"
@@ -20,18 +20,19 @@ while true; do
         --cluster bodaborgprivate5 \
         --docker-image gcr.io/${PROJECT_ID}/tonyjohnchen_runner_custom \
         --workload ${WORKLOAD_NAME} \
-        --priority low \
+        --priority medium \
         --tpu-type=${TPU_TYPE} \
         --num-slices=${NUM_SLICES}  \
         --command "echo 'libtpu' && echo \$TPU_LIBRARY_PATH; EMIT_MEGASCALE_METRICS=true TPU_NAME=local JAX_USE_PJRT_C_API_ON_TPU=1 TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue steps=100 per_device_batch_size=1"
 
-        sleep 10
+        echo "sleep 120s"
+        sleep 120
         workloads_num=$(python3 ../experimental/users/vbarr/multipod/xpk/xpk.py workload list --cluster bodaborgprivate5 | grep -E "${USER}-st*" | awk '{print $1}' | wc -l)
         echo "workloads_num: $workloads_num"
     done
     workloads_num=$(python3 ../experimental/users/vbarr/multipod/xpk/xpk.py workload list --cluster bodaborgprivate5 | grep -E "${USER}-st*" | awk '{print $1}' | wc -l)
     echo "workloads_num: $workloads_num"
     # Sleep for 1 minute before running the script again
-    echo "sleep 60s"
-    sleep 60
+    echo "sleep 120s"
+    sleep 120
 done
