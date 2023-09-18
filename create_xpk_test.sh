59c912d7c4a51047455a13bed7f72352248d7e72
tonyjohnchen <tonyjohnchen@google.com>
2023-09-18 17:43:45 +0000

    save prog

--- create_xpk_test.sh
+++ create_xpk_test.sh
@@ -17,7 +17,7 @@ do
     --cluster bodaborgprivate5 \
     --docker-image gcr.io/${PROJECT_ID}/tonyjohnchen_runner_nightly \
     --workload ${WORKLOAD_NAME} \
-    --priority medium \
+    --priority low \
     --tpu-type=${TPU_TYPE} \
     --num-slices=${NUM_SLICES}  \
     --command "echo 'libtpu' && echo \$TPU_LIBRARY_PATH; EMIT_MEGASCALE_METRICS=true TPU_NAME=local JAX_USE_PJRT_C_API_ON_TPU=1 TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue steps=100 per_device_batch_size=1"
