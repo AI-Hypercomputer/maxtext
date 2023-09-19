1e23549ca7753d0dd6549f12c19d51630ba30256
gobbleturk <mattdavidow@google.com>
2023-09-19 00:37:10 +0000

    logits working

--- MaxText/layers.py
+++ MaxText/layers.py
@@ -707,23 +707,24 @@ class Embed(nn.Module):
     dtype = self.attend_dtype if self.attend_dtype is not None else self.dtype
     if not self.config.int8_training:
       #return jnp.dot(query, jnp.asarray(self.embedding, dtype).T)
-      return lax.dot_general(query, jnp.asarray(self.embedding, dtype).T, (((1,),(0,)),((),()))
+      return lax.dot_general(query, jnp.asarray(self.embedding, dtype).T, (((query.ndim - 1,),(0,)),((),())))
     else:
       aqt_cfg = maxtext_sweeps.sweep1(
-        cfg.fwd_int8_qk,
-        cfg.dlhs_int8_qk,
-        cfg.drhs_int8_qk,
-        use_dummy_static_bound=cfg.aqt_use_dummy_static_bound,
-        rng_type=cfg.aqt_rng_type,
-        use_fwd_quant=cfg.aqt_use_fwd_quant,
+        self.config.fwd_int8_qk,
+        self.config.dlhs_int8_qk,
+        self.config.drhs_int8_qk,
+        use_dummy_static_bound=self.config.aqt_use_dummy_static_bound,
+        rng_type=self.config.aqt_rng_type,
+        use_fwd_quant=self.config.aqt_use_fwd_quant,
       )
       aqt_dot_general = aqt.make_dot_general(aqt_cfg)
       aqt_key = self.make_rng('aqt')
       context = aqt.Context(key=aqt_key, train_step=None)
       aqt_dot_general = functools.partial(aqt_dot_general, context=context)
-      return aqt_dot_general(query, jnp.asarray(self.embedding, dtype).T, (((1,),(0,)),((),()))
-      lax.dot_general(a, b, (((1,),(0,)),((),()))
-      attn_weights = jnp.einsum('bqhd,bkhd->bhqk', query, key, _dot_general=aqt_dot_general)
+      # !!! AQT requires LHS dtype equals RHS dtype
+      # return aqt_dot_general(query, jnp.asarray(self.embedding).T, (((query.ndim - 1,),(0,)),((),())))
+      # The below fails with jnp.bfloat16
+      return aqt_dot_general(jnp.asarray(query, jnp.float32), jnp.asarray(self.embedding, jnp.float32).T, (((query.ndim - 1,),(0,)),((),())))
 
 class RelativePositionBiases(nn.Module):
   """Adds T5-style relative positional embeddings to the attention logits.
