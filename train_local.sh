9f3d736a7d6002a9c1e06a07d91ef9181e05ec02
gobbleturk <mattdavidow@google.com>
2023-09-19 16:51:52 +0000

    revert train_local.sh

--- train_local.sh
+++ train_local.sh
@@ -1,5 +1,4 @@
 #! /bin/bash -eu
 
 cfg="$1"
-#MaxText/train.py "$cfg" steps=10 run_name=test_run global_parameter_scale=1 load_from_other_directory="" load_from_other_directory_step=-1
-MaxText/train.py "$cfg" steps=10 run_name=test_run global_parameter_scale=1 enable_profiler=False
+MaxText/train.py "$cfg" steps=10 run_name=test_run global_parameter_scale=1 load_from_other_directory="" load_from_other_directory_step=-1
