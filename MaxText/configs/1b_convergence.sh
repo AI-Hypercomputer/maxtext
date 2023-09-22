27a53644b07ce2183787bb19f151c5a8f4036a1e
gobbleturk <mattdavidow@google.com>
2023-09-22 20:50:48 +0000

    improve 1b_convergence loss threshold comment

--- MaxText/configs/1b_convergence.sh
+++ MaxText/configs/1b_convergence.sh
@@ -4,5 +4,5 @@ RUN_NAME=${1}
 OUTPUT_PATH=${2}
 DATASET_PATH=${3}
 
-LOSS_THRESHOLD=100.0 # Set to large value so test is guaranteed to pass.
+LOSS_THRESHOLD=100.0 # Set to large value so test is guaranteed to pass, since we are not running as a test.
 bash end_to_end/test_convergence_1b_params.sh ${RUN_NAME} ${LOSS_THRESHOLD} ${OUTPUT_PATH} ${DATASET_PATH}
