1dc85e150864a754827d458c764e173f8655f733
gobbleturk <mattdavidow@google.com>
2023-09-24 17:19:15 +0000

    Add training commands

---
+++ ex_no_load.sh
@@ -0,0 +1,6 @@
+RUN_NAME=${1}
+
+ckpt_dir=gs://maxtext-experiments-multipod/mattdavidow-save-b/checkpoints
+bd=gs://maxtext-experiments-multipod
+dataset=gs://max-datasets-rogue
+python3 MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME} steps=6 base_output_directory=$bd dataset_path=$dataset learning_rate=1e-2 global_parameter_scale=1 per_device_batch_size=1 enable_checkpointing=True save_period=4
