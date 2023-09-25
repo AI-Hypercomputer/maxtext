7ff9a4e96d7f0aed9c20fa72f4866545c569fd16
gobbleturk <mattdavidow@google.com>
2023-09-25 23:23:08 +0000

    fix load dir

--- MaxText/configs/largest_job/128b_load.sh
+++ MaxText/configs/largest_job/128b_load.sh
@@ -23,7 +23,7 @@ bash gke_rto_setup.sh
 echo '142.250.123.95 www.googleapis.com' | tee -a /etc/hosts
 echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
 
-load_directory="gs://maxtext-experiments-multipod/${load_run_name}/checkpoints"
+load_directory="gs://maxtext-experiments-multipod-useast/${load_run_name}/checkpoints"
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
