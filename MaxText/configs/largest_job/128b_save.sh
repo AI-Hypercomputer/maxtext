607f26b66ab2faf7cbfbc2b63a5ceb52e48e6cb6
gobbleturk <mattdavidow@google.com>
2023-09-25 21:26:38 +0000

    try to remove iota

--- MaxText/configs/largest_job/128b_save.sh
+++ MaxText/configs/largest_job/128b_save.sh
@@ -24,10 +24,10 @@ echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
-    steps=7 per_device_batch_size=1 enable_checkpointing=true async_checkpointing=true\
+    steps=7 per_device_batch_size=0.5 enable_checkpointing=true async_checkpointing=true\
     save_period=5\
-    enable_profiler=false remat_policy=full global_parameter_scale=1\
+    enable_profiler=false remat_policy=full global_parameter_scale=16\
     ici_fsdp_parallelism=16 ici_tensor_parallelism=16\
     max_target_length=2048 base_output_directory=$OUTPUT_PATH\
-    dataset_path=$DATASET_PATH use_iota_embed=true reuse_example_batch=1\
+    dataset_path=$DATASET_PATH use_iota_embed=false reuse_example_batch=1\
     dataset_type=synthetic
