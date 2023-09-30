c8142ca362e9d168cb4e5008e776439e24f16c0c
Rafi Witten <rwitten@google.com>
2023-09-30 03:45:25 +0000

    Set up stable job. Synthetic data.

--- MaxText/configs/largest_job/64bstable.sh
+++ MaxText/configs/largest_job/64bstable.sh
@@ -1,7 +1,7 @@
 
 #bash docker_build_dependency_image.sh MODE=stable LIBTPU_GCS_PATH=gs://libtpu_internal/raymondzou/viperlite/2023-09-27-22:32:47-libtpu.so  (won't work for non-Googlers, contact Googlers for access)
 
-echo "Running 64bstable.sh"
+echo "Running 64bstable.sh 26"
 # Example command to invoke this script
 # bash MaxText/configs/largest_job/64bstable.sh 
 
@@ -30,9 +30,10 @@ export TPU_LOG_DIR=0
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
-    steps=10000 per_device_batch_size=2 enable_checkpointing=true\
+    steps=10000 per_device_batch_size=1 enable_checkpointing=true\
     enable_profiler=false remat_policy=full global_parameter_scale=64\
     max_target_length=2048 base_output_directory=$OUTPUT_PATH\
     dataset_path=$DATASET_PATH use_iota_embed=true\
+    reuse_example_batch=1\
     expansion_factor_real_data=16 enable_data_shuffling=false log_period=1000000 save_period=100\
-    collect_stack_trace=true load_from_other_directory=gs://maxtext-experiments-multipod-useast/mattdavidow-o-save-scale64-slices1-a1/checkpoints
+    collect_stack_trace=false load_from_other_directory=gs://maxtext-experiments-multipod-useast/mattdavidow-o-save-scale64-slices1-a1/checkpoints
