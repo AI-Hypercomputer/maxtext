01e11e7e13bf77b52b7609ed0194e4a3f19f592f
gobbleturk <mattdavidow@google.com>
2023-10-10 00:08:50 +0000

    polished, but still ex_input is unsharded

--- xaot_ex.py
+++ xaot_ex.py
@@ -12,10 +12,10 @@ from jax import tree_util
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Xaot example ptions')
+parser = argparse.ArgumentParser(description='Xaot example options')
 parser.add_argument('--save', type=bool, default=True, action=argparse.BooleanOptionalAction)
-parser.add_argument('--run_f', type=bool, default=False, action=argparse.BooleanOptionalAction)
 parser.add_argument('--load', type=bool, default=False, action=argparse.BooleanOptionalAction)
+parser.add_argument('--run_f', type=bool, default=False, action=argparse.BooleanOptionalAction)
 parser.add_argument('--version', type=str, default='v4-8')
 args = parser.parse_args()
 
@@ -23,57 +23,53 @@ args = parser.parse_args()
 def fun(x):
     return x * x
 
-
-## Use "fake" topology devices to compile f
-if args.version=='v4-8':
-    devices = get_topology_desc(
+if args.save:
+    ## Use "fake" topology devices to compile f
+    if args.version=='v4-8':
+        fake_devices = get_topology_desc(
+            platform='tpu',
+            topology_name=f'v4:2x2x1',
+            chip_config_name='megacore',
+            chips_per_host_bounds=(2, 2, 1),
+            num_slices=1,
+        ).devices
+    elif args.version=='v4-16':
+        fake_devices = get_topology_desc(
         platform='tpu',
-        topology_name=f'v4:2x2x1',
+        topology_name=f'v4:2x2x2',
         chip_config_name='megacore',
-        chips_per_host_bounds=(2, 2, 1),
+        chips_per_host_bounds=(2, 2, 2),
         num_slices=1,
     ).devices
-elif args.version=='v4-16':
-    devices = get_topology_desc(
-    platform='tpu',
-    topology_name=f'v4:2x2x2',
-    chip_config_name='megacore',
-    chips_per_host_bounds=(2, 2, 2),
-    num_slices=1,
-).devices
 
-# jit, lower, and compile f using fake devices
-with jax.sharding.Mesh(np.array(devices), ('data',)):
-    jitted = pjit.pjit(
-        fun, in_shardings=P('data'), out_shardings=P(None, 'data')
-    )
-    lowered = jitted.lower(
-        jax.core.ShapedArray(shape=(128, 128), dtype=np.float32)
-    )
-orig_compiled = lowered.compile()
-print(f"{orig_compiled=}")
+    # jit, lower, and compile f using fake devices
+    with jax.sharding.Mesh(np.array(fake_devices), ('data',)):
+        jitted = pjit.pjit(
+            fun, in_shardings=P('data'), out_shardings=P(None, 'data')
+        )
+        lowered = jitted.lower(
+            jax.core.ShapedArray(shape=(128, 128), dtype=np.float32)
+        )
+    orig_compiled = lowered.compile()
 
-
-if args.save:
+    # Serialize and save the compiled object
     serialized, in_tree, out_tree = serialize(orig_compiled)
     with open(f"x_aot_{args.version}.pickle", "wb") as f:
         pickle.dump(serialized, f)
-    # with open(f"x_in_tree_{args.version}.pickle", "wb") as f:
-    #     pickle.dump(in_tree, f)
-    # with open(f"x_out_tree_{args.version}.pickle", "wb") as f:
-    #     pickle.dump(out_tree, f)
 
 if args.run_f:
+    # This is not a sharded array. However we tell pjit above that the input is sharded along the "data" axis.
+    # Why does this work?
     ex_input = 2.0 * jnp.ones((128, 128), dtype=jnp.float32)
     if args.load:
         with open(f"x_aot_{args.version}.pickle", "rb") as f:
             serialized_compiled = pickle.load(f)
 
-        # Get shape of input
+        # Input shape
         # ex_input_for_shape = jax.core.ShapedArray(shape=(128, 128), dtype=np.float32)
         flat_in_shaped, in_tree_recreated = tree_util.tree_flatten(((ex_input,),{}))
 
-        # Get shape of output
+        # Output shape
         out_shaped = jax.eval_shape(fun, ex_input)
         flat_out_shaped, out_tree_recreated = jax.tree_util.tree_flatten(out_shaped)
 
