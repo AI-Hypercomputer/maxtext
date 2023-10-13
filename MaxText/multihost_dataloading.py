d9678e7408b956fb3f78df133ee90d873010b824
gobbleturk <mattdavidow@google.com>
2023-10-13 18:14:42 +0000

    trying Ivy form_gda

--- MaxText/multihost_dataloading.py
+++ MaxText/multihost_dataloading.py
@@ -145,6 +145,9 @@ def get_next_batch_sharded(local_dataset: tf.data.Dataset,
   input_sharding_constraint = PartitionSpec(*data_sharding, None)
 
   def form_gda(local_data, shape):
+    return jax.device_put(local_data, jax.sharding.NamedSharding(global_mesh, input_sharding_constraint))
+
+  def form_gda_old(local_data, shape):
     device_buffers = _put_to_devices(local_data)
     #  Wrap device buffers as GDA
     shape = tuple(shape)
