18ebf867b0f7512c2b4d61e7162ee55b49774c04
Abhinav Goel <abhinavgoel95@gmail.com>
2023-10-17 15:07:10 -0700

    Adding support for GPU

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 # Description:
-# bash setup.sh MODE={stable,nightly,head,libtpu-only} LIBTPU_GCS_PATH={gcs_path_to_custom_libtpu}
+# bash setup.sh MODE={stable,nightly,head,libtpu-only} LIBTPU_GCS_PATH={gcs_path_to_custom_libtpu} DEVICE={tpu,gpu}
 
 # You need to specificy a MODE, default value stable. 
 # You have the option to provide a LIBTPU_GCS_PATH that points to a libtpu.so provided to you by Google. 
@@ -45,21 +45,22 @@ if [[ -n $JAX_VERSION && ! ($MODE == "stable" || -z $MODE) ]]; then
      exit 1
 fi
 
-libtpu_path="$HOME/custom_libtpu/libtpu.so"
-
-if [[ "$MODE" == "libtpu-only" ]]; then
-    # Only update custom libtpu.
-    if [[ -n "$LIBTPU_GCS_PATH" ]]; then
-        # Install custom libtpu
-        echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
-        # Install required dependency
-        pip3 install -U crcmod
-        # Copy libtpu.so from GCS path
-        gsutil cp "$LIBTPU_GCS_PATH" "$libtpu_path"
-        exit 0
-    else
-        echo -e "\n\nError: You must provide a custom libtpu for libtpu-only mode.\n\n"
-        exit 1
+if [[ $DEVICE == "tpu" ]]; then 
+    libtpu_path="$HOME/custom_libtpu/libtpu.so"
+    if [[ "$MODE" == "libtpu-only" ]]; then
+        # Only update custom libtpu.
+        if [[ -n "$LIBTPU_GCS_PATH" ]]; then
+            # Install custom libtpu
+            echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
+            # Install required dependency
+            pip3 install -U crcmod
+            # Copy libtpu.so from GCS path
+            gsutil cp "$LIBTPU_GCS_PATH" "$libtpu_path"
+            exit 0
+        else
+            echo -e "\n\nError: You must provide a custom libtpu for libtpu-only mode.\n\n"
+            exit 1
+        fi
     fi
 fi
 
@@ -88,26 +89,47 @@ fi
 
 if [[ "$MODE" == "stable" || ! -v MODE ]]; then
 # Stable mode
-    if [[ -n "$JAX_VERSION" ]]; then
-        echo "Installing stable jax, jaxlib, libtpu version ${JAX_VERSION}"
-        pip3 install jax[tpu]==${JAX_VERSION} -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-    else
-        echo "Installing stable jax, jaxlib, libtpu"
-        pip3 install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-    fi
+    if [[ $DEVICE == "tpu" ]]; then 
+        echo "Installing stable jax, jaxlib for tpu"
+        if [[ -n "$JAX_VERSION" ]]; then
+            echo "Installing stable jax, jaxlib, libtpu version ${JAX_VERSION}"
+            pip3 install jax[tpu]==${JAX_VERSION} -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+        else
+            echo "Installing stable jax, jaxlib, libtpu for tpu"
+            pip3 install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+        fi
 
-    if [[ -n "$LIBTPU_GCS_PATH" ]]; then 
-        # Install custom libtpu
-        echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
-        # Install required dependency
-        pip3 install -U crcmod
-        # Copy libtpu.so from GCS path
-        gsutil cp "$LIBTPU_GCS_PATH" "$libtpu_path"
+        if [[ -n "$LIBTPU_GCS_PATH" ]]; then 
+            # Install custom libtpu
+            echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
+            # Install required dependency
+            pip3 install -U crcmod
+            # Copy libtpu.so from GCS path
+            gsutil cp "$LIBTPU_GCS_PATH" "$libtpu_path"
+        fi
+        if [[ -n "$LIBTPU_GCS_PATH" ]]; then 
+            # Install custom libtpu
+            echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
+            # Install required dependency
+            pip3 install -U crcmod
+            # Copy libtpu.so from GCS path
+            gsutil cp "$LIBTPU_GCS_PATH" "$libtpu_path"
+        fi
+    elif [[ $DEVICE == "gpu" ]]; then 
+        echo "Installing stable jax, jaxlib for NVIDIA gpu"
+        if [[ -n "$JAX_VERSION" ]]; then
+            echo "Installing stable jax, jaxlib ${JAX_VERSION}"
+            pip3 install -U "jax[cuda12_pip]==${JAX_VERSION}" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+        else
+            echo "Installing stable jax, jaxlib, libtpu for NVIDIA gpu"
+            pip3 install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+        fi
     fi
     echo "Installing stable tensorboard plugin profile"
     pip3 install tensorboard-plugin-profile --upgrade
 elif [[ $MODE == "nightly" ]]; then 
 # Nightly mode
+# Not supported for gpu right now
     echo "Installing jax-head, jaxlib-nightly"
     # Install jax from GitHub head
     pip3 install git+https://github.com/google/jax
@@ -130,6 +152,7 @@ elif [[ $MODE == "nightly" ]]; then
     pip3 install tbp-nightly --upgrade
 elif [[ $MODE == "head" ]]; then 
 # Head mode
+# Not supported for gpu right now
     if [[ -n "$LIBTPU_GCS_PATH" ]]; then
         # Install custom libtpu
         echo "Installing libtpu.so from $LIBTPU_GCS_PATH to $libtpu_path"
