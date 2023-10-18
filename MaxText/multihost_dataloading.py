e6677083cb61101fe2058529042158b3465a8c30
gobbleturk <mattdavidow@google.com>
2023-10-18 23:57:26 +0000

    quietly removing the failed form_gda attempt

--- MaxText/multihost_dataloading.py
+++ MaxText/multihost_dataloading.py
@@ -144,7 +144,6 @@ def get_next_batch_sharded(local_dataset: tf.data.Dataset,
   # manual device placing we just did.
   input_sharding_constraint = PartitionSpec(*data_sharding, None)
 
-
   def form_gda(local_data, shape):
     device_buffers = _put_to_devices(local_data)
     #  Wrap device buffers as GDA
