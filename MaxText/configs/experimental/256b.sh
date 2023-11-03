b23e370cebfdebcd41d8716e2cf7935b7b8345ee
Raymond Zou <raymondzou@google.com>
2023-11-03 22:23:56 +0000

    Disable profiler in configs

--- MaxText/configs/experimental/256b.sh
+++ MaxText/configs/experimental/256b.sh
@@ -29,7 +29,7 @@ echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
 export LIBTPU_INIT_ARGS="--xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_collective_permute=true --xla_tpu_enable_ag_backward_pipelining=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME\
     steps=20 per_device_batch_size=1 enable_checkpointing=false\
-    enable_profiler=true remat_policy=minimal global_parameter_scale=256\
+    enable_profiler=false remat_policy=minimal global_parameter_scale=256\
     ici_fsdp_parallelism=-1 ici_tensor_parallelism=8\
     max_target_length=2048 base_output_directory=$OUTPUT_PATH\
     dataset_path=$DATASET_PATH use_iota_embed=true reuse_example_batch=1\
