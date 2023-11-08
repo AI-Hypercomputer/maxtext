64cdfa743e9a0d9b48a7521d6866a9d2d3a88c63
Priyanka Ganesha <prii@google.com>
2023-11-08 12:42:06 -0800

    setup and dns lookup

--- MaxText/configs/experimental/32b.sh
+++ MaxText/configs/experimental/32b.sh
@@ -14,16 +14,19 @@ for ARGUMENT in "$@"; do
     export "$KEY"="$VALUE"
 done
 
-# Set up network
 if [[ $PLATFORM == "gce" ]]; then
+    # Set up network for running on gce
     bash rto_setup.sh
+    # For DNS lookup when running on large number of VMs on gce
+    echo '142.250.123.95 www.googleapis.com' | sudo tee -a /etc/hosts
+    echo '142.251.4.128 storage.googleapis.com' | sudo tee -a /etc/hosts
 else
+    # Set up network for running on gke
     bash gke_rto_setup.sh
+    # For DNS lookup when running on large number of VMs on gke
+    echo '142.250.123.95 www.googleapis.com' | tee -a /etc/hosts
+    echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
 fi
-
-# For DNS lookup when running on large number of VMs
-echo '142.250.123.95 www.googleapis.com' | tee -a /etc/hosts
-echo '142.251.4.128 storage.googleapis.com' | tee -a /etc/hosts
 
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_megacore_fusion_allow_ags=false --xla_enable_async_collective_permute=true --xla_tpu_enable_ag_backward_pipelining=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
