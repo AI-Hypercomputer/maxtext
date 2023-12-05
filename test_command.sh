88463ea0853c4f002daa5e5a6983136ed9002709
tonyjohnchen <tonyjohnchen@google.com>
2023-12-05 01:47:27 +0000

    save test command for hybrid sim

--- test_command.sh
+++ test_command.sh
@@ -1,12 +1,3 @@
-# export PROJECT_ID=tpu-prod-env-multipod
-# export ACCELERATOR_TYPE=v5p-8
-# export ZONE=us-east5-a
-# export RUNTIME_VERSION=v2-alpha-tpuv5
-# export NODE_COUNT=2
-# export TPU_NAME=tonyjohnchen-v5-tpu-v5p-8-2-slices-dcn-9k-jh
-# export NETWORK=${USER}-mtu9k
-# export GCS_PATH="${USER}-1vm/dcn_benchmarks"
-
 export PROJECT_ID=tpu-prod-env-multipod
 export ACCELERATOR_TYPE=v4-8
 export ZONE=us-central2-b
@@ -19,29 +10,24 @@ gcloud auth list
 gcloud config set project ${PROJECT_ID}
 gcloud config set compute/zone ${ZONE}
 
+# # # Remove QR if it exists.
+# yes | gcloud alpha compute tpus queued-resources delete $TPU_NAME --force
+
+# # # Create with QR in default network.
+# gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
+# --node-prefix ${TPU_NAME} \
+# --node-count ${NODE_COUNT} \
+# --project ${PROJECT_ID} \
+# --zone ${ZONE} \
+# --accelerator-type ${ACCELERATOR_TYPE} \
+# --runtime-version ${RUNTIME_VERSION}
+
 RUN_NAME=${USER}-maxtext-$(date +%Y-%m-%d-%H-%M-%S)
 
 python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
---COMMAND="bash setup.sh MODE=stable; \
+--COMMAND="bash setup.sh MODE=nightly; \
 XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
 base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue \
 steps=10 enable_checkpointing=false enable_profiler=false \
-enable_flash_attention=false int8_training=false;"
-
-# #install libtpu-nightly
-# python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
-# --COMMAND="bash setup.sh MODE=nightly; \
-# EMIT_MEGASCALE_METRICS=true \
-# python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-# base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue \
-# steps=50 enable_checkpointing=false enable_profiler=false \
-# enable_flash_attention=false int8_training=false;"
-
-# #already installed libtpu-nightly
-# python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
-# --COMMAND="EMIT_MEGASCALE_METRICS=true \
-# python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-# base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue \
-# steps=50 enable_checkpointing=false enable_profiler=false \
-# enable_flash_attention=false int8_training=false;"
+attention=mha int8_training=false;"
