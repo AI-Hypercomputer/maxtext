6e8d7a3999b8da2e8ab4d25d4fb07b2cb344d34c
tonyjohnchen <tonyjohnchen@google.com>
2023-12-13 19:25:07 +0000

    save progs

--- test_command.sh
+++ test_command.sh
@@ -2,45 +2,41 @@ export PROJECT_ID=tpu-prod-env-multipod
 export ACCELERATOR_TYPE=v5p-8
 export ZONE=us-east5-a
 export RUNTIME_VERSION=v2-alpha-tpuv5
-export NODE_COUNT=16
-export TPU_NAME=tonyjohnchen-tpu-${ACCELERATOR_TYPE}-${NODE_COUNT}-slices-9k-tony
+export NODE_COUNT=14
+export TPU_NAME=tonyjohnchen-tpu-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-mtu9k
 export NETWORK=${USER}-mtu9k
-export GCS_PATH="${USER}-1vm/dcn_benchmarks"
 
 gcloud auth list
 gcloud config set project ${PROJECT_ID}
 gcloud config set compute/zone ${ZONE}
 
-# # Create with QR in mtu9k network
-# gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
-# --node-prefix ${TPU_NAME} \
-# --node-count ${NODE_COUNT} \
-# --project ${PROJECT_ID} \
-# --zone ${ZONE} \
-# --network ${NETWORK} \
-# --accelerator-type ${ACCELERATOR_TYPE} \
-# --runtime-version ${RUNTIME_VERSION}
+# yes | gcloud alpha compute tpus queued-resources delete $TPU_NAME --force
 
-RUN_NAME=${USER}-maxtext-$(date +%Y-%m-%d-%H-%M-%S)
+# Create with QR in mtu9k network
+gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
+--node-prefix ${TPU_NAME} \
+--node-count ${NODE_COUNT} \
+--project ${PROJECT_ID} \
+--zone ${ZONE} \
+--network ${NETWORK} \
+--accelerator-type ${ACCELERATOR_TYPE} \
+--runtime-version ${RUNTIME_VERSION} --best-effort
+gcloud alpha compute tpus queued-resources list --filter=tonyjohnchen
 
-# install libtpu-nightly
+DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+RUN_NAME=${USER}-offload-collective-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
 python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
 --COMMAND="bash setup.sh MODE=nightly; \
 LIBTPU_INIT_ARGS=\"--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true\"
 XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue \
+base_output_directory=gs://tonyjohnchen-offload-collective/ dataset_path=gs://max-datasets-rogue \
 per_device_batch_size=6 reuse_example_batch=1 \
-steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=5 \
+steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=0 gcs_metrics=true \
 attention=mha int8_training=false;"
 
-# # already installed libtpu-nightly
-# python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
-# --COMMAND=" \
-# LIBTPU_INIT_ARGS=\"--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true\"
-# XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
-# python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-# base_output_directory=gs://maxtext-experiments-tpem/ dataset_path=gs://max-datasets-rogue \
-# per_device_batch_size=6 reuse_example_batch=1 \
-# steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=5 \
-# attention=mha int8_training=false;"
+_source_path=/tmp/${DATETIME}/output_slice_0000_worker_0000.txt 
+_dest_path=/cns/pl-d/home/tonyjohnchen/mxla-benchmark-logs/offload/${DATETIME}
+/google/data/ro/projects/cloud/bigstore/fileutil_bs mkdir -p "${_dest_path}"
+/google/data/ro/projects/cloud/bigstore/fileutil_bs cp "${_source_path}" "${_dest_path}"
+echo "check $_dest_path/output_slice_0000_worker_0000.txt"
