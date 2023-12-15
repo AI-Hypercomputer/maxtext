89012c22a4ff3fc7e0ba5c0a3075df54038d18f2
maxtext authors <no-reply@google.com>
2023-12-15 13:13:55 -0800

    Add preflight.sh and delete v5e_high_performance.md
    PiperOrigin-RevId: 591340765

---
+++ preflight.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Running preflight.sh"
+# Command Flags:
+# PLATFORM (Required, must be "gke" or "gce")
+#
+# Example to invoke this script:
+# bash preflight.sh PLATFORM=gke
+
+# Stop execution if any command exits with error
+set -e
+
+# Set environment variables
+for ARGUMENT in "$@"; do
+    IFS='=' read -r KEY VALUE <<< "$ARGUMENT"
+    export "$KEY"="$VALUE"
+done
+
+if [ -z $PLATFORM ]; then
+    echo "Error: PLATFORM flag is missing."
+    exit 1
+fi
+
+if [[ $PLATFORM == "gce" ]]; then
+    # Set up network for running on gce
+    echo "Setting up network for GCE"
+    bash rto_setup.sh
+else
+    # Set up network for running on gke
+    echo "Setting up network for GKE"
+    bash gke_rto_setup.sh
+fi
