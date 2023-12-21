de408a8e74aecf2a40599b571423cb834df32fdc
gobbleturk <mattdavidow@google.com>
2023-12-21 02:51:34 +0000

    Fix v5p name to v5 for AOT API in accelerator_to_spec_map

--- MaxText/tests/train_compile_test.py
+++ MaxText/tests/train_compile_test.py
@@ -25,12 +25,26 @@ class TrainCompile(unittest.TestCase):
   """Tests for the Ahead of Time Compilation functionality, train_compile.py"""
 
   @pytest.mark.tpu
-  def test_save_compiled(self):
-    compiled_trainstep_file='test_compiled.pickle'
+  def test_save_compiled_v4(self):
+    compiled_trainstep_file='test_compiled_v4.pickle'
     train_compile_main((None, "configs/base.yml", f"compiled_trainstep_file={compiled_trainstep_file}",
       "compile_topology=v4-8", "compile_topology_num_slices=1", "base_emb_dim=256", "base_mlp_dim=256",
       "base_num_decoder_layers=2"))
-  
+
+  @pytest.mark.tpu
+  def test_save_compiled_v5e(self):
+    compiled_trainstep_file='test_compiled_v5e.pickle'
+    train_compile_main((None, "configs/base.yml", f"compiled_trainstep_file={compiled_trainstep_file}",
+      "compile_topology=v5e-16", "compile_topology_num_slices=1", "base_emb_dim=256", "base_mlp_dim=256",
+      "base_num_decoder_layers=2"))
+
+  @pytest.mark.tpu
+  def test_save_compiled_v5p_two_slices(self):
+    compiled_trainstep_file='test_compiled_v5p_two_slices.pickle'
+    train_compile_main((None, "configs/base.yml", f"compiled_trainstep_file={compiled_trainstep_file}",
+      "compile_topology=v5p-8", "compile_topology_num_slices=2", "base_emb_dim=256", "base_mlp_dim=256",
+      "base_num_decoder_layers=2"))
+
   @pytest.mark.tpu
   def test_sequence_parallelism(self):
     compiled_trainstep_file='test_compiled.pickle'
