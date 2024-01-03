f1e03013b6e69c9e32f9f2ddfb88424602ed60f6
Priyanka Ganesha <prii@google.com>
2024-01-03 14:40:39 -0800

    remove >1 device restriction

--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -187,7 +187,6 @@ def create_device_mesh(config, devices=None, logging=True):
     num_slices = 1
   num_devices_per_slice = num_devices//num_slices
   max_logging.log(f"Devices: {devices} (num_devices: {num_devices})")
-  assert len(devices) > 1, "You must have at least two devices"
 
   multi_slice_env = num_slices > 1
 
