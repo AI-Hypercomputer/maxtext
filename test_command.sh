42eccaaa1905a5843b4ddf6cf809d3857613de28
tonyjohnchen <tonyjohnchen@google.com>
2024-01-09 19:54:58 +0000

    save prog

--- test_command.sh
+++ test_command.sh
@@ -1,8 +1,8 @@
 export PROJECT_ID=tpu-prod-env-multipod
-export ACCELERATOR_TYPE=v5p-8
+export ACCELERATOR_TYPE=v5p-32
 export ZONE=us-east5-a
 export RUNTIME_VERSION=v2-alpha-tpuv5
-export NODE_COUNT=14
+export NODE_COUNT=8
 export TPU_NAME=tonyjohnchen-tpu-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-mtu9k
 export NETWORK=${USER}-mtu9k
 
@@ -23,20 +23,33 @@ gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
 --runtime-version ${RUNTIME_VERSION} --best-effort
 gcloud alpha compute tpus queued-resources list --filter=tonyjohnchen
 
+
 DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
-RUN_NAME=${USER}-offload-collective-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+RUN_NAME=${USER}-mxla-steptime-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
 python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
 --COMMAND="bash setup.sh MODE=nightly; \
-LIBTPU_INIT_ARGS=\"--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true\"
+LIBTPU_INIT_ARGS=\"xla_tpu_use_megascale_host_reduction=true\"
 XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-base_output_directory=gs://tonyjohnchen-offload-collective/ dataset_path=gs://max-datasets-rogue \
+base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
 per_device_batch_size=6 reuse_example_batch=1 \
-steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=0 gcs_metrics=true \
+steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=0 gcs_metrics=false \
 attention=mha int8_training=false;"
 
-_source_path=/tmp/${DATETIME}/output_slice_0000_worker_0000.txt 
-_dest_path=/cns/pl-d/home/tonyjohnchen/mxla-benchmark-logs/offload/${DATETIME}
-/google/data/ro/projects/cloud/bigstore/fileutil_bs mkdir -p "${_dest_path}"
-/google/data/ro/projects/cloud/bigstore/fileutil_bs cp "${_source_path}" "${_dest_path}"
-echo "check $_dest_path/output_slice_0000_worker_0000.txt"
+# DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+# RUN_NAME=${USER}-offload-collective-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+# python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
+# --COMMAND="bash setup.sh MODE=nightly; \
+# LIBTPU_INIT_ARGS=\"--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true\"
+# XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
+# python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
+# base_output_directory=gs://tonyjohnchen-offload-collective/ dataset_path=gs://max-datasets-rogue \
+# per_device_batch_size=6 reuse_example_batch=1 \
+# steps=50 enable_checkpointing=false enable_profiler=true profile_start_step=0 gcs_metrics=true \
+# attention=mha int8_training=false;"
+
+# _source_path=/tmp/${DATETIME}/output_slice_0000_worker_0000.txt 
+# _dest_path=/cns/pl-d/home/tonyjohnchen/mxla-benchmark-logs/offload/${DATETIME}
+# /google/data/ro/projects/cloud/bigstore/fileutil_bs mkdir -p "${_dest_path}"
+# /google/data/ro/projects/cloud/bigstore/fileutil_bs cp "${_source_path}" "${_dest_path}"
+# echo "check $_dest_path/output_slice_0000_worker_0000.txt"
--- v4_test.sh
+++ v4_test.sh
@@ -3,14 +3,12 @@ export ACCELERATOR_TYPE=v4-8
 export ZONE=us-central2-b
 export RUNTIME_VERSION=tpu-ubuntu2204-base
 export NODE_COUNT=2
-export TPU_NAME=tonyjohnchen-tpu-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-mtu9k
+export TPU_NAME=tonyjohnchen-tpu-${ACCELERATOR_TYPE}-${NODE_COUNT}slices
 export NETWORK=${USER}-mtu9k
 
 gcloud auth list
 gcloud config set project ${PROJECT_ID}
 gcloud config set compute/zone ${ZONE}
-
-# yes | gcloud alpha compute tpus queued-resources delete $TPU_NAME --force 
 
 # Create with QR in mtu9k network
 gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
@@ -20,17 +18,93 @@ gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
 --zone ${ZONE} \
 --network ${NETWORK} \
 --accelerator-type ${ACCELERATOR_TYPE} \
---runtime-version ${RUNTIME_VERSION}  --best-effort
+--runtime-version ${RUNTIME_VERSION} --best-effort
 gcloud alpha compute tpus queued-resources list --filter=tonyjohnchen
+
+#asan
+blaze build -c opt \
+    --config=asan \
+    --config=gce \
+    --copt=-mavx \
+    --copt=-mavx2 \
+    --copt=-mno-fma \
+    --copt=-DPLATFORM_CLOUD_TPU \
+    --copt=-DTPU_GCS_FS \
+    --copt='-DSTRIP_FLAG_HELP=1' \
+    --copt=-DLIBTPU_EXCLUDE_C_API_IMPL \
+    --copt=-DTF_CAPI_WEAK \
+    --//learning/brain/tfrc/executor:max_gce_tpu_version=${TPU_FISHNAME} \
+    --//learning/brain/tfrc/executor:enable_sparsecore \
+    --//third_party/tf_runtime:eigen_mkldnn_contraction_kernel=false \
+    --define=tensorflow_mkldnn_contraction_kernel=0 \
+    --define=with_tpu_support=true \
+    --define=tpu=1 \
+    ${EXTRA_LIBTPU_BUILD_DEFINES} \
+    //learning/brain/tfrc/executor:libtpu.so
+
+ls blaze-bin/learning/brain/tfrc/executor/libtpu.so
+gsutil cp blaze-bin/learning/brain/tfrc/executor/libtpu.so gs://libtpu_internal/tonyjohnchen/mxla/libtpu_stable_with_asan.so
 
 
 DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
 RUN_NAME=${USER}-mxla-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
 python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
---COMMAND="bash setup.sh MODE=nightly LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/mxla/libtpu.so; \
+--COMMAND="bash setup.sh MODE=stable LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/mxla/libtpu_stable_with_asan.so; \
 TPU_LIBRARY_PATH=\$HOME/custom_libtpu/libtpu.so \
+TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 \
 python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
 base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
 per_device_batch_size=1 \
 steps=50 enable_checkpointing=false \
 attention=mha int8_training=false;"
+
+
+# no-asan
+blaze build -c opt \
+    --config=gce \
+    --copt=-mavx \
+    --copt=-mavx2 \
+    --copt=-mno-fma \
+    --copt=-DPLATFORM_CLOUD_TPU \
+    --copt=-DTPU_GCS_FS \
+    --copt='-DSTRIP_FLAG_HELP=1' \
+    --copt=-DLIBTPU_EXCLUDE_C_API_IMPL \
+    --copt=-DTF_CAPI_WEAK \
+    --//learning/brain/tfrc/executor:max_gce_tpu_version=${TPU_FISHNAME} \
+    --//learning/brain/tfrc/executor:enable_sparsecore \
+    --//third_party/tf_runtime:eigen_mkldnn_contraction_kernel=false \
+    --define=tensorflow_mkldnn_contraction_kernel=0 \
+    --define=with_tpu_support=true \
+    --define=tpu=1 \
+    ${EXTRA_LIBTPU_BUILD_DEFINES} \
+    //learning/brain/tfrc/executor:libtpu.so
+
+ls blaze-bin/learning/brain/tfrc/executor/libtpu.so
+gsutil cp blaze-bin/learning/brain/tfrc/executor/libtpu.so gs://libtpu_internal/tonyjohnchen/mxla/libtpu_stable_without_asan.so
+
+
+DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+RUN_NAME=${USER}-mxla-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
+--COMMAND="bash setup.sh MODE=stable LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/mxla/libtpu_stable_without_asan.so; \
+TPU_LIBRARY_PATH=\$HOME/custom_libtpu/libtpu.so \
+TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 \
+python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
+base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
+per_device_batch_size=1 \
+steps=50 enable_checkpointing=false \
+attention=mha int8_training=false;"
+
+
+TPU_NAME=tonyjohnchen-tpu-v4-8-2slices-1
+gcloud compute tpus tpu-vm ssh $TPU_NAME --zone=$ZONE
+
+TPU_LIBRARY_PATH=/home/tonyjohnchen/custom_libtpu/libtpu.so
+
+DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+RUN_NAME=${USER}-mxla-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
+--COMMAND="bash setup.sh MODE=stable; \
+python3 -c 'import jax; print(jax.lib.xla_bridge.get_backend().platform_version)'"
+
+
