7f76c8bdbb75b58898f2a2dc5bf4305b5a03b46e
tonyjohnchen <tonyjohnchen@google.com>
2024-01-29 23:58:40 +0000

    save prog

---
+++ gcs_to_cns.sh
@@ -0,0 +1,132 @@
+
+cns_path=/cns/qn-d/home/mxla-debug/1slices-v5p-networksetting/
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-tonyjohnchen-tpu-v5p-8-1slices-mtu9k-2024-01-27-00-36-44/tensorboard/plugins/profile/2024_01_27_00_38_16/t1v-n-a8f16079-w-0.xplane.pb $cns_path
+
+/cns/qn-d/home/mxla-debug/1slices-v5p-networksetting/t1v-n-a8f16079-w-0.xplane.pb 
+
+cns_path=/cns/qn-d/home/mxla-debug/1slices-v5p-networksetting/ 
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-2slices-2024-01-26-19-13-14/tensorboard/plugins/profile/2024_01_26_19_14_22/t1v-n-a8f16079-w-0.xplane.pb $cns_path
+/cns/qn-d/home/mxla-debug/1slices-v5p/t1v-n-a8f16079-w-0.xplane.pb 
+
+cns_path=/cns/qn-d/home/mxla-debug/2slices-v5p/
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-2slices-2024-01-26-19-17-58/tensorboard/plugins/profile/2024_01_26_19_21_17/t1v-n-9fbbb8ca-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-2slices-2024-01-26-19-17-58/tensorboard/plugins/profile/2024_01_26_19_21_17/t1v-n-cd0358e5-w-0.xplane.pb $cns_path
+
+/cns/qn-d/home/mxla-debug/2slices-v5p/t1v-n-9fbbb8ca-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/2slices-v5p/t1v-n-cd0358e5-w-0.xplane.pb
+
+
+cns_path=/cns/qn-d/home/mxla-debug/4slices-v5p/
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-4slices-2024-01-26-21-07-07/tensorboard/plugins/profile/2024_01_26_21_10_35/t1v-n-2faff3ab-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-4slices-2024-01-26-21-07-07/tensorboard/plugins/profile/2024_01_26_21_10_35/t1v-n-568af329-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-4slices-2024-01-26-21-07-07/tensorboard/plugins/profile/2024_01_26_21_10_35/t1v-n-78b165c6-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-4slices-2024-01-26-21-07-07/tensorboard/plugins/profile/2024_01_26_21_10_35/t1v-n-92d50782-w-0.xplane.pb $cns_path
+
+/cns/qn-d/home/mxla-debug/4slices-v5p/t1v-n-2faff3ab-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/4slices-v5p/t1v-n-568af329-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/4slices-v5p/t1v-n-78b165c6-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/4slices-v5p/t1v-n-92d50782-w-0.xplane.pb
+
+
+
+
+
+cns_path=/cns/qn-d/home/mxla-debug/8slices-v5p/
+
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-0567faa9-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-4e236d6e-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-787b7265-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-a40b4110-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-b1cbd466-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-b87a0917-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-cd2e827e-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-18-49-46/tensorboard/plugins/profile/2024_01_26_18_51_25/t1v-n-f9d05285-w-0.xplane.pb $cns_path
+
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-0567faa9-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-4e236d6e-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-787b7265-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-a40b4110-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-b1cbd466-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-b87a0917-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-cd2e827e-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p/t1v-n-f9d05285-w-0.xplane.pb
+
+
+cns_path=/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/
+
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-08fe381d-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-1311cd01-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-248450c2-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-2c7d11e8-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-84210e4e-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-902e3754-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-9584b6b3-w-0.xplane.pb $cns_path
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-22-34-47/tensorboard/plugins/profile/2024_01_26_22_36_40/t1v-n-d6c7f2b2-w-0.xplane.pb $cns_path
+
+
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-08fe381d-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-1311cd01-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-248450c2-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-2c7d11e8-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-84210e4e-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-902e3754-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-9584b6b3-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting2/t1v-n-d6c7f2b2-w-0.xplane.pb
+
+
+
+cns_path=/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/
+
+fileutil mkdir $cns_path
+
+alias fileutil_bs='/google/data/ro/projects/cloud/bigstore/mpm/fileutil_bs/stable/bin/fileutil_bs'
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-08fe381d-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-1311cd01-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-248450c2-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-2c7d11e8-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-84210e4e-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-902e3754-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-9584b6b3-w-0.xplane.pb $cns_path
+
+fileutil_bs cp /bigstore/tonyjohnchen-mxla-debug/tonyjohnchen-mxla-steptime-debug-v5p-8-8slices-2024-01-26-23-55-34/tensorboard/plugins/profile/2024_01_26_23_57_11/t1v-n-d6c7f2b2-w-0.xplane.pb $cns_path
+
+
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-08fe381d-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-1311cd01-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-248450c2-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-2c7d11e8-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-84210e4e-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-902e3754-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-9584b6b3-w-0.xplane.pb,
+/cns/qn-d/home/mxla-debug/8slices-v5p-networksetting3/t1v-n-d6c7f2b2-w-0.xplane.pb
--- test_command.sh
+++ test_command.sh
@@ -1,7 +1,8 @@
 export PROJECT_ID=tpu-prod-env-multipod
-export ACCELERATOR_TYPE=v5p-8
-export ZONE=us-east5-a
-export RUNTIME_VERSION=v2-alpha-tpuv5
+
+# export ACCELERATOR_TYPE=v5p-8
+# export ZONE=us-east5-a
+# export RUNTIME_VERSION=v2-alpha-tpuv5
 
 export ACCELERATOR_TYPE=v4-8
 export ZONE=us-central2-b
@@ -32,7 +33,7 @@ gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
 --zone ${ZONE} \
 --network ${NETWORK} \
 --accelerator-type ${ACCELERATOR_TYPE} \
---runtime-version ${RUNTIME_VERSION} --best-effort
+--runtime-version ${RUNTIME_VERSION}
 gcloud alpha compute tpus queued-resources list --filter=tonyjohnchen
 
 ## Create Multislice in mtu9k network
@@ -44,41 +45,49 @@ gcloud alpha compute tpus queued-resources create ${TPU_NAME} \
 --network ${NETWORK} \
 --accelerator-type ${ACCELERATOR_TYPE} \
 --runtime-version ${RUNTIME_VERSION} --best-effort
+
 gcloud alpha compute tpus queued-resources list --filter=tonyjohnchen
 
-DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
-RUN_NAME=${USER}-mxla-steptime-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
-python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
---COMMAND="bash setup.sh MODE=nightly LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/viperfish/2024-01-23-23:02:29-libtpu.so && sudo apt install numactl && dpkg -l | grep numactl \
-&& sudo bash MaxText/network_setting.sh; \
-XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
-LIBTPU_INIT_ARGS=\"--xla_tpu_enable_megascale_barrier=true grpc_experiments=event_engine_client,event_engine_listener,trace_record_callops megascale_grpc_enable_xor_tracer=true census_enabled=false megascale_crash_on_stale_unmatched_keys=true megascale_transport_type=grpc tpu_link_up_check_timeout=15s xla_tpu_use_megascale_host_reduction=true megascale_use_insecure_grpc=true grpc_filter_insecure_rpc=false enforce_kernel_ipv6_support=false\" \
-TPU_LIBRARY_PATH=\$HOME/custom_libtpu/libtpu.so TPU_NAME=local JAX_USE_PJRT_C_API_ON_TPU=1 \
-TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 \
-numactl --membind 0 --cpunodebind=0 --strict python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
-base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
-dataset_type=synthetic \
-per_device_batch_size=6 reuse_example_batch=1 \
-global_parameter_scale=1 \
-metrics_file='metrics.txt' \
-steps=50 enable_checkpointing=false gcs_metrics=true enable_profiler=true profiler_steps=5;"
+# DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+# RUN_NAME=${USER}-mxla-steptime-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+# python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
+# --COMMAND="bash setup.sh MODE=nightly LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/viperfish/2024-01-23-23:02:29-libtpu.so && sudo apt install numactl && dpkg -l | grep numactl \
+# && sudo bash MaxText/network_setting.sh; \
+# XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
+# LIBTPU_INIT_ARGS=\"--xla_tpu_enable_megascale_barrier=true grpc_experiments=event_engine_client,event_engine_listener,trace_record_callops megascale_grpc_enable_xor_tracer=true census_enabled=false megascale_crash_on_stale_unmatched_keys=true megascale_transport_type=grpc tpu_link_up_check_timeout=15s xla_tpu_use_megascale_host_reduction=true megascale_use_insecure_grpc=true grpc_filter_insecure_rpc=false enforce_kernel_ipv6_support=false\" \
+# TPU_LIBRARY_PATH=\$HOME/custom_libtpu/libtpu.so TPU_NAME=local JAX_USE_PJRT_C_API_ON_TPU=1 \
+# TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 \
+# numactl --membind 0 --cpunodebind=0 --strict python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
+# base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
+# dataset_type=synthetic \
+# per_device_batch_size=6 reuse_example_batch=1 \
+# global_parameter_scale=1 \
+# metrics_file='metrics.txt' \
+# steps=50 enable_checkpointing=false gcs_metrics=true enable_profiler=true profiler_steps=5 && \
+# python3 end_to_end/eval_assert.py metrics_average metrics.txt 0.0 perf/step_time_seconds;"
 
 DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
-RUN_NAME=${USER}-mxla-steptime-debug-${ACCELERATOR_TYPE}-${NODE_COUNT}slices-${DATETIME}
+RUN_NAME=${USER}-preflight-${TPU_NAME}-${DATETIME}
 python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
---COMMAND="bash setup.sh MODE=nightly LIBTPU_GCS_PATH=gs://libtpu_internal/tonyjohnchen/viperfish/2024-01-23-23:02:29-libtpu.so && sudo apt install numactl && dpkg -l | grep numactl \
-&& sudo bash MaxText/network_setting.sh; \
+--COMMAND="bash preflight.sh PLATFORM=gce"
+
+DATETIME=$(date +%Y-%m-%d-%H-%M-%S)
+RUN_NAME=${USER}-mxla-steptime-debug-${TPU_NAME}-${DATETIME}
+python3 multihost_runner.py --TPU_PREFIX=$TPU_NAME \
+--COMMAND="bash setup.sh MODE=nightly && sudo apt install numactl && dpkg -l | grep numactl; \
+sudo bash MaxText/network_setting.sh; \
 XLA_FLAGS=\"--xla_dump_to=/tmp/xla_dump/\" \
 LIBTPU_INIT_ARGS=\"--xla_tpu_enable_megascale_barrier=true\" \
-TPU_LIBRARY_PATH=\$HOME/custom_libtpu/libtpu.so TPU_NAME=local JAX_USE_PJRT_C_API_ON_TPU=1 \
+TPU_NAME=local \
 TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TPU_VMODULE=tpu_configuration_ops_impl=3 TF_CPP_MIN_LOG_LEVEL=0 \
 numactl --membind 0 --cpunodebind=0 --strict python3 MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME \
 base_output_directory=gs://tonyjohnchen-mxla-debug/ dataset_path=gs://max-datasets-rogue \
 dataset_type=synthetic \
-per_device_batch_size=6 reuse_example_batch=1 \
+per_device_batch_size=24 reuse_example_batch=1 \
 global_parameter_scale=1 \
 metrics_file='metrics.txt' \
-steps=50 enable_checkpointing=false gcs_metrics=true enable_profiler=true profiler_steps=5;"
+steps=50 enable_checkpointing=false gcs_metrics=true enable_profiler=true profiler_steps=5 upload_all_profiler_results=true && \
+python3 end_to_end/eval_assert.py metrics_average metrics.txt 0.0 perf/step_time_seconds;"
 
 
 TPU_NAME_ssh=$TPU_NAME-0
@@ -88,3 +97,7 @@ gs://tonyjohnchen-mxla-debug/10_steps_xplane
 
 gcloud compute tpus tpu-vm ssh $TPU_NAME --worker=0 --zone=$ZONE --project=$PROJECT_ID
 gcloud compute tpus tpu-vm ssh $TPU_NAME --worker=1 --zone=$ZONE --project=$PROJECT_ID
+
+
+
+
