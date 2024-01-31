16556bc6de2f1ba27c533c6dc98a2448b699502f
A9isha <55637700+A9isha@users.noreply.github.com>
2024-01-31 15:56:50 -0800

    Add llama finetuning test (#94)
    * Add Llama2 finetuning test
    
    * reformat

--- dags/multipod/maxtext_end_to_end.py
+++ dags/multipod/maxtext_end_to_end.py
@@ -35,18 +35,24 @@ with models.DAG(
     catchup=False,
 ) as dag:
   test_name_prefix = "maxtext"
-  test_models = ["llama2", "mistral", "gamma", "gpt3"]
+  test_models = {
+      "llama2": ["test_llama2", "llama_finetuning_test"],
+      "mistral": ["test_mistral"],
+      "gamma": ["test_gamma"],
+      "gpt3": ["test_gpt3"],
+  }
   test_modes = [SetupMode.STABLE, SetupMode.NIGHTLY]
 
-  for model in test_models:
+  for model in test_models.keys():
     for mode in test_modes:
-      maxtext_gce_config.get_maxtext_end_to_end_test_config(
-          tpu_version=TpuVersion.V4,
-          tpu_cores=8,
-          tpu_zone=Zone.US_CENTRAL2_B.value,
-          time_out_in_min=60,
-          is_tpu_reserved=False,
-          test_name=f"{test_name_prefix}-{mode.value}-{model}",
-          test_script=f"test_{model}",
-          test_mode=mode,
-      ).run()
+      for test_script in test_models[model]:
+        maxtext_gce_config.get_maxtext_end_to_end_test_config(
+            tpu_version=TpuVersion.V4,
+            tpu_cores=8,
+            tpu_zone=Zone.US_CENTRAL2_B.value,
+            time_out_in_min=60,
+            is_tpu_reserved=False,
+            test_name=f"{test_name_prefix}-{mode.value}-{test_script}",
+            test_script=test_script,
+            test_mode=mode,
+        ).run()
