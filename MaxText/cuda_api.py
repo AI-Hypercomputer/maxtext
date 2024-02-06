8fdb49aa71faa5bf88ab3e7f01d5073f659ed65c
michelle-yooh <yooh@google.com>
2024-02-06 20:19:01 +0000

    Enable cudaProfilerAPI for nsys

---
+++ MaxText/cuda_api.py
@@ -0,0 +1,26 @@
+from ctypes import cdll, c_char_p, c_int, c_uint64, c_void_p
+libcudart = cdll.LoadLibrary('libcudart.so')
+def cudaProfilerStart():
+    libcudart.cudaProfilerStart()
+def cudaProfilerStop():
+    libcudart.cudaProfilerStop()
+
+libnvtx = cdll.LoadLibrary('libnvToolsExt.so')
+libnvtx.nvtxRangePushA.argtypes = [c_char_p]
+libnvtx.nvtxRangePushA.restype = c_int
+libnvtx.nvtxRangeStartA.argtypes = [c_char_p]
+libnvtx.nvtxRangeStartA.restype = c_uint64
+libnvtx.nvtxRangeEnd.argtypes = [c_uint64]
+libnvtx.nvtxDomainRangeStartEx.argtypes = [c_char_p, c_void_p]
+def nvtxRangePush(name):
+    return libnvtx.nvtxRangePushA(name.encode())
+def nvtxRangePop():
+    libnvtx.nvtxRangePop()
+def nvtxRangeStart(name):
+    return libnvtx.nvtxRangeStartA(name.encode())
+def nvtxRangeEnd(range_id):
+    libnvtx.nvtxRangeEnd(range_id)
+def nvtxDomainRangeStart(domain, attrs):
+    return libnvtx.nvtxRangeStartEx(domain, attrs)
+
+
