a6322a2718838826b9656a604fd2825aae54daf5
A9isha <mazumdera@google.com>
2024-02-12 20:12:20 +0000

    fix finetuning

--- MaxText/finetune-llama2-70b.sh
+++ MaxText/finetune-llama2-70b.sh
@@ -3,6 +3,14 @@ export TPU_MIN_LOG_LEVEL=0
 export TPU_VMODULE=tpu_configuration_ops_impl=3
 export JAX_USE_PJRT_C_API_ON_TPU=1
 export TF_CPP_MIN_LOG_LEVEL=0
-python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-finetuning-maxtext base_output_directory=gs://mazumdera-test-bucket-us-west4/maxtext/llama2/02062024/1  dataset_path=gs://maxtext-dataset load_parameters_path=gs://maxtext-llama-us-west4/llama2-70b/maxtext-ckpt/1/0/default model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama-us-west4/llama2-70b steps=5 async_checkpointing=false
+# python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-finetuning-maxtext base_output_directory=gs://mazumdera-test-bucket-us-west4/maxtext/llama2/02062024/1  dataset_path=gs://maxtext-dataset load_parameters_path=gs://maxtext-llama-us-west4/llama2-70b/maxtext-ckpt/3-unchanged/0/default model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama-us-west4/llama2-70b steps=5 async_checkpointing=false
 
-# python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-finetuning-maxtext base_output_directory=gs://mazumdera-test-bucket-us-central2/maxtext/llama2/02062024/1  dataset_path=gs://maxtext-dataset model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama/llama2-7b steps=5 enable_checkpointing=false
+# python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-pretraining-maxtext base_output_directory=gs://mazumdera-test-bucket-us-central2/maxtext/llama2/02062024/1  dataset_path=gs://maxtext-dataset model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama/llama2-7b steps=5 enable_checkpointing=false
+
+# python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-pretraining-maxtext base_output_directory=gs://mazumdera-test-bucket-us-west4/maxtext/llama2/02092024/1  dataset_path=gs://maxtext-dataset model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama/llama2-7b steps=5 async_checkpointing=false
+
+# python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-pretraining-maxtext base_output_directory=gs://mazumdera-test-bucket-us-west4/maxtext/llama2/02092024/1  dataset_path=gs://maxtext-dataset model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama-us-west4/llama2-70b steps=15 async_checkpointing=false
+
+
+python3 MaxText/train.py MaxText/configs/base.yml run_name=llama2-finetuning-maxtext base_output_directory=gs://mazumdera-test-bucket-us-central2/maxtext/llama2/02092024/1  dataset_path=gs://maxtext-dataset load_parameters_path=gs://mazumdera-test-bucket-us-central2/llama2-70b/maxtext-ckpt/4-unchanged/0/default model_name="llama2-70b" per_device_batch_size=.0625 assets_path=gs://maxtext-llama-us-west4/llama2-70b steps=10 async_checkpointing=false
+
