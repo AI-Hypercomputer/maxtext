649df1fd39a48014e3f7ef2a69a3880641a4e99b
Rafi Witten <rwitten@google.com>
2024-02-13 05:22:59 +0000

    Incremental integ

--- MaxText/experimental_decode.py
+++ MaxText/experimental_decode.py
@@ -22,46 +22,61 @@ jax.config.update('jax_platform_name', 'cpu')
 import jax.numpy as jnp
 import numpy as np
 
-import mock
+import myengine
 from inference_engine import token_utils
 from absl.testing import absltest
 
-engine = mock.TestEngine(batch_size=32, cache_length=256, weight=2.0)
-params = engine.load_params()
+import os
+import pyconfig
+import sys
 
-text = 'AB'
-metadata = engine.get_tokenizer()
-vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
-tokens, true_length = token_utils.tokenize_and_pad(text, vocab, is_bos=True)
-prefill_result = engine.prefill(
-    params=params, padded_tokens=tokens, true_length=3
-)
-slot=1
 
-decode_state = engine.init_decode_state()
-decode_state = engine.insert(
-    prefix=prefill_result, decode_state=decode_state, slot=slot
-)
-decode_state, sampled_tokens = engine.generate(
-    params=params, decode_state=decode_state
-)
 
-metadata = engine.get_tokenizer()
-tokenizer = token_utils.load_vocab(
-    metadata.path, metadata.extra_ids
-).tokenizer
-# Char for 266
-tok, _, _ = sampled_tokens.get_result_at_slot(slot)
-assert tokenizer.IdToPiece(int(tok.item())) == 'Ċ'
-decode_state, sampled_tokens = engine.generate(
-    params=params, decode_state=decode_state
-)
-# Char for 399
-tok, _, _ = sampled_tokens.get_result_at_slot(slot)
-assert tokenizer.IdToPiece(int(tok.item())) == 'Ə'
-_, sampled_tokens = engine.generate(
-    params=params, decode_state=decode_state
-)
-# Char for 598
-tok, _, _ = sampled_tokens.get_result_at_slot(slot)
-assert tokenizer.IdToPiece(int(tok.item())) == 'ɖ'
+def main(config):
+  engine = myengine.TestEngine(config)
+  params = engine.load_params()
+
+  text = 'AB'
+  metadata = engine.get_tokenizer()
+  vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
+  tokens, true_length = token_utils.tokenize_and_pad(text, vocab, is_bos=True)
+  prefill_result = engine.prefill(
+      params=params, padded_tokens=tokens, true_length=3
+  )
+  slot=1
+
+  decode_state = engine.init_decode_state()
+  breakpoint()
+  decode_state = engine.insert(
+      prefix=prefill_result, decode_state=decode_state, slot=slot
+  )
+  decode_state, sampled_tokens = engine.generate(
+      params=params, decode_state=decode_state
+  )
+
+  metadata = engine.get_tokenizer()
+  tokenizer = token_utils.load_vocab(
+      metadata.path, metadata.extra_ids
+  ).tokenizer
+  # Char for 266
+  tok, _, _ = sampled_tokens.get_result_at_slot(slot)
+  assert tokenizer.IdToPiece(int(tok.item())) == 'Ċ'
+  decode_state, sampled_tokens = engine.generate(
+      params=params, decode_state=decode_state
+  )
+  # Char for 399
+  tok, _, _ = sampled_tokens.get_result_at_slot(slot)
+  assert tokenizer.IdToPiece(int(tok.item())) == 'Ə'
+  _, sampled_tokens = engine.generate(
+      params=params, decode_state=decode_state
+  )
+  # Char for 598
+  tok, _, _ = sampled_tokens.get_result_at_slot(slot)
+  assert tokenizer.IdToPiece(int(tok.item())) == 'ɖ'
+
+if __name__ == "__main__":
+  jax.config.update('jax_default_prng_impl', 'unsafe_rbg')
+  os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
+  pyconfig.initialize(sys.argv)
+  config = pyconfig.config
+  main(config)
