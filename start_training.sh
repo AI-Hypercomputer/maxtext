b1d10f5b348b36b2746330720e62486a7a46ca7c
Param Bole <parambole@google.com>
2024-02-16 19:25:25 +0000

    Adding harware=gpu information to start_train

--- start_training.sh
+++ start_training.sh
@@ -89,7 +89,7 @@ non_blocking_wait() {
 PIDS=()
 for ((LOCAL_DEVICE_ID=0; LOCAL_DEVICE_ID <= $((GPUS_PER_NODE - 1)); LOCAL_DEVICE_ID++)); do
    PROCESS_ID=$(($GPUS_PER_NODE*$NODE_RANK + $LOCAL_DEVICE_ID))
-   python MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME}_$(date +%Y-%m-%d-%H-%M) local_device_id=$LOCAL_DEVICE_ID process_id=$PROCESS_ID &
+   python MaxText/train.py MaxText/configs/base.yml hardware=gpu run_name=${RUN_NAME}_$(date +%Y-%m-%d-%H-%M) local_device_id=$LOCAL_DEVICE_ID process_id=$PROCESS_ID &
    PID=$!
    PIDS+=($PID)
    echo "Launched MaxText/train.py for local_device_id: $LOCAL_DEVICE_ID process_id: $PROCESS_ID and PID $PID"
