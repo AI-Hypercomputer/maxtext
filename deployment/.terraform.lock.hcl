b71bebc6604545ff0c81c3b447254242c0c7dfcb
Will Cromar <wcromar@google.com>
2024-02-20 15:35:18 -0800

    Add GPU clusters to deployments (#126)
    * Add GPU clusters to deployments
    
    * remove commented code
    
    Change-Id: I11785303c0284af8cee619aa4f6fe7b2e5c83143
    
    * headless service
    
    Change-Id: Idd20a1c407a21bc8f3d2f5e6f96bdbc782d025c0
    
    * make service actually headless
    
    Change-Id: Iee705f97d376ea55e619ad85448dfc61aebc0140

--- deployment/.terraform.lock.hcl
+++ deployment/.terraform.lock.hcl
@@ -38,3 +38,22 @@ provider "registry.terraform.io/hashicorp/google-beta" {
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version = "2.26.0"
+  hashes = [
+    "h1:MMxX/EY9AEGwp5DbGQ+LTd3c9YmjwrnPJHLlyc9u0eU=",
+    "zh:3f8ee1bffab1ba4f6ae549daae1648974214880d3606b6821cb0aceb365284a4",
+    "zh:5596b1248231cc3b8f6a98f5b78df7120cd3153fd2b34b369dc20356a75bf35b",
+    "zh:64420c9e4aa49c5e443afcd60f3e8d293ea6bd78797d402e21e23605f7757954",
+    "zh:8327a488854e15f8d7eaf8272c3b9d6d1d9a6e68212a8dcb111d7b4023aac6b5",
+    "zh:94c1c9b65280847d28a3e90e5046650858ac0bf87feefd2349336444e21e68e8",
+    "zh:a3fb0b0b4bfd1844bb94011ae80111cedc188085235cf466313ca2151e75c8ca",
+    "zh:ab5e381928144e0c2a9d9768a48e38797642e5c5fb2184370c7c08df500e5db3",
+    "zh:da78995e8d6daf3acfd4c455ebbd12f6bf154cadf455f14ef35c0862e58dd2ec",
+    "zh:e24cdd5b90196df93215f40d821af3a7b4473c53992be4c3038940d117a50eb4",
+    "zh:e632efb3bce6d089b7c08507660af8b2c5e3f94c34fe401bfa228f154405e26e",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f5aea9da0eba25d35fee49db193c4b44cd3746a5578065092c62a53077e50b84",
+  ]
+}
---
+++ deployment/clusters.tf
@@ -0,0 +1,95 @@
+resource "google_container_cluster" "gpu-uc1" {
+  name     = "gpu-uc1"
+  project  = "cloud-ml-auto-solutions"
+  location = "us-central1"
+
+  release_channel {
+    channel = "RAPID"
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary" {
+  name       = "primary-pool"
+  project  = google_container_cluster.gpu-uc1.project
+  location   = google_container_cluster.gpu-uc1.location
+  cluster    = google_container_cluster.gpu-uc1.name
+  node_count = 1
+
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+
+resource "google_container_node_pool" "nvidia-v100x2" {
+  name       = "nvidia-v100x2-pool"
+  project  = google_container_cluster.gpu-uc1.project
+  location   = google_container_cluster.gpu-uc1.location
+  cluster    = google_container_cluster.gpu-uc1.name
+  node_count = 3
+
+  node_locations = [
+    "us-central1-b"
+  ]
+
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-highmem-16"
+
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    guest_accelerator {
+      type  = "nvidia-tesla-v100"
+      count = 2
+      gpu_driver_installation_config {
+        gpu_driver_version = "LATEST"
+      }
+    }
+  }
+}
+
+data "google_client_config" "provider" {}
+
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.gpu-uc1.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.gpu-uc1.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+// Headless service required for service discovery within a Job.
+// Pods will be addressable as `hostname.headless-svc`
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "headless-svc"
+  }
+  spec {
+    selector = {
+      headless-svc = "true"
+    }
+    cluster_ip = "None"
+  }
+}
