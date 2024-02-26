9241114ccbf43675ae3009bf657bfaf06d2831a1
gobbleturk <mattdavidow@google.com>
2024-02-26 23:48:59 +0000

    permissions are hard

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,8 +1,9 @@
-local_image_name="image"
+set -e
+local_image_name="mattdavidow_local_image"
 project=tpu-prod-env-multipod
 cloud_image_name="mattdavidow_cloud_image"
 
-bash docker_build_dependency_image.sh $local_image_name
+bash docker_build_fake_image.sh LOCAL_IMAGE_NAME=$local_image_name
 
 docker tag ${local_image_name} gcr.io/$project/${cloud_image_name}:latest
 docker push gcr.io/$project/${cloud_image_name}:latest
