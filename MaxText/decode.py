acf407f5f5aa01a9ce67d7f3bb9071e9a7ee70d0
NinaCai <ninacai@google.com>
2024-02-27 00:12:45 +0000

    change step to after prefill

--- MaxText/decode.py
+++ MaxText/decode.py
@@ -293,12 +293,12 @@ def decode_loop(model, model_vars, sp_tokenizer, rng, prompts):
     if step == first_profiling_step:
       max_utils.activate_profiler(config)
     
-    if step == 8 and jax.process_index() == 0:
+    if step == 70 and jax.process_index() == 0:
       cudaProfilerStart()
-      print("====================step 8=================")
-    if step == 10 and jax.process_index() == 0:
+      print("====================step 70=================")
+    if step == 75 and jax.process_index() == 0:
       cudaProfilerStop()
-      print("=====================step 10================")
+      print("=====================step 75================")
 
     new_position, new_cache, next_logit, selected_id = decode_ar_one_step(
     config, model, model_vars, new_cache, new_position, next_logit, rngs[step],
