bb63733796650e5fb7e49bbd77f8813fe2c6b179
gobbleturk <mattdavidow@google.com>
2024-02-27 00:14:00 +0000

    Debug user

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -6,6 +6,8 @@ cloud_image_name="mattdavidow_cloud_image"
 bash docker_build_fake_image.sh LOCAL_IMAGE_NAME=$local_image_name
 
 docker tag ${local_image_name} gcr.io/$project/${cloud_image_name}:latest
+
+echo "Trying to push with name $USER"
 docker push gcr.io/$project/${cloud_image_name}:latest
 
 echo "All done, check out your artifacts at: gcr.io/$project/${cloud_image_name}"
