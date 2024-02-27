a038e80902c5eb78ba495517d512dd30befe0676
A9isha <mazumdera@google.com>
2024-02-27 21:18:25 +0000

    use jsonl training data

--- MaxText/input_pipeline/input_pipeline_interface.py
+++ MaxText/input_pipeline/input_pipeline_interface.py
@@ -28,6 +28,11 @@ from input_pipeline import _grain_data_processing
 from input_pipeline import _tfds_data_processing_c4_mlperf
 import tokenizer
 
+import max_logging
+import tensorflow as tf
+import functools
+import ml_collections
+
 def get_tokenizer(tokenizer_path, add_bos=True, add_eos=True):
   # Load tokenizer
   sp_tokenizer = tokenizer.load_tokenizer(tokenizer_path=tokenizer_path,
@@ -48,6 +53,74 @@ def make_c4_mlperf_train_iterator_and_tokenizer(config, mesh, add_bos, add_eos):
     data_shuffle_seed=config.data_shuffle_seed
   )
   return train_iter, eval_iter, sp_tokenizer
+
+def get_converted_jsonl_datasets(
+  config: ml_collections.ConfigDict,
+  read_config = None,
+):
+  """Load and return dataset of batched examples for use during training."""
+  # Training dataset.
+  # converted_jsonl_dataset_path = config.dataset_path
+  max_logging.log(f"Trying to read training data from path: {config.file_pattern_for_train_data}")
+  # train_ds = tf.data.TFRecordDataset.list_files(config.file_pattern_for_train_data) 
+  # train_ds = train_ds.map(tf.data.TFRecordDataset)
+  # train_ds = tf.data.TFRecordDataset(config.file_pattern_for_train_data)
+  train_ds = tf.data.TFRecordDataset(tf.data.Dataset.list_files(config.file_pattern_for_train_data))
+
+  def parse_example(example_proto, is_train):
+    max_seq_length = config.max_target_length if is_train else config.max_eval_target_length
+    # output = [('text', tf.io.VarLenFeature(tf.int64))]
+    output = [('text', tf.io.VarLenFeature(tf.string))]
+    feature_description = dict(output)
+    parsed_example = tf.io.parse_single_example(example_proto, feature_description)
+    parsed_example["text"] = tf.sparse.to_dense(parsed_example["text"])  
+    return parsed_example 
+
+  # max_logging.log(f"Training dataset has: {count_records_dataset(train_ds)} entries" )
+  parse_example_partial = functools.partial(parse_example, is_train=True)
+  train_ds = train_ds.map(parse_example_partial) 
+  # shard the dataset as soon as it is loaded
+  train_ds = train_ds.shard(num_shards = jax.process_count(), index = jax.process_index())
+  train_ds = _tfds_data_processing.normalize_features(train_ds)
+
+  # Evaluation dataset.
+  if config.file_pattern_for_eval_data:
+    max_logging.log(f"Trying to read eval data from path: {config.file_pattern_for_eval_data}")
+    # eval_ds = tf.data.TFRecordDataset.list_files(config.file_pattern_for_eval_data) 
+    # eval_ds = eval_ds.map(tf.data.TFRecordDataset)
+    # eval_ds = tf.data.TFRecordDataset(config.file_pattern_for_eval_data)
+    eval_ds = tf.data.TFRecordDataset(tf.data.Dataset.list_files(config.file_pattern_for_eval_data))
+    # max_logging.log(f"Eval dataset has: {count_records_dataset(eval_ds)} entries")
+    parse_example_partial = functools.partial(parse_example, is_train=False)
+    eval_ds = eval_ds.map(parse_example_partial)
+    eval_ds = eval_ds.shard(num_shards = jax.process_count(), index = jax.process_index())
+    eval_ds = _tfds_data_processing.normalize_features(eval_ds)
+  else:
+    eval_ds = train_ds
+    max_logging.log(f"Reusing training dataset as eval dataset")
+
+  return train_ds, eval_ds
+
+def make_converted_jsonl_train_iterator_and_tokenizer(config, mesh, add_bos, add_eos):
+  """ Make train iterator and tokenizer for converted_jsonl dataset"""
+
+  read_config = tfds.ReadConfig(
+    shuffle_seed = config.data_shuffle_seed,
+  )
+  # train_ds, eval_ds = get_datasets(
+  train_ds, eval_ds = get_converted_jsonl_datasets(
+    config=config,
+    read_config = read_config,
+  )
+
+  sp_tokenizer = get_tokenizer(config.tokenizer_path, add_bos, add_eos)
+  train_iter, _, _ = _tfds_data_processing.preprocess_dataset(
+    config,
+    mesh,
+    train_ds, eval_ds, sp_tokenizer,
+    data_shuffle_seed = config.data_shuffle_seed,
+  )
+  return train_iter, None, sp_tokenizer
 
 def make_c4_train_iterator_and_tokenizer(config, mesh, add_bos, add_eos):
   """ Make train iterator and tokenizer for C4 dataset"""
@@ -131,6 +204,8 @@ def create_data_iterator_with_tokenizer(config, mesh, add_bos = True, add_eos = 
   elif config.dataset_type == "c4_mlperf":
     print("Overwrite both add_bos and add_eos to False")
     return make_c4_mlperf_train_iterator_and_tokenizer(config, mesh, add_bos=False, add_eos=False)
+  elif config.dataset_type == "converted_jsonl":
+    return make_converted_jsonl_train_iterator_and_tokenizer(config, mesh, add_bos, add_eos)
   else:
     assert False, "dataset type not implemented"
 
