d27905d506dc0873f8b123e7d72fcf3da06d8924
michelle-yooh <yooh@google.com>
2024-03-04 18:27:58 +0000

    Add llama2-7b flash script

---
+++ llama2-7b-flash.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+set -e
+set -u
+set -o pipefail
+
+set_nccl_gpudirect_tcpx_specific_configuration() {
+  if [[ "$USE_GPUDIRECT_TCPX" == "yes" ]]; then
+    echo "Using GPUDirect-TCPX"
+    export NCCL_CROSS_NIC=0
+    export NCCL_ALGO=Ring
+    export NCCL_PROTO=Simple
+    export NCCL_DEBUG=INFO
+    export NCCL_NET_GDR_LEVEL=PIX
+    export NCCL_P2P_PXN_LEVEL=0
+    export NCCL_DEBUG_SUBSYS=INIT,GRAPH,ENV,TUNING,NET,VERSION
+    export LD_LIBRARY_PATH="/usr/local/tcpx/lib64"
+    export NCCL_GPUDIRECTTCPX_FORCE_ACK=0
+    export NCCL_GPUDIRECTTCPX_TX_COMPLETION_NANOSLEEP=1000
+    export NCCL_DYNAMIC_CHUNK_SIZE=524288
+    export NCCL_P2P_NET_CHUNKSIZE=524288
+    export NCCL_P2P_PCI_CHUNKSIZE=524288
+    export NCCL_P2P_NVL_CHUNKSIZE=1048576
+    export NCCL_NSOCKS_PERTHREAD=4
+    export NCCL_SOCKET_NTHREADS=1
+    export NCCL_MAX_NCHANNELS=12
+    export NCCL_MIN_NCHANNELS=12
+    export NCCL_GPUDIRECTTCPX_PROGRAM_FLOW_STEERING_WAIT_MICROS=1000000
+    export NCCL_SOCKET_IFNAME=eth0
+    export NCCL_GPUDIRECTTCPX_TX_BINDINGS="eth1:8-21,112-125;eth2:8-21,112-125;eth3:60-73,164-177;eth4:60-73,164-177"
+    export NCCL_GPUDIRECTTCPX_RX_BINDINGS="eth1:22-35,124-139;eth2:22-35,124-139;eth3:74-87,178-191;eth4:74-87,178-191"
+    export NCCL_GPUDIRECTTCPX_SOCKET_IFNAME=eth1,eth2,eth3,eth4
+    export NCCL_GPUDIRECTTCPX_CTRL_DEV=eth0
+    export NCCL_NVLS_ENABLE=0
+  else
+    echo "NOT using TCPX"
+  fi
+}
+
+
+set_nccl_gpudirect_tcpx_specific_configuration
+
+echo "LD_LIBRARY_PATH ${LD_LIBRARY_PATH}"
+
+# HLO dump
+export XLA_FLAGS="--xla_dump_to=/tmp/xladump --xla_gpu_enable_latency_hiding_scheduler=true --xla_gpu_enable_async_all_gather=true --xla_gpu_enable_async_reduce_scatter=true --xla_gpu_enable_triton_gemm=false
+                --xla_gpu_simplify_all_fp_conversions --xla_gpu_graph_level=0 --xla_gpu_enable_async_all_reduce=true --xla_gpu_enable_highest_priority_async_stream=true
+                --xla_gpu_all_reduce_combine_threshold_bytes=8589934592 --xla_gpu_all_gather_combine_threshold_bytes=8589934592 --xla_gpu_reduce_scatter_combine_threshold_bytes=8589934592
+                --xla_gpu_enable_pipelined_all_gather=true --xla_gpu_enable_pipelined_reduce_scatter=true --xla_gpu_enable_pipelined_all_reduce=true --xla_gpu_enable_while_loop_double_buffering=true
+                --xla_gpu_enable_triton_softmax_fusion=false --xla_gpu_enable_all_gather_combine_by_dim=false --xla_gpu_enable_reduce_scatter_combine_by_dim=false
+                --xla_disable_hlo_passes=rematerialization"
+
+python3 MaxText/train.py MaxText/configs/base.yml dataset_path=gs://maxtext-dataset \
+  load_parameters_path=gs://maxtext-llama/test/yooh-2024-0123-1440/decode-ckpt-maxtext/0/default \
+  run_name=runner_$(date +%Y-%m-%d-%H-%M) model_name='llama2-7b' tokenizer_path=gs://maxtext-llama/llama2-7b/tokenizer.llama2 \
+  attention=cudnn_flash_te async_checkpointing=False base_output_directory=gs://runner-maxtext-logs steps=30 hardware=gpu
