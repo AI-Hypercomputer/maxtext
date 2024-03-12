e80cd9f1dce45626b113b84be5eaf33430f90198
priyanka-ganesha <prii@google.com>
2024-03-12 10:38:14 -0700

    made changes

--- end_to_end/cpu/8b.sh
+++ end_to_end/cpu/8b.sh
@@ -1,5 +1,5 @@
-echo "Running 16b.sh"
-# 16B parameter model inference.
+echo "Running 8b.sh"
+# 8B parameter model inference.
 # This config will work out of the box for a single CPU VM.
 #
 # Command Flags:
@@ -8,7 +8,7 @@ echo "Running 16b.sh"
 # RUN_NAME (Required, unless run_name is already set in base.yml)
 #
 # Example to invoke this script:
-# bash MaxText/configs/cpu/16b.sh RUN_NAME="<your_run_name>" OUTPUT_PATH="gs://<your_output_path>" DATASET_PATH="gs://<your_dataset_path>"
+# bash MaxText/configs/cpu/8b.sh RUN_NAME="<your_run_name>" OUTPUT_PATH="gs://<your_output_path>" DATASET_PATH="gs://<your_dataset_path>"
 
 
 # Stop execution if any command exits with error
@@ -17,12 +17,11 @@ set -e
 # Set environment variables
 for ARGUMENT in "$@"; do
     IFS='=' read -r KEY VALUE <<< "$ARGUMENT"
-    export "$KEY"="$VALUE"
+    export M_"$KEY"="$VALUE"
 done
 
 # Decode
-python3 MaxText/decode.py MaxText/configs/base.yml run_name=$RUN_NAME\
+python3 MaxText/decode.py MaxText/configs/base.yml\
     per_device_batch_size=1 enable_checkpointing=false\
-    enable_profiler=false global_parameter_scale=16\
-    max_target_length=128 base_output_directory=$OUTPUT_PATH\
-    dataset_path=$DATASET_PATH attention='dot_product' add_eos=false\
+    enable_profiler=false global_parameter_scale=8\
+    max_target_length=128 attention='dot_product' add_eos=false\
