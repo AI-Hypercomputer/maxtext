85d72c6388a797f2c46013672900d955c6425c84
Pate Motter <patemotter@google.com>
2024-03-12 08:07:21 +0000

    Fix.

--- MaxText/experimental_decode_microbenchmark.py
+++ MaxText/experimental_decode_microbenchmark.py
@@ -69,10 +69,10 @@ def prefill_benchmark_loop(engine, decode_state, params, tokens, true_length, st
     decode_state = engine.insert(prefill_result, decode_state, slot=int(slot))
   jax.block_until_ready(decode_state)
 
-def prefill_benchmark(config, engine, params, tokens, true_length, steps=10): 
+def prefill_benchmark(config, engine, params, tokens, true_length, steps=10, num_model_params=None): 
   decode_state = engine.init_decode_state()
-  _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
-  num_model_params, model_size, _ = summarize_pytree_data(params, name="Params")
+  if num_model_params == None:
+    num_model_params, _ = summarize_pytree_data(params, name="Params")
 
   prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
   decode_state = engine.insert(prefill_result, decode_state, slot=0)
@@ -81,12 +81,14 @@ def prefill_benchmark(config, engine, params, tokens, true_length, steps=10):
   time_in_s = prefill_benchmark_loop(engine, decode_state, params, tokens, true_length, config.steps)
 
   prefill_average_ms = 1000 * time_in_s / config.steps
-  print(f"Prefill took on average {prefill_average_ms:.2f} milliseconds")
   total_prefill_tflops, _, _ = maxtext_utils.calculate_tflops_prefill(num_model_params, tokens.size, config)
-  tflops_per_sec_per_device = total_prefill_tflops / jax.device_count() / prefill_average_ms / 1000.
-  print(f"TFLOP/s/device achieved: {tflops_per_sec_per_device}")
-  return {"prefill_in_ms": prefill_average_ms, 
-          "prefill_tflops": total_prefill_tflops, 
+  tflops_per_sec_per_device = total_prefill_tflops / jax.device_count() / prefill_average_ms * 1000.
+  print(f"Prefill results:\n"
+        f"\tPrefill step average time: {prefill_average_ms:.2f}ms\n"
+        f"\tPrefill total TFLOPs: {total_prefill_tflops}\n"
+        f"\tPrefill TFLOPs/sec/device: {tflops_per_sec_per_device}\n")
+  return {"prefill_time_in_ms": prefill_average_ms, 
+          "prefill_total_tflops": total_prefill_tflops, 
           "prefill_tflops_per_sec_per_device": tflops_per_sec_per_device}
 
 @profile
@@ -101,10 +103,12 @@ def ar_benchmark_loop(engine, decode_state, params, tokens, true_length, global_
   jax.block_until_ready(decode_state)
 
 
-def ar_benchmark(config, engine, params, tokens, true_length, steps=10): 
+def ar_benchmark(config, engine, params, tokens, true_length, steps=10, cache_size=None, model_size=None): 
   decode_state = engine.init_decode_state()
-  _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
-  num_model_params, model_size, _ = summarize_pytree_data(params, name="Params")
+  if cache_size == None:
+    _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
+  if model_size == None:
+    _, model_size, _ = summarize_pytree_data(params, name="Params")
   global_batch_size = jax.device_count() * config.per_device_batch_size
 
   # Init
@@ -123,33 +127,42 @@ def ar_benchmark(config, engine, params, tokens, true_length, steps=10):
 
   GB_per_step_per_device = (model_size + cache_size) / 2**30 / jax.device_count()
   bw_per_device = GB_per_step_per_device/seconds_per_step
-  print(f"AR step average time: {ar_average_ms:.2f}ms\n"
-        f"AR global batch size: {global_batch_size}\n"
-        f"AR throughput: {total_throughput:.2f} tokens/second\n"
-        f"AR memory bandwidth per device: {bw_per_device:.2f} GB/s")
+  print(f"AutoRegressive results:\n"
+        f"\tAR step average time: {ar_average_ms:.2f}ms\n"
+        f"\tAR global batch size: {global_batch_size}\n"
+        f"\tAR throughput: {total_throughput:.2f} tokens/second\n"
+        f"\tAR memory bandwidth per device: {bw_per_device:.2f} GB/s")
   return {"ar_step_in_ms": ar_average_ms, 
-          "global_batch_size": global_batch_size, 
-          "total_throughput_tokens_per_second": total_throughput,
-          "device_bandwidth_GB_per_second": bw_per_device}
+          "ar_global_batch_size": global_batch_size, 
+          "ar_total_throughput_tokens_per_second": total_throughput,
+          "ar_device_bandwidth_GB_per_second": bw_per_device}
 
 def main(config):
   engine = myengine.TestEngine(config)
   params = engine.load_params()
-  prefill_lengths = [512, 1024] #[128, 256#, 512, 1024, 2048, 4096]
+  prefill_lengths = [512]#, 1024] #[128, 256#, 512, 1024, 2048, 4096]
   num_steps = 10
   text = config.prompt
   metadata = engine.get_tokenizer()
   vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
-  results = {"config": {}}
+
+  decode_state = engine.init_decode_state()
+  _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
+  num_model_params, model_size, _ = summarize_pytree_data(params, name="Params")
+  results = {"config": {}, 
+             "sizes": {
+                "model_size_in_GB": model_size / 2**30,
+                "cache_size_in_GB": cache_size / 2**30,
+                "num_model_params_in_billions": num_model_params / 1e9,
+             }}
   for k, v in dict(config.get_keys()).items():
     results["config"][k] = str(v) if k == "dtype" else v
 
   for prefill_length in prefill_lengths:
     tokens, true_length = token_utils.tokenize_and_pad(text, vocab, is_bos=True, prefill_lengths=[prefill_length])
     print(f"Prompt tokenized to size {tokens.size}")
-
-    prefill_results = prefill_benchmark(config, engine, params, tokens, true_length, num_steps)
-    ar_results = ar_benchmark(config, engine, params, tokens, true_length, num_steps)
+    prefill_results = prefill_benchmark(config, engine, params, tokens, true_length, num_steps, num_model_params)
+    ar_results = ar_benchmark(config, engine, params, tokens, true_length, num_steps, cache_size, model_size)
     prefill_results.update(ar_results)
     results[prefill_length] = prefill_results
 
