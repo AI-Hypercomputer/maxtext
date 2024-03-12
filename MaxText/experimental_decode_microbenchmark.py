24cc6b121f6712699f92e2a1265fbde3da371eff
Pate Motter <patemotter@google.com>
2024-03-12 07:30:11 +0000

    Working prefill and decode w/ json output.

--- MaxText/experimental_decode_microbenchmark.py
+++ MaxText/experimental_decode_microbenchmark.py
@@ -14,19 +14,12 @@ If we then insert that and run three generation steps, we should see
 266 + [266] /2  = 399
 266 + [266, 399] /2 = 598
 I.e. ['Ċ', 'Ə', 'ɖ'] when converted back with chr()
-
-Prompt tokenized to size 1024
-Number of params=8.538 billion, memory usage=15.903GB, bytes per param=2.000
-Number of cache entries=3.759 billion, memory usage=7.002GB, bytes per cache=2.000
-number parameters: 8.538 billion
-Per prefill step, total TFLOPs will be 17.73, split as 98.64% learnable weight flops and 1.36% attention flops
-Prefill took on average 62.19 milliseconds
-TFLOP/s/device achieved: 35.63
 """
 import datetime
 import sys
 import jax
 import jax.numpy as jnp
+import json
 import numpy as np
 
 import max_utils
@@ -54,8 +47,8 @@ def profile(func):
     return (end - start).total_seconds()
   return wrapper
 
-def print_objects():
-  print(f"Objects {len(gc.get_objects())}")
+# def print_objects():
+#   print(f"Objects {len(gc.get_objects())}")
 
 def summarize_pytree_data(params, name="Params", log=True):
   num_params, total_param_size, avg_param_size = max_utils.summarize_size_from_pytree(params)
@@ -80,69 +73,88 @@ def prefill_benchmark(config, engine, params, tokens, true_length, steps=10):
   decode_state = engine.init_decode_state()
   _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
   num_model_params, model_size, _ = summarize_pytree_data(params, name="Params")
+
   prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
   decode_state = engine.insert(prefill_result, decode_state, slot=0)
   jax.block_until_ready(decode_state)
 
-  time_in_secs = prefill_benchmark_loop(engine, decode_state, params, tokens, true_length, config.steps)
+  time_in_s = prefill_benchmark_loop(engine, decode_state, params, tokens, true_length, config.steps)
 
-  prefill_average_ms = 1000 * time_in_secs / config.steps
+  prefill_average_ms = 1000 * time_in_s / config.steps
   print(f"Prefill took on average {prefill_average_ms:.2f} milliseconds")
   total_prefill_tflops, _, _ = maxtext_utils.calculate_tflops_prefill(num_model_params, tokens.size, config)
-  print(f"TFLOP/s/device achieved: {total_prefill_tflops/jax.device_count()/(prefill_average_ms/1000):.2f}")
-  return prefill_average_ms, total_prefill_tflops
+  tflops_per_sec_per_device = total_prefill_tflops / jax.device_count() / prefill_average_ms / 1000.
+  print(f"TFLOP/s/device achieved: {tflops_per_sec_per_device}")
+  return {"prefill_in_ms": prefill_average_ms, 
+          "prefill_tflops": total_prefill_tflops, 
+          "prefill_tflops_per_sec_per_device": tflops_per_sec_per_device}
+
+@profile
+def ar_benchmark_loop(engine, decode_state, params, tokens, true_length, global_batch_size, steps=10):
+  for i in range(config.steps):
+    slot = int(i % (global_batch_size))
+    # print(f"STEP {i} {slot}")
+    prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
+    decode_state = engine.insert(prefill_result, decode_state, slot=slot)
+    decode_state, sampled_tokens = engine.generate(params, decode_state)
+    # print_objects()
+  jax.block_until_ready(decode_state)
+
 
 def ar_benchmark(config, engine, params, tokens, true_length, steps=10): 
   decode_state = engine.init_decode_state()
   _, cache_size, _ = summarize_pytree_data(decode_state['cache'], name="Cache")
   num_model_params, model_size, _ = summarize_pytree_data(params, name="Params")
+  global_batch_size = jax.device_count() * config.per_device_batch_size
+
+  # Init
   prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
   decode_state = engine.insert(prefill_result, decode_state, slot=0)
   jax.block_until_ready(decode_state)
+
+  # Warmup
   decode_state, sampled_tokens = engine.generate(params, decode_state)
   jax.block_until_ready(decode_state)
 
+  time_in_s = ar_benchmark_loop(engine, decode_state, params, tokens, true_length, global_batch_size, steps=steps)
+  seconds_per_step = time_in_s / config.steps
+  ar_average_ms = seconds_per_step*1000
+  total_throughput = jax.device_count() * config.per_device_batch_size / seconds_per_step
+
+  GB_per_step_per_device = (model_size + cache_size) / 2**30 / jax.device_count()
+  bw_per_device = GB_per_step_per_device/seconds_per_step
+  print(f"AR step average time: {ar_average_ms:.2f}ms\n"
+        f"AR global batch size: {global_batch_size}\n"
+        f"AR throughput: {total_throughput:.2f} tokens/second\n"
+        f"AR memory bandwidth per device: {bw_per_device:.2f} GB/s")
+  return {"ar_step_in_ms": ar_average_ms, 
+          "global_batch_size": global_batch_size, 
+          "total_throughput_tokens_per_second": total_throughput,
+          "device_bandwidth_GB_per_second": bw_per_device}
+
 def main(config):
-  benchmark = "prefill"
   engine = myengine.TestEngine(config)
   params = engine.load_params()
-  prefill_lengths = [1024]
-
+  prefill_lengths = [512, 1024] #[128, 256#, 512, 1024, 2048, 4096]
+  num_steps = 10
   text = config.prompt
   metadata = engine.get_tokenizer()
   vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
-  tokenizer = vocab.tokenizer
-  tokens, true_length = token_utils.tokenize_and_pad(text, vocab, is_bos=True, prefill_lengths=prefill_lengths)
-  print(f"Prompt tokenized to size {tokens.size}")
+  results = {"config": {}}
+  for k, v in dict(config.get_keys()).items():
+    results["config"][k] = str(v) if k == "dtype" else v
 
-  if benchmark == "prefill":
-    prefill_avg_ms, prefill_total_tflops = prefill_benchmark(config, engine, params, tokens, true_length, steps=10)
-  else:
+  for prefill_length in prefill_lengths:
+    tokens, true_length = token_utils.tokenize_and_pad(text, vocab, is_bos=True, prefill_lengths=[prefill_length])
+    print(f"Prompt tokenized to size {tokens.size}")
 
-    max_utils.activate_profiler(config)
-    start = datetime.datetime.now()
+    prefill_results = prefill_benchmark(config, engine, params, tokens, true_length, num_steps)
+    ar_results = ar_benchmark(config, engine, params, tokens, true_length, num_steps)
+    prefill_results.update(ar_results)
+    results[prefill_length] = prefill_results
 
-    global_batch_size = jax.device_count() * config.per_device_batch_size
-    for i in range(config.steps):
-      slot = int(i % (global_batch_size))
-      print(f"STEP {i} {slot}")
-
-      prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
-      decode_state = engine.insert(prefill_result, decode_state, slot=slot)
-      decode_state, sampled_tokens = engine.generate(params, decode_state)
-      print_objects()
-
-    jax.block_until_ready(decode_state)
-    end = datetime.datetime.now()
-    max_utils.deactivate_profiler(config)
-
-    seconds_per_step = (end-start).total_seconds() / config.steps
-    total_tok_per_sec = (jax.device_count() * config.per_device_batch_size) / seconds_per_step
-
-    GB_per_step_per_device = (model_size+cache_size)/(2**30 * jax.device_count())
-    print(f"Each tick took {seconds_per_step*1000:.2f}ms, with a global_batch_size {global_batch_size}"
-          f" so {total_tok_per_sec:.2f} tokens/second" )
-    print(f"Per device memory bandwidth {GB_per_step_per_device/seconds_per_step:.2f} GB/s")
+  with open("benchmark_results.json", "w") as f:
+    json.dump(results, f, indent=2)
 
 
 if __name__ == "__main__":
