e6a48e2dbd04e96c55a1324ea6bbe8a540b8508b
Pate Motter <patemotter@google.com>
2024-03-13 00:27:55 +0000

    Adds microbenchmarking and uses standard 1e9 for memory calcs.

--- MaxText/inference_scratch/analyze_sharegpt.py
+++ MaxText/inference_scratch/analyze_sharegpt.py
@@ -12,77 +12,97 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import json
 
-CUTOFF_INPUT = 1024
-CUTOFF_OUTPUT = 1024
+PREFILL_BUCKET_SIZE_TO_MS = {64: 9.174, 128: 11.087, 256: 18.468, 512: 29.128, 1024: 58.386}
+SYSTEM_TIME_PER_DECODE_TOKEN_MS = 0.32591875
+MAX_INPUT_TOKENS = 1024
+MAX_OUTPUT_TOKENS = 1024
 
-prefill_bucket_size_to_ms = {64 : 14.02, 128:18.29, 256:23.59, 512:35.28, 1024: 60.28}
-
-system_time_per_decode_token_ms = 33.67/96 
-
-def next_power_of_2(x):  
-    return 1 if x == 0 else 2**(x - 1).bit_length()
+def next_power_of_2(x):
+  return 1 if x == 0 else 2**(x - 1).bit_length()
 
 def tokens_in_input_str(s):
-    return_val =  int(1.3 * len(s.split()))
-    #print(f"{s=} -> {return_val=}")
-    return return_val
+  return_val =  int(1.3 * len(s.split()))
+  return return_val
 
-convo_numbers = []
-loaded_share_gpt = json.load(open('/home/rwitten/ShareGPT_V3_unfiltered_cleaned_split.json', 'r'))
-for example in loaded_share_gpt:
+def get_prefill_and_generate_times(filename=""):
+  if filename == "":
+    return PREFILL_BUCKET_SIZE_TO_MS, SYSTEM_TIME_PER_DECODE_TOKEN_MS
+
+  prefill_bucket_size_to_ms = {}
+  with open(filename, "r") as f:
+    microbenchmark_results = json.load(f)
+  for k, v in microbenchmark_results["Prefill"].items():
+    prefill_bucket_size_to_ms[int(k)] = round(v["prefill_time_in_ms"], 3)
+
+  return prefill_bucket_size_to_ms, microbenchmark_results['AutoRegressive']['ar_step_in_ms_per_seq']
+
+def get_conversations_from_file(filename, max_input_tokens, max_output_tokens):
+  convo_token_numbers = []
+  with open(filename, 'r') as f:
+    loaded_share_gpt = json.load(f)
+  for example in loaded_share_gpt:
     if len(example['conversations']) < 2:
-        continue
-    input_tokens = tokens_in_input_str(example['conversations'][0]['value'])
-    output_tokens = tokens_in_input_str(example['conversations'][1]['value'])
-    convo_numbers.append( (input_tokens, output_tokens))
+      continue
+    num_input_tokens = tokens_in_input_str(example['conversations'][0]['value'])
+    num_output_tokens = tokens_in_input_str(example['conversations'][1]['value'])
+    convo_token_numbers.append((num_input_tokens, num_output_tokens))
 
-num_convos = len(convo_numbers)
-kept_convos = [c for c in convo_numbers if c[0] <= CUTOFF_INPUT and c[1] <= CUTOFF_OUTPUT]
+  num_convos = len(convo_token_numbers)
+  kept_convos = [c for c in convo_token_numbers if c[0] <= max_input_tokens and c[1] <= max_output_tokens]
 
-mean_input = sum([c[0] for c in kept_convos]) / len(kept_convos)
-mean_output = sum([c[1] for c in kept_convos]) / len(kept_convos)
+  mean_input = sum(c[0] for c in kept_convos) / len(kept_convos)
+  mean_output = sum(c[1] for c in kept_convos) / len(kept_convos)
 
-print(f"Total {num_convos=} but only kept {kept_convos=}. Out of kept, {mean_input=}, {mean_output=}")
+  print(f"Kept {len(kept_convos)} of {num_convos} total convos. {len(100*kept_convos)/num_convos:.3f}%")
+  print(f"Out of kept convos, mean input tokens: {mean_input:.3f}, mean output tokens: {mean_output:.3f}")
+  return kept_convos
 
-total_prefill_system_ms = 0
-total_generate_system_ms = 0
 
-total_system_output_tokens = 0
-for convo in kept_convos:
+def compute_times(convos, prefill_bucket_size_to_ms, system_time_per_decode_token_ms, verbose=False):
+  total_prefill_system_ms = 0
+  total_generate_system_ms = 0
+  for convo in convos:
     input_tok, output_tok = convo
-    bucket = max(128,next_power_of_2(input_tok))
-    generate_system_ms = output_tok * system_time_per_decode_token_ms 
+    bucket = max(128, next_power_of_2(input_tok))
+    generate_system_ms = output_tok * system_time_per_decode_token_ms
     prefill_system_ms = prefill_bucket_size_to_ms[bucket]
-
-    print(f"{convo=} {bucket=}, {prefill_system_ms=:.2f}, {generate_system_ms=:.2f}")
-
     total_prefill_system_ms += prefill_system_ms
     total_generate_system_ms += generate_system_ms
+    if verbose:
+      print(f"{convo} {bucket}, {prefill_system_ms:.2f}, {generate_system_ms:.2f}")
 
-total_time_ms = total_prefill_system_ms + total_generate_system_ms
-input_tokens = sum([c[0] for c in kept_convos])
+  total_prefill_time_seconds = total_prefill_system_ms / 1000
+  total_generate_time_seconds = total_generate_system_ms / 1000
+  total_time_seconds = total_prefill_time_seconds + total_generate_time_seconds
 
-output_tokens = sum([c[1] for c in kept_convos])
-print(f"Output tokens {output_tokens} in {total_time_ms/1000:.2f} seconds, for {output_tokens/(total_time_ms/1000):.2f} out tok/s")
-
-total_prefill_sec = total_prefill_system_ms/1000
-total_generate_sec = total_generate_system_ms/1000
-
-print(f"Total time {total_time_ms/1000:.2f} seconds, split {total_prefill_sec=:.2f} seconds and {total_generate_sec=:.2f} seconds")
-
-idealized_prefill_sec = 1.1 * input_tokens/1024 * prefill_bucket_size_to_ms[1024] / 1000
-
-prefill_savings_sec = total_prefill_sec-idealized_prefill_sec
+  print(f"\nTotal time {total_time_seconds:.3f} seconds: "
+        f"\n\tPrefill time: {total_prefill_time_seconds:.3f} seconds"
+        f"\n\tGenerate time: {total_generate_time_seconds:.3f} seconds")
+  return total_time_seconds, total_prefill_time_seconds, total_generate_time_seconds
 
 
-idealized_generate_sec = total_generate_sec/2 # (Roughly save 75% on KV cache high cost on the rest)
-generate_savings_sec = total_generate_sec - idealized_generate_sec
+def get_num_tokens_in_convos(convos):
+  num_input_tokens = sum(c[0] for c in convos)
+  num_output_tokens = sum(c[1] for c in convos)
+  return num_input_tokens, num_output_tokens
 
-print(f"we think prefill will take {total_prefill_sec=:.2f}, we could get it to {idealized_prefill_sec=:.2f} so we'd save {prefill_savings_sec=:.2f} seconds ")
-print(f"with sparsity we could go from  {total_generate_sec=:.2f}, we could get it to {idealized_generate_sec=:.2f} so we'd save {generate_savings_sec=:.2f} seconds ")
 
-idealized_overall_time = idealized_generate_sec + idealized_prefill_sec
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('convo_file', type=str,
+                      help='a json file containing conversations')
+  parser.add_argument('-t', '--mb_timing_file', type=str, default="",
+                      help='a json file containing microbenchmark timing results')
+  parser.add_argument('-v', '--verbose', action="store_true")
+  args = parser.parse_args()
 
-print(f"Idealized out tokens {output_tokens} in {idealized_overall_time:.2f} seconds, for {output_tokens/idealized_overall_time:.2f} out tok/s")
+  kept_convos = get_conversations_from_file(args.convo_file, MAX_INPUT_TOKENS, MAX_OUTPUT_TOKENS)
+  total_input_tokens, total_output_tokens = get_num_tokens_in_convos(kept_convos)
+  prefill_time_ms_buckets, generate_time_ms = get_prefill_and_generate_times(filename=args.mb_timing_file)
+  total_time_seconds, _, _ = compute_times(kept_convos, prefill_time_ms_buckets, generate_time_ms, args.verbose)
+
+  print(f"Output {total_output_tokens} tokens in {total_time_seconds:.3f} seconds "
+        f"= {total_output_tokens/total_time_seconds:.3f} out tok/s")
