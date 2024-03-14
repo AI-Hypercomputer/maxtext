f8512280ab68ba22eb4d9369ae1bca8debd2fc7d
Ran Ran <ranran@google.com>
2024-03-13 21:04:24 -0700

    Copybara import of the project:
    --
    f4d3343cba8404835702b34f99437cfc0a826fe7 by Ran Ran <ranran@google.com>:
    
    Fix Mistral training OOM
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/maxtext/pull/513 from RissyRan:fix_mistral f4d3343cba8404835702b34f99437cfc0a826fe7
    PiperOrigin-RevId: 615641823

--- end_to_end/test_mistral.sh
+++ end_to_end/test_mistral.sh
@@ -19,4 +19,4 @@ python3 MaxText/llama_or_mistral_ckpt.py --base-model-path /tmp/mistral-7B-v0.1 
 python3 MaxText/decode.py MaxText/configs/base.yml load_parameters_path=gs://maxtext-mistral/test/${idx}/decode-ckpt-maxtext/0/items run_name=runner_direct_${idx} per_device_batch_size=1 model_name='mistral-7b' tokenizer_path=gs://maxtext-external/mistral-7B-v0.1/tokenizer.mistral ici_tensor_parallelism=4 max_prefill_predict_length=4 max_target_length=16 prompt="I love to" autoregressive_decode_assert="read. I love to read about the Bible. I love" attention=dot_product
 
 # Training
-python3 MaxText/train.py MaxText/configs/base.yml load_parameters_path=gs://maxtext-mistral/test/${idx}/decode-ckpt-maxtext/0/items run_name=runner_${idx} per_device_batch_size=1 model_name='mistral-7b' ici_tensor_parallelism=4 steps=10
+python3 MaxText/train.py MaxText/configs/base.yml load_parameters_path=gs://maxtext-mistral/test/${idx}/decode-ckpt-maxtext/0/items run_name=runner_${idx} per_device_batch_size=1 model_name='mistral-7b' ici_tensor_parallelism=4 steps=10 max_target_length=1024 tokenizer_path=gs://maxtext-external/mistral-7B-v0.1/tokenizer.mistral
