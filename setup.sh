cf93279618d3e8e7da96e85b740e31551693b1c5
yangyuwei <yangyuwei@google.com>
2024-03-14 18:46:56 -0700

    Install TransformerEngine needed for FlashAttention.

--- setup.sh
+++ setup.sh
@@ -140,6 +140,8 @@ if [[ "$MODE" == "stable" || ! -v MODE ]]; then
             echo "Installing stable jax, jaxlib, libtpu for NVIDIA gpu"
             pip3 install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
         fi
+        export NVTE_FRAMEWORK=jax
+        pip3 install git+https://github.com/NVIDIA/TransformerEngine.git@stable
     fi
 elif [[ $MODE == "nightly" ]]; then
 # Nightly mode
