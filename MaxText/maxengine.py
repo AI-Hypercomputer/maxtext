193d104de0b8f08632c174b3c30c28bd0ee720c8
Junwei Yang <jwyang@google.com>
2024-05-01 22:57:58 +0000

    Remove cached_ar_key/cached_ar_value in prefill results to save memory.

--- MaxText/maxengine.py
+++ MaxText/maxengine.py
@@ -176,12 +176,12 @@ class MaxEngine(engine_api.Engine):
     # delete the "cached_ar_key" and "cached_ar_value" keys would cause error in insert() function
     # so set cached_ar_key/cached_ar_value to dummy shape (1,) value to save memory
     for i in range(self.config.num_decoder_layers):
-      if "layers_{}".format(i) in new_vars["cache"]["decoder"]:
-        new_vars["cache"]["decoder"]["layers_{}".format(i)]["self_attention"]["AttentionOp_0"]["cached_ar_key"] = jnp.zeros(
+      if f"layers_{i}" in new_vars["cache"]["decoder"]:
+        new_vars["cache"]["decoder"][f"layers_{i}"]["self_attention"]["AttentionOp_0"]["cached_ar_key"] = jnp.zeros(
             (1), dtype=jnp.int32
         )
-        new_vars["cache"]["decoder"]["layers_{}".format(i)]["self_attention"]["AttentionOp_0"]["cached_ar_value"] = (
-            jnp.zeros((1), dtype=jnp.int32)
+        new_vars["cache"]["decoder"][f"layers_{i}"]["self_attention"]["AttentionOp_0"]["cached_ar_value"] = jnp.zeros(
+            (1), dtype=jnp.int32
         )
 
     return {
