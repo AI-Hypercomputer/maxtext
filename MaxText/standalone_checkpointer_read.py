4d6a9774b104223b3a61122e8ee24eded87b002e
Divya Rawal <divyarawal@google.com>
2024-05-10 17:41:56 +0000

    Updates write and read standalone binaries

--- MaxText/standalone_checkpointer_read.py
+++ MaxText/standalone_checkpointer_read.py
@@ -1,18 +1,18 @@
 """
- Copyright 2023 Google LLC
+Copyright 2023 Google LLC
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-      https://www.apache.org/licenses/LICENSE-2.0
+     https://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- """
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 # pylint: disable=g-bad-todo, abstract-method, consider-using-with, ungrouped-imports
 """Standalone checkpointer - only saves and restores checkpoints at regular intervals, accesses storage needs."""
@@ -22,23 +22,26 @@
 
 import datetime
 import os
+import time
+import sys
 
 from typing import Sequence
 from absl import app
 from flax.linen import partitioning as nn_partitioning
 import jax
+from jax import numpy as jnp
 import numpy as np
-import time
 
 import checkpointing
 import max_utils
 import max_logging
 import pyconfig
-from train import setup_mesh_and_model, validate_train_config
+from train import setup_mesh_and_model, get_first_step, validate_train_config, save_checkpoint
 
 from layers import models
 
 Transformer = models.Transformer
+
 
 def checkpoint_loop(config, state=None):
   """Main Checkpointing loop.
@@ -49,33 +52,30 @@ def checkpoint_loop(config, state=None):
     ckpt_path:
   Returns:
   """
-  init_rng, writer, checkpoint_manager, mesh, model, _, tx = setup_mesh_and_model(config)
+  init_rng, _, checkpoint_manager, mesh, model, _, tx = setup_mesh_and_model(config)
 
-  unboxed_abstract_state, state_mesh_annotations = max_utils.get_abstract_state(model, tx,
-                                                config, init_rng, mesh, is_training=True)
+  unboxed_abstract_state, _, _ = max_utils.get_abstract_state(model, tx, config, init_rng, mesh, is_training=True)
   ckpt_read_time = []
-  for step in range(config.steps):
-    # A barrier to sync all hosts before starting to restore checkpoint
-    jax.experimental.multihost_utils.sync_global_devices("Barrier before load")
-    checkpoint_load_start = datetime.datetime.now()
-    with nn_partitioning.axis_rules(config.logical_axis_rules):
-      state, _ = checkpointing.load_state_if_possible(checkpoint_manager,
-                                                  config.load_parameters_path,
-                                                  config.load_full_state_path,
-                                                  unboxed_abstract_state,
-                                                  mesh,
-                                                  state_mesh_annotations)
-    jax.block_until_ready(state)
-    checkpoint_load_end = datetime.datetime.now()
-    if state is not None: # Checkpoint was available for restore
-      time_diff = (checkpoint_load_end-checkpoint_load_start).total_seconds()
+  # A barrier to sync all hosts before starting to restore checkpoint
+  jax.experimental.multihost_utils.sync_global_devices("Barrier before load")
+  checkpoint_load_start = datetime.datetime.now()
+  with nn_partitioning.axis_rules(config.logical_axis_rules):
+    state, _ = checkpointing.load_state_if_possible(
+        checkpoint_manager, None, config.load_parameters_path, config.load_full_state_path, unboxed_abstract_state
+    )
+    if state:
+      state = state["items"]
+
+  jax.block_until_ready(state)
+  checkpoint_load_end = datetime.datetime.now()
+  if state is not None:  # Checkpoint was available for restore
+    if jax.process_index() == 0:
       ckpt_read_time.append([jax.process_index(), step, time_diff])
-      max_logging.log(f"STANDALONE CHECKPOINTER : Checkpoint restored in: "
-                      f"{time_diff}")
-    else: # Checkpoint was unavailable, state needs to be initialized
-      raise Exception("Checkpoint not available")
-    max_logging.log(f"Finished step {step}, sleeping for 20s...")
-    time.sleep(20)
+      max_logging.log(f"STANDALONE CHECKPOINTER : Checkpoint restored in : {checkpoint_load_end - checkpoint_load_start}")
+  else:  # Checkpoint was unavailable, state needs to be initialized
+    state, _, _ = max_utils.setup_training_state(model, None, tx, config, init_rng, mesh, checkpoint_manager)
+  state = add_entropy_to_checkpoint(state)
+  time.sleep(20)
 
   if config.gcs_csv_folder != '':
     max_logging.log("Uploading metrics to GCS")
@@ -87,8 +87,25 @@ def checkpoint_loop(config, state=None):
   max_utils.close_summary_writer(writer)
   return state
 
+
+def add_entropy_to_checkpoint(state):
+  """Introduce randomness in checkpoints. This is useful to simulate real checkpoints, without training.
+  Args:
+    state: Initial state
+  Returns:
+    state: Returns state with entropy added to the optimizer state.
+  """
+  with jax.spmd_mode("allow_all"):
+    opt_0 = state.opt_state[0]
+    opt_0 = opt_0._replace(mu=jax.tree_util.tree_map(lambda k: jnp.cos(1000 * k), state.params))
+    opt_0 = opt_0._replace(nu=jax.tree_util.tree_map(lambda k: jnp.sin(1000 * k), state.params))
+    new_opt = [opt_0] + list(state.opt_state[1:])
+    state = state.replace(opt_state=new_opt)
+    return state
+
+
 def main(argv: Sequence[str]) -> None:
-  jax.config.update('jax_cpu_enable_gloo_collectives', True)
+  jax.config.update("jax_cpu_enable_gloo_collectives", True)
   os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
   pyconfig.initialize(argv)
   config = pyconfig.config
@@ -99,5 +116,7 @@ def main(argv: Sequence[str]) -> None:
   os.environ["TFDS_DATA_DIR"] = config.dataset_path
   checkpoint_loop(config)
 
+
 if __name__ == "__main__":
   app.run(main)
+
