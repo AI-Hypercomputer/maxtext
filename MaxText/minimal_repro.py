33c2b4ead06471df8df8ab1920e65134164defd6
gobbleturk <mattdavidow@google.com>
2024-05-12 20:10:11 +0000

    minimal repro

--- MaxText/minimal_repro.py
+++ MaxText/minimal_repro.py
@@ -3,69 +3,56 @@ import jax
 from jax.experimental import mesh_utils
 from jax.sharding import Mesh
 from jax import numpy as jnp
+from jax.sharding import PartitionSpec as P
 
 
-def create_inputs_and_embed():
-    mesh_axes = ["data", "fsdp", "tensor"]
-    dcn_parallelism = [args.dcn_data_parallelism, args.dcn_fsdp_parallelism, args.dcn_tensor_parallelism]
-    ici_parallelism = [args.ici_data_parallelism, args.ici_fsdp_parallelism, args.ici_tensor_parallelism]
+def simple_repro():
+    mesh_axes = ["axis_1", "axis_2"]
+    
+    dcn_parallelism = [args.dcn_axis_1_parallelism, args.dcn_axis_2_parallelism]
+    ici_parallelism = [args.ici_axis_1_parallelism, args.ici_axis_2_parallelism]
 
-    multislice_env = args.dcn_data_parallelism > 1 or args.dcn_fsdp_parallelism > 1 or args.dcn_tensor_parallelism > 1
-
+    multislice_env = args.dcn_axis_1_parallelism > 1  or args.dcn_axis_2_parallelism > 1
     if multislice_env:
         device_mesh = mesh_utils.create_hybrid_device_mesh(ici_parallelism, dcn_parallelism)
     else:
         device_mesh = mesh_utils.create_device_mesh(ici_parallelism)
     mesh = Mesh(device_mesh, mesh_axes)
+    
+    # Shard over both axes
+    data_sharding = (("axis_1", "axis_2"),)
+    data_pspec = P(*data_sharding)
+    data_pspec_shardings = jax.sharding.NamedSharding(mesh, data_pspec)
 
+    def generate_fully_sharded_data():
+        def generate_data_base():
+            return jax.numpy.zeros((args.global_batch_size), dtype=jax.numpy.int32) # also fails with bf16
+        jit_generate_data = jax.jit(generate_data_base, out_shardings=data_pspec_shardings)
+        return jit_generate_data()
 
-
-    data_shape = (args.global_batch_size, args.sequence_len)
-    data_partition_spec = jax.sharding.PartitionSpec(("data", "fsdp","tensor"),)
-    data_sharding = jax.sharding.NamedSharding(mesh, data_partition_spec)
-
-    # Problem: This will first create the data on one device, the shard it. How can we start with the data sharded?
-    data = jax.numpy.ones(data_shape, dtype=jnp.int32)
-    data = jax.lax.with_sharding_constraint(data, data_sharding)
-
-    # Alternative:
-    # Create the sharded array
-    # data = global_device_array.GlobalDeviceArray.from_callback(
-    #     global_shape=data_shape,
-    #     mesh=mesh,
-    #     distribution_specs=data_partition_spec,
-    #     callback=create_shard
-
-
-
-    embed_shape = (args.vocab_size, args.embed_size)
-    embed_partition_spec = jax.sharding.PartitionSpec("fsdp", "tensor")
-    embed_sharding = jax.sharding.NamedSharding(mesh, embed_partition_spec)
-
-    embed = jax.random.normal(jax.random.PRNGKey(0), embed_shape)
-    embed = jax.lax.with_sharding_constraint(embed, embed_sharding)
-
-    inputs = embed[data]
-    print(f"Print statement to force inputs to be execute {inputs[0][0][0]}", flush=True)
-
+    def dummy_op_on_data(data):
+        return jnp.linalg.norm(data)
+    
+    print("Creating data...", flush=True)
+    data = generate_fully_sharded_data()
+    data.block_until_ready()
+    print("Data created!", flush=True)
+    jit_data_op = jax.jit(dummy_op_on_data, in_shardings=data_pspec_shardings) 
+    norm = jit_data_op(data) 
+    print(f"Data norm of {norm}", flush=True) # Runtime crash, XlaRuntimeError: UNKNOWN
+    # with verbose libtpu logging platforms/asic_sw/driver/2a886c8/common/internal/host_queue.cc:323 is flagged
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='Sharding and size settings')
-    parser.add_argument('--dcn_data_parallelism', type=int, default=1)
-    parser.add_argument('--dcn_fsdp_parallelism', type=int, default=1)
-    parser.add_argument('--dcn_tensor_parallelism', type=int, default=1)
-    parser.add_argument('--ici_data_parallelism', type=int, default=1)
-    parser.add_argument('--ici_fsdp_parallelism', type=int, default=1)
-    parser.add_argument('--ici_tensor_parallelism', type=int, default=1)
+    parser.add_argument('--ici_axis_1_parallelism', type=int, default=1)
+    parser.add_argument('--ici_axis_2_parallelism', type=int, default=1)
+    parser.add_argument('--dcn_axis_1_parallelism', type=int, default=1)
+    parser.add_argument('--dcn_axis_2_parallelism', type=int, default=1)
     parser.add_argument('--global_batch_size', type=int, default=128)
-    parser.add_argument('--sequence_len', type=int, default=1024)
-    parser.add_argument('--vocab_size', type=int, default=256)
-    parser.add_argument('--embed_size', type=int, default=512)
     global args
     args = parser.parse_args()
     
-    create_inputs_and_embed()
-
+    simple_repro()
 
 main()
