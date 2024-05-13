e32cfac110b1fe37f455a56d9420cd0a510f2d60
gobbleturk <mattdavidow@google.com>
2024-05-13 02:17:01 +0000

    Stage first working

--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -319,22 +319,22 @@ def create_device_mesh(config, devices=None):
   multi_slice_env = num_slices > 1
 
   dcn_parallelism = [
+      config.dcn_pipeline_parallelism,
       config.dcn_data_parallelism,
       config.dcn_fsdp_parallelism,
       config.dcn_fsdp_transpose_parallelism,
       config.dcn_sequence_parallelism,
       config.dcn_tensor_parallelism,
-      config.dcn_autoregressive_parallelism,
-      config.dcn_pipeline_parallelism,
+      config.dcn_autoregressive_parallelism,      
   ]
   ici_parallelism = [
+      config.ici_pipeline_parallelism,
       config.ici_data_parallelism,
       config.ici_fsdp_parallelism,
       config.ici_fsdp_transpose_parallelism,
       config.ici_sequence_parallelism,
       config.ici_tensor_parallelism,
-      config.ici_autoregressive_parallelism,
-      config.ici_pipeline_parallelism,
+      config.ici_autoregressive_parallelism,  
   ]
 
   # Find possible unspecified parallelisms
