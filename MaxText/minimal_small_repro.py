2ca037c03d758651a2f87a3d217e75e63fdb9842
gobbleturk <mattdavidow@google.com>
2024-05-13 18:47:03 +0000

    Use sum in minimal small with logging

--- MaxText/minimal_small_repro.py
+++ MaxText/minimal_small_repro.py
@@ -5,8 +5,8 @@ from jax import numpy as jnp
 from jax.sharding import PartitionSpec as P
 
 mesh_axes = ["axis_1", "axis_2"]
-ici_parallelism = [1,4]
-dcn_parallelism = [2,1]
+ici_parallelism = [4,1]
+dcn_parallelism = [1,2]
 device_mesh = mesh_utils.create_hybrid_device_mesh(ici_parallelism, dcn_parallelism)
 mesh = Mesh(device_mesh, mesh_axes)
 data_sharding = (("axis_1", "axis_2"),)
@@ -15,4 +15,6 @@ data_pspec_shardings = jax.sharding.NamedSharding(mesh, data_pspec)
 data = jax.numpy.ones(1024)
 data = jax.lax.with_sharding_constraint(data, data_pspec_shardings)
 jit_sum = jax.jit(jnp.sum)
-jit_sum(data) # runtime crash
+print("About to perform the sum...",flush=True)
+output = jit_sum(data) # runtime crash
+print(f"Output of sum is {output}", flush=True)
--- MaxText/repro_bad_hlo.sh
+++ MaxText/repro_bad_hlo.sh
@@ -1,4 +1,4 @@
 export LIBTPU_INIT_ARGS="--xla_jf_bounds_check"
 (python3 MaxText/train.py MaxText/configs/base.yml run_name=mattdavidow-train-base base_output_directory=gs://maxtext-experiments-multipod dataset_path=gs://max-datasets-rogue steps=5 enable_checkpointing=False dcn_pipeline_parallelism=4 base_num_decoder_layers=64 scan_layers=True   per_device_batch_size=12 enable_profiler=True scan_pipeline_iterations=True decoder_block=simple) 2>&1 | tee /tmp/log
-gcs_path="gs://mattdavidow-maxtext-br/pipeline-debug-a4-slice-${MEGASCALE_SLICE_ID}/"
+gcs_path="gs://mattdavidow-maxtext-br/pipeline-debug-minimal-a2-slice-${MEGASCALE_SLICE_ID}/"
 gsutil cp /tmp/log ${gcs_path}
