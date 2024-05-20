d2c2902fb8e56377f35a9ce517f21f857725c395
RissyRan <ran.rissy@gmail.com>
2024-05-20 22:31:02 +0000

    clean_up

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -295,29 +295,10 @@ class MoeBlock(nn.Module):
   weight_dtype: DType = jnp.float32
   dtype: DType = jnp.float32
 
-  def generate_kernels(self, num_experts, base_emb_dim, mlp_dim, inputs):
+  def generate_kernels(self, num_experts, base_emb_dim, mlp_dim):
     
-    features = _canonicalize_tuple(mlp_dim)
-    wo_features = _canonicalize_tuple(inputs.shape[-1])
-    axis = _canonicalize_tuple(-1)
-    axis = _normalize_axes(axis, inputs.ndim)
-    kernel_shape = tuple(inputs.shape[ax] for ax in axis) + features
-    wo_kernel_shape = tuple(inputs.shape[ax] for ax in axis) + wo_features
-    kernel_in_axis = np.arange(len(axis))
-    kernel_out_axis = np.arange(len(axis), len(axis) + len(features))
-    wo_kernel_out_axis = np.arange(len(axis), len(axis) + len(wo_features))
-
-    # print("mega.......")
-    # print(f"features: {features}")
-    # print(f"axis: {axis}")
-    # print(f"kernel_shape: {kernel_shape}")
-    # print(f"kernel_shape_0: {wo_kernel_shape}")
-    # print(f"kernel_in_axis: {kernel_in_axis}")
-    # print(f"kernel_out_axis: {kernel_out_axis}")
-    # print(f"wo_kernel_out_axis: {wo_kernel_out_axis}")
-
-    # kernel_in_axis = np.arange(1)
-    # kernel_out_axis = np.arange(1, 2)
+    kernel_in_axis = np.arange(1)
+    kernel_out_axis = np.arange(1, 2)
     kernel_init = nd_dense_init(1.0, 'fan_in', 'truncated_normal')
 
     kernel_axes = ('exp', 'embed', 'mlp')
@@ -328,8 +309,8 @@ class MoeBlock(nn.Module):
         nn.with_logical_partitioning(kernel_init, kernel_axes),
         (num_experts, base_emb_dim, mlp_dim),
         self.weight_dtype,
-        np.arange(1),
-        np.arange(1, 2),
+        kernel_in_axis,
+        kernel_out_axis,
       )
     w0_kernel = jnp.asarray(w0_kernel, self.dtype)
     w1_kernel = self.param(
@@ -337,8 +318,8 @@ class MoeBlock(nn.Module):
         nn.with_logical_partitioning(kernel_init, kernel_axes),
         (num_experts, base_emb_dim, mlp_dim),
         self.weight_dtype,
-        np.arange(1),
-        np.arange(1, 2),
+        kernel_in_axis,
+        kernel_out_axis,
       )
     w1_kernel = jnp.asarray(w1_kernel, self.dtype)
     wo_kernel = self.param(
@@ -346,121 +327,11 @@ class MoeBlock(nn.Module):
         nn.with_logical_partitioning(kernel_init, wo_kernel_axes),
         (num_experts, mlp_dim, base_emb_dim),
         self.weight_dtype,
-        np.arange(1),
-        np.arange(1, 3),
+        kernel_in_axis,
+        kernel_out_axis,
       )
     wo_kernel = jnp.asarray(wo_kernel, self.dtype)
     return w0_kernel, w1_kernel, wo_kernel
-
-  # def call_gmm(self,
-  #              inputs,
-  #              num_experts,
-  #              base_emb_dim,
-  #              mlp_dim,
-  #              mlp_activation,
-  #              group_sizes):
-
-  #   @functools.partial(
-  #       shard_map.shard_map,
-  #       mesh=self.mesh,
-  #       in_specs=(
-  #             (nn.logical_to_mesh_axes((None, None))),
-  #             (nn.logical_to_mesh_axes((None, None, None))),
-  #             (nn.logical_to_mesh_axes((None,))),
-  #         ),
-  #       out_specs=(nn.logical_to_mesh_axes((None, None))),
-  #       # in_specs=(
-  #       #       (nn.logical_to_mesh_axes(("activation_batch_length", "embed"))),
-  #       #       (nn.logical_to_mesh_axes((None, "activation_embed", "mlp"))),
-  #       #       (nn.logical_to_mesh_axes((None,))),
-  #       #   ),
-  #       # out_specs=(nn.logical_to_mesh_axes(("activation_batch_length", "mlp"))),
-  #       check_rep=False,
-  #   )
-  #   def gmm(inputs, kernel, group_sizes):
-  #     hs_shape = inputs.shape
-  #     # pad lengh is the 1st dimension of tiling size in gmm call
-  #     pad_length = 512
-  #     if hs_shape[0] % pad_length:
-  #       # padding
-  #       pad_length = pad_length - hs_shape[0] % pad_length
-
-  #       inputs = jax.lax.pad(inputs.astype(jnp.float32), 0.0, [(0, pad_length, 0), (0,0,0)])
-  #       inputs = inputs.astype(self.dtype)
-
-  #     # print(f"{inputs.shape} {kernel.shape} {group_sizes.shape}")
-  #     #jax.debug.print("{group_sizes}", group_sizes=group_sizes)
-  #     inputs = inputs.astype(self.dtype)
-  #     kernel = kernel.astype(self.weight_dtype)
-  #     output = mblx.gmm(lhs=inputs, 
-  #                       rhs=kernel, 
-  #                       group_sizes=group_sizes,
-  #                       tiling=(512, 512, 512))
-      
-  #     if hs_shape[0] % pad_length:
-  #       output = output[:hs_shape[0]]
-
-  #     return output
-  
-  #   w0_kernel, w1_kernel, wo_kernel = self.generate_kernels(num_experts,base_emb_dim,mlp_dim,inputs.ndim)
-
-  #   # jax.debug.print("group sizes: {group_sizes}", group_sizes=group_sizes)
-  #   layer_1 = gmm(inputs, w0_kernel, group_sizes)
-  #   layer_2 = gmm(inputs, w1_kernel, group_sizes)
-  #   layer_1_act = _convert_to_activation_function(mlp_activation)(layer_1)
-  #   intermediate_layer = jnp.multiply(layer_1_act, layer_2)
-  #   output = gmm(intermediate_layer, wo_kernel, group_sizes)
-  #   print("running gmm")
-  #   return output
-
-  # def permute(self, inputs, gate_logits, base_emb_dim):
-  #   inputs_2d = jnp.reshape(inputs, (-1, base_emb_dim))
-  #   weights, selected_experts = jax.lax.top_k(gate_logits, self.num_experts_per_tok)
-  #   weights = jax.nn.softmax(weights.astype(self.weight_dtype), axis=-1).astype(self.dtype)
-  #   flatten_selected_experts = jnp.ravel(selected_experts)
-  #   sorted_selected_experts = jnp.argsort(flatten_selected_experts)
-  #   repeat_hidden_states = jnp.repeat(inputs_2d, self.num_experts_per_tok, axis=0)
-  #   sorted_output = jnp.take(repeat_hidden_states, indices=sorted_selected_experts, axis=0).astype(self.dtype)
-  #   expert_order, expert_size = jnp.unique(flatten_selected_experts, return_counts=True, size=self.num_experts)
-  #   group_size = jnp.zeros(self.num_experts, dtype=jnp.int32)
-
-  #   expert_is_unused = expert_size == 0
-  #   indices = jnp.where(expert_is_unused, len(group_size), expert_order)
-  #   group_size = group_size.at[indices].set(jnp.where(expert_is_unused, group_size[indices], expert_size))
-
-  #   return sorted_output, sorted_selected_experts, weights, group_size
-
-  # def unpermute(self, layer_3, inputs, sorted_selected_experts, weights):
-  #   unsort_output = jnp.take(layer_3, indices=jnp.argsort(sorted_selected_experts), axis=0)
-  #   flatten_weights = jnp.ravel(weights)
-  #   combined_output = jnp.multiply(unsort_output, flatten_weights[:, None])
-  #   groups = jnp.reshape(combined_output, (-1, self.num_experts_per_tok, combined_output.shape[1]))
-  #   return jnp.sum(groups, axis=1).reshape(inputs.shape).astype(self.dtype)
-
-  # @nn.compact
-  # def __call__(self, inputs):
-  #   cfg = self.config
-
-  #   gate_logits = DenseGeneral(
-  #           self.num_experts,
-  #           dtype=self.dtype,
-  #           kernel_init=self.kernel_init,
-  #           kernel_axes=self.kernel_axes,
-  #           name='gate')(inputs)
-  #   sorted_hidden_states, sorted_selected_experts, weights, group_size = self.permute(inputs,
-  #                                                                                     gate_logits,
-  #                                                                                     cfg.base_emb_dim)
-  
-  #   intermediate_output = self.call_gmm(sorted_hidden_states,
-  #                                       cfg.num_experts,
-  #                                       cfg.base_emb_dim,
-  #                                       cfg.mlp_dim,
-  #                                       cfg.mlp_activations[0],
-  #                                       group_size)
-
-  #   output = self.unpermute(intermediate_output, inputs, sorted_selected_experts, weights)
-
-  #   return output
 
   @nn.compact
   def __call__(self, inputs):
@@ -491,8 +362,7 @@ class MoeBlock(nn.Module):
 
     w0_kernel, w1_kernel, wo_kernel = self.generate_kernels(cfg.num_experts,
                                                             cfg.base_emb_dim,
-                                                            cfg.mlp_dim,
-                                                            inputs)
+                                                            cfg.mlp_dim)
     
     with jax.named_scope("wi_0"):
       layer_w0 = jnp.einsum("BLE,NEH -> BLNH", inputs, w0_kernel)
