0eaa7a41bae7fafa4661f1a449e1b487971db85c
gobbleturk <mattdavidow@google.com>
2024-05-28 02:49:13 +0000

    workable test

--- MaxText/tests/pipeline_parallelism_test.py
+++ MaxText/tests/pipeline_parallelism_test.py
@@ -43,67 +43,22 @@ from flax.core import meta
 
 import jax.numpy as jnp
 from flax import linen as nn
+from layers import simple_layer
 
 
-def assert_same_output_and_grad(f1,f2, targets, *inputs, f1_extra_inputs=[], f2_extra_inputs=[],f1_name="regular",f2_name="pipeline"):
-  f1_inputs = (*inputs, *f1_extra_inputs)
-  f2_inputs = (*inputs, *f2_extra_inputs)
-  def f1_loss(*f1_inputs):
-    return jnp.linalg.norm(f1(*f1_inputs) - targets)
-
-  def f2_loss(*f2_inputs):
-    return jnp.linalg.norm(f2(*f2_inputs) - targets)
-
-  def print_norms(a,b,f1_name="regular",f2_name="pipeline",diff_name="diff"):
-    a_norm = jnp.linalg.norm(a)
-    b_norm = jnp.linalg.norm(b)
-    diff_norm = jnp.linalg.norm(a-b)
-
-    print(f"{diff_name} norm of {diff_norm}")
-    print(f"{f1_name} norm of {a_norm}")
-    print(f"{f2_name} norm of {b_norm}")
-
-  def my_ravel(pytree):
-    ravelled_tree = jax.tree_map(jnp.ravel, pytree)
-    ravelled_leaves, _ = jax.tree_util.tree_flatten(ravelled_tree)
-    return jnp.concatenate(ravelled_leaves)
-
-  f1_value = f1(*f1_inputs)
-  f2_value = f2(*f2_inputs)
-  _, f1_grad = jax.value_and_grad(f1_loss)(*f1_inputs)
-  _, f2_grad = jax.value_and_grad(f2_loss)(*f2_inputs)
-
-  f1_grad = my_ravel(f1_grad)
-  f2_grad = my_ravel(f2_grad)
-  print(f"{f1_grad.shape=}")
-
-  print_norms(f1_value, f2_value, f1_name=f"{f1_name} output", f2_name=f"{f2_name} output", diff_name="Output difference")
-  print_norms(f1_grad, f2_grad, f1_name=f"{f1_name} grad", f2_name=f"{f2_name} grad", diff_name="Gradient difference")
-
-
-def assert_same_output_and_grad_2(f1,f2, *inputs, f1_name="regular",f2_name="pipeline"):
+def assert_same_output_and_grad(f1, f2, *inputs):
   f1_value, f1_grad = jax.value_and_grad(f1)(*inputs)
   f2_value, f2_grad = jax.value_and_grad(f2)(*inputs)
 
-  def my_ravel(pytree):
+  def pytree_ravel(pytree):
     ravelled_tree = jax.tree_map(jnp.ravel, pytree)
     ravelled_leaves, _ = jax.tree_util.tree_flatten(ravelled_tree)
     return jnp.concatenate(ravelled_leaves)
-
-  f1_grad = my_ravel(f1_grad)
-  f2_grad = my_ravel(f2_grad)
-  print(f"{f1_grad.shape=}")
-
-  def print_norms(a,b,f1_name="regular",f2_name="pipeline",diff_name="diff"):
-    a_norm = jnp.linalg.norm(a)
-    b_norm = jnp.linalg.norm(b)
-    diff_norm = jnp.linalg.norm(a-b)
-
-    print(f"{diff_name} norm of {diff_norm}")
-    print(f"{f1_name} norm of {a_norm}")
-    print(f"{f2_name} norm of {b_norm}")
-  print_norms(f1_value, f2_value, f1_name=f"{f1_name} output", f2_name=f"{f2_name} output", diff_name="Output difference")
-  print_norms(f1_grad, f2_grad, f1_name=f"{f1_name} grad", f2_name=f"{f2_name} grad", diff_name="Gradient difference")
+  f1_grad = pytree_ravel(f1_grad)
+  f2_grad = pytree_ravel(f2_grad)
+  
+  assert jax.numpy.allclose(f1_value, f2_value, rtol=1e-2, equal_nan=False)
+  assert jax.numpy.allclose(f1_grad, f2_grad, rtol=1e-2, equal_nan=False)
 
 
 class PipelineParallelismTest(unittest.TestCase):
@@ -118,19 +73,17 @@ class PipelineParallelismTest(unittest.TestCase):
         max_target_length=128,
         base_emb_dim=28,
         ici_pipeline_parallelism=4,
-        base_num_decoder_layers=4            
+        base_num_decoder_layers=8,
+        num_pipeline_microbatches=8,
+        per_device_batch_size=4
     )
     config = pyconfig.config
-    self.config = config
+    devices_array = max_utils.create_device_mesh(config)
+    mesh = Mesh(devices_array, config.mesh_axes)
 
-    self.rng = jax.random.PRNGKey(1234)
-
-    devices_array = max_utils.create_device_mesh(self.config)
-    mesh = Mesh(devices_array, self.config.mesh_axes)
-    self.mesh = mesh
 
     def get_inputs(batch_size, sequence, features):
-        '''Get random inputs, and random targets
+        '''Get random inputs, and random dummy targets
             Returns
                 inputs: [batch_size, sequence, features]
                 targets: [batch_size, sequence, features]
@@ -138,30 +91,24 @@ class PipelineParallelismTest(unittest.TestCase):
                 segmentations: [batch_size, segmentation]
         '''
         input_shape = [batch_size, sequence, features]
-        k = jax.random.PRNGKey(2)
-        inputs = jax.random.normal(k,input_shape, dtype=jnp.float32)
+        inputs = jax.random.normal(jax.random.PRNGKey(2), input_shape, dtype=jnp.float32)
 
         # dummy targets same shape as inputs to use for a dummy loss function to check gradient correctness
-        k = jax.random.PRNGKey(3)
-        dummy_targets = jax.random.normal(k,input_shape, dtype=jnp.float32)
+        dummy_targets = jax.random.normal(jax.random.PRNGKey(3),input_shape, dtype=jnp.float32)
 
         inputs_position = jnp.array([jnp.arange(sequence, dtype=jnp.int32) for _ in range(batch_size)], dtype=jnp.int32)
         inputs_segmentation = jnp.ones((batch_size, sequence), dtype=jnp.int32)
         return inputs, dummy_targets, inputs_position, inputs_segmentation
 
     self.inputs, self.dummy_targets, self.inputs_position, self.inputs_segmentation = get_inputs(config.global_batch_size_to_train_on, config.max_target_length, config.emb_dim)
-
-
     self.deterministic = True
     self.model_mode = common_types.MODEL_MODE_TRAIN
-    pipeline_stage = llama2.LlamaDecoderLayer(config=config, mesh=self.mesh)
-    from layers import simple_layer
-    pipeline_stage = simple_layer.SimpleDecoderLayer(config=config, mesh=self.mesh)
-    self.pipeline_stage = pipeline_stage
+    # We use a simpler single matmul decoder layer for fast compilation to run in tests.    
+    single_pipeline_stage = simple_layer.SimpleDecoderLayer(config=config, mesh=mesh)
     my_pipeline = pipeline_flax.Pipeline(
-        config=self.config,
-        layers=pipeline_stage,
-        mesh=self.mesh
+        config=config,
+        layers=single_pipeline_stage,
+        mesh=mesh
     )
     self.init_pipeline_params = my_pipeline.init(jax.random.PRNGKey(0), self.inputs, self.inputs_position, self.inputs_segmentation, self.deterministic, self.model_mode)
 
@@ -171,58 +118,34 @@ class PipelineParallelismTest(unittest.TestCase):
        loss = jnp.linalg.norm(outputs - dummy_targets)
        return loss
 
-    self.config=config
-    def run_regular_pipeline(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode):
-        config = self.config
-        decoder_layer_instance = self.pipeline_stage
-        reg_layer_activations = inputs
-
+    def regular_sequential_layers(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode):
+        # We need to reshape the pipeline's weights to remove the circular dimension to apply layers sequentially.
         def get_cur_layer_params(params, layer_idx):
             def get_cur_layer_params_arr(leaf):
-                if config.num_pipeline_repeats > 1 and config.num_layers_per_pipeline_stage == 1:
-                    new_shape = (leaf.shape[0] * leaf.shape[1],) + leaf.shape[2:]
-                    leaf = jnp.reshape(leaf, new_shape)
-                elif config.num_pipeline_repeats > 1 and config.num_layers_per_pipeline_stage > 1:
-                    new_shape = (leaf.shape[0] * leaf.shape[1] * leaf.shape[2],) + leaf.shape[3:]
-                    leaf = jnp.reshape(leaf, new_shape)
-                elif config.num_pipeline_repeats == 1 and config.num_layers_per_pipeline_stage > 1:
-                    new_shape = (leaf.shape[0] * leaf.shape[1],) + leaf.shape[2:]
-                    leaf = jnp.reshape(leaf, new_shape)
+                new_shape = (leaf.shape[0] * leaf.shape[1],) + leaf.shape[2:]
+                leaf = jnp.reshape(leaf, new_shape)
                 return leaf[layer_idx]
             return jax.tree.map(get_cur_layer_params_arr, params)
 
-        old=False
+        reg_layer_activations = inputs
         for layer in range(config.num_decoder_layers):
-            if old:
-                cur_layer_params = params['params'][f'layers_{layer}']
-                cur_layer_params = {'params':cur_layer_params}
-            else:
-                cur_layer_params = get_cur_layer_params(params, layer)
-                cur_layer_params['params'] = cur_layer_params['params']['layers']
-            if config.num_pipeline_repeats > 1 and config.num_layers_per_pipeline_stage > 1:
-                cur_layer_params['params'] = meta.remove_axis(cur_layer_params['params'], 0, {nn.PARTITION_NAME:"circular_repeats"})
-                cur_layer_params['params'] = meta.remove_axis(cur_layer_params['params'], 0, {nn.PARTITION_NAME:"layers"})
-            reg_layer_activations, _ = decoder_layer_instance.apply(cur_layer_params, reg_layer_activations, inputs_position, inputs_segmentation, deterministic, model_mode)
+            cur_layer_params = get_cur_layer_params(params, layer)
+            cur_layer_params['params'] = cur_layer_params['params']['layers'] # Remove the leading "layers" structure
+            reg_layer_activations, _ = single_pipeline_stage.apply(cur_layer_params, reg_layer_activations, inputs_position, inputs_segmentation, deterministic, model_mode)
         return reg_layer_activations
 
-    def run_regular_pipeline_dummy_loss(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode, dummy_targets):
-       outputs = run_regular_pipeline(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode)
+    def regular_sequential_layers_dummy_loss(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode, dummy_targets):
+       outputs = regular_sequential_layers(params, inputs, inputs_position, inputs_segmentation, deterministic, model_mode)
        loss = jnp.linalg.norm(outputs - dummy_targets)
        return loss
 
-  
-    self.reg_layers = run_regular_pipeline
-    self.pipeline_func = my_pipeline.apply
-
     self.pipeline_dummy = pipeline_parallelism_dummy_loss_func
-    self.regular_dummy = run_regular_pipeline_dummy_loss
+    self.regular_dummy = regular_sequential_layers_dummy_loss
 
 
   @pytest.mark.tpu
   def test_pipeline_parallelism_same_output_and_grad(self):
-    assert 2 > 1
-    #assert_same_output_and_grad(self.reg_layers,self.pipeline_func, self.dummy_targets, self.init_pipeline_params, self.inputs, self.inputs_segmentation, self.inputs_position, self.deterministic, self.model_mode)
-    assert_same_output_and_grad_2(self.regular_dummy,self.pipeline_dummy, self.init_pipeline_params, self.inputs, self.inputs_segmentation, self.inputs_position, self.deterministic, self.model_mode, self.dummy_targets)
+    assert_same_output_and_grad(self.regular_dummy,self.pipeline_dummy, self.init_pipeline_params, self.inputs, self.inputs_segmentation, self.inputs_position, self.deterministic, self.model_mode, self.dummy_targets)
+
 if __name__ == "__main__":
-  
   unittest.main()
