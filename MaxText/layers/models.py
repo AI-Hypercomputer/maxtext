57429da226e7e0a93420fad9a19e4d9f94adc9f9
Morgan Du <morgandu@google.com>
2024-05-30 20:54:26 +0000

    Enable kv cache layout control

--- MaxText/layers/models.py
+++ MaxText/layers/models.py
@@ -92,6 +92,10 @@ class DecoderLayer(nn.Module):
         name="self_attention",
         quant=self.quant,
         quantize_kvcache=cfg.quantize_kvcache,
+        prefill_key_axis_order=tuple([int(i) for i in cfg.prefill_key_axis_order.split(",")]),
+        prefill_value_axis_order=tuple([int(i) for i in cfg.prefill_value_axis_order.split(",")]),
+        ar_key_axis_order=tuple([int(i) for i in cfg.ar_key_axis_order.split(",")]),
+        ar_value_axis_order=tuple([int(i) for i in cfg.ar_value_axis_order.split(",")]),
     )
 
     attention_lnx = attention_layer(
