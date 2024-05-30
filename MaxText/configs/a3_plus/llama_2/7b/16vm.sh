814fb0decefb28589520f1bca1ae47ec0028af8a
yangyuwei <yangyuwei@google.com>
2024-05-30 07:28:06 -0700

    Make a small fix.

--- MaxText/configs/a3_plus/llama_2/7b/16vm.sh
+++ MaxText/configs/a3_plus/llama_2/7b/16vm.sh
@@ -79,4 +79,4 @@ export XLA_FLAGS="--xla_dump_to=$OUTPUT_PATH/$RUN_NAME/HLO_dumps/
 python MaxText/train.py MaxText/configs/base.yml run_name=$RUN_NAME hardware=gpu \
     steps=30 dcn_data_parallelism=16 ici_fsdp_parallelism=8 per_device_batch_size=4 max_target_length=4096 model_name=llama2-7b \
     enable_checkpointing=false attention=cudnn_flash_te remat_policy=minimal_flash use_iota_embed=true scan_layers=false \
-    dataset_type=synthetic async_checkpointing=false base_output_directory=gs://runner-maxtext-logs profiler=xplane logits_dot_in_fp32=false
+    dataset_type=synthetic async_checkpointing=false base_output_directory=gs://runner-maxtext-logs enable_profiler=true logits_dot_in_fp32=false
