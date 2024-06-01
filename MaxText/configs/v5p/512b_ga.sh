360687ece392caed5bc0b6e26a1cb9cec881659a
ZhiyuLi-goog <lizhiyu@google.com>
2024-05-31 23:36:37 -0700

    add debug flag

--- MaxText/configs/v5p/512b_ga.sh
+++ MaxText/configs/v5p/512b_ga.sh
@@ -38,6 +38,11 @@ fi
 # Set up network optimizations
 bash preflight.sh PLATFORM=$PLATFORM
 
+# debug
+export TPU_STDERR_LOG_LEVEL=0
+export TF_CPP_MIN_LOG_LEVEL=0
+export TPU_MIN_LOG_LEVEL=0
+
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_megacore_fusion_allow_ags=false --xla_enable_async_collective_permute=true --xla_tpu_enable_ag_backward_pipelining=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/$EXECUTABLE MaxText/configs/base.yml\
