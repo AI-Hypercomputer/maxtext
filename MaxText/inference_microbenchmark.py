951200eb5239e59f57c5be40305e151bdd09c17c
Andy Ye <andyye333@gmail.com>
2024-06-06 20:13:18 +0000

    Fix

--- MaxText/inference_microbenchmark.py
+++ MaxText/inference_microbenchmark.py
@@ -289,7 +289,9 @@ def main(config, inference_metadata: Optional[Dict[str, Any]] = None):
         prefill_true_lengths[prefill_length],
         benchmark_loop_iters
       )
-      benchmark_results["insert"][prefill_length]["time_in_ms"] = prefill_insert_time - benchmark_results["prefill"][prefill_length]["time_in_ms"]
+      benchmark_results["insert"][prefill_length]["time_in_ms"] = (
+        prefill_insert_time - benchmark_results["prefill"][prefill_length]["time_in_ms"]
+      )
 
   if "generate" in stages_to_benchmark:
     benchmark_results["autoregressive"], decode_state = ar_benchmark(
