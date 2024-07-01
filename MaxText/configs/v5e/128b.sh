869bfba3940e19f420d39bd04cce55b34f2e94c6
Akanksha Gupta <akshu@google.com>
2024-07-01 23:10:07 +0000

    Add --xla_tpu_enable_megascale_barrier=true

--- MaxText/configs/v5e/128b.sh
+++ MaxText/configs/v5e/128b.sh
@@ -39,7 +39,7 @@ fi
 bash preflight.sh PLATFORM=$PLATFORM
 
 # Train
-export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
+export LIBTPU_INIT_ARGS="--xla_tpu_enable_megascale_barrier=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 
 python3 MaxText/$EXECUTABLE MaxText/configs/base.yml\
     steps=30 per_device_batch_size=1 enable_checkpointing=false\
