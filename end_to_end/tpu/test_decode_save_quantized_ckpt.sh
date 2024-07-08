8fb12fcbd0883ed17fce6e3ace4739b184569887
Mitali Singh <msingh@google.com>
2024-07-08 22:14:46 +0000

    Adding support for mixed precision quantization configs.

--- end_to_end/tpu/test_decode_save_quantized_ckpt.sh
+++ end_to_end/tpu/test_decode_save_quantized_ckpt.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Example run: bash end_to_end/tpu/test_decode_save_quantized_ckpt.sh -m llama2-70b -r 070924 -n
+
 dry_run=false
 model='llama2-7b'
+run_name=$(date +'%Y%m%d%H%M%S')
 
-while getopts "nm:" opt
+while getopts "nm:r:" opt
 do
   case "$opt" in
       n ) dry_run=true ;;
       m ) model="$OPTARG" ;;
+      r ) run_name="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
   esac
 done
@@ -26,7 +30,6 @@ if [ "$model" = "llama2-70b" ]; then
   checkpoint_ts='2024-05-08-23-16'
 fi
 
-
 export MODEL_NAME=${model}
 export TOKENIZER_PATH=assets/tokenizer.llama2
 export LOAD_PARAMETERS_PATH=gs://inference-benchmarks/models/${MODEL_NAME}-chat/${checkpoint_ts}/param-only-decode-ckpt-maxtext/checkpoints/0/items
@@ -38,10 +41,17 @@ export ICI_TENSOR_PARALLELISM=-1
 export SCAN_LAYERS=false
 export WEIGHT_DTYPE=bfloat16
 export PER_DEVICE_BATCH_SIZE=11
-export QUANTIZATION="int8"
-export QUANTIZE_KVCACHE=True
-export SAVE_QUANTIZED_CHECKPOINT_PATH=gs://${USER}-bkt/checkpoints/quantized/aqt_ckpt/${MODEL_NAME}-chat
-
+export QUANTIZATION="intmp"
+export QUANT_CFG="mp_scale"
+export QUANT_CFG_PATH="MaxText/configs/quantization/${QUANT_CFG}.json"
+export QUANTIZE_KVCACHE=False
+export CHKPT_SUBDIR="${run_name}/${QUANTIZATION}_${QUANT_CFG}"
+export SAVE_QUANTIZED_CHECKPOINT_PATH=gs://${USER}-bkt/checkpoints/quant_${MODEL_NAME}-chat/${CHKPT_SUBDIR}
+export OUTDIR="/tmp/${cmd}_res_save_chkpt/${CHKPT_SUBDIR}"
+export OUTFILE="${OUTDIR}/decode.txt"
+mkdir -p $OUTDIR
+echo
+# Run command
 ${cmd} python MaxText/decode.py \
   MaxText/configs/base.yml \
   tokenizer_path=${TOKENIZER_PATH} \
@@ -56,5 +66,15 @@ ${cmd} python MaxText/decode.py \
   weight_dtype=${WEIGHT_DTYPE} \
   per_device_batch_size=${PER_DEVICE_BATCH_SIZE} \
   quantization=${QUANTIZATION} \
+  quant_cfg_path=${QUANT_CFG_PATH} \
   quantize_kvcache=${QUANTIZE_KVCACHE} \
   save_quantized_params_path=${SAVE_QUANTIZED_CHECKPOINT_PATH} \
+  | tee -a $OUTFILE
+echo
+echo "Output directed to: ${OUTFILE}"
+echo
+echo "Checkpoint saved at:$SAVE_QUANTIZED_CHECKPOINT_PATH"
+${cmd} gsutil ls -lh $SAVE_QUANTIZED_CHECKPOINT_PATH >> ${OUTFILE}
+echo
+
+
