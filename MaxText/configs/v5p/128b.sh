c0b18da55f005777f3c599698a70da47718489cb
Raymond Zou <raymondzou@google.com>
2024-07-15 23:56:54 +0000

    Lower number of steps for configs

--- MaxText/configs/v5p/128b.sh
+++ MaxText/configs/v5p/128b.sh
@@ -41,7 +41,7 @@ bash preflight.sh PLATFORM=$PLATFORM
 # Train
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_megacore_fusion_allow_ags=false --xla_enable_async_collective_permute=true --xla_tpu_enable_ag_backward_pipelining=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 python3 MaxText/$EXECUTABLE MaxText/configs/base.yml\
-    steps=30 per_device_batch_size=1 enable_checkpointing=false\
+    steps=15 per_device_batch_size=1 enable_checkpointing=false\
     remat_policy=minimal global_parameter_scale=128\
     ici_fsdp_parallelism=-1 ici_tensor_parallelism=8\
     max_target_length=2048 base_output_directory=$OUTPUT_PATH\
