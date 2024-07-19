d890339a7193dbdb5b366ae3af72998e59d4ec5b
Ran Ran <ran.rissy@gmail.com>
2024-07-18 17:17:17 -0700

    Copybara import of the project:
    --
    a3ed121cfbebe350ba60ab06d92327b8a3dc76b6 by RissyRan <ran.rissy@gmail.com>:
    
    Update tp sharding annotation for MoE block
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/maxtext/pull/788 from google:tp_update a3ed121cfbebe350ba60ab06d92327b8a3dc76b6
    PiperOrigin-RevId: 653803904

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -49,7 +49,6 @@ bias_init = initializers.default_bias_init
 RMSNorm = normalizations.RMSNorm
 Quant = quantizations.AqtQuantization
 
-
 def _convert_to_activation_function(fn_or_string: Union[str, Callable[..., Any]]) -> Callable[..., Any]:
   """Convert a string to an activation function."""
   if fn_or_string == "linear":
@@ -378,7 +377,9 @@ class MoeBlock(nn.Module):
         output = output[:hs_shape[0]]
       return output
 
-    # Currently, we only support data parallelism with Megablox (sharding on batch dimensions)
+    # Currently, we only support data and tensor parallelism with Megablox.
+    # We all gather the input activations over tensor parallelism to follow strategy
+    # in https://parsa.epfl.ch/course-info/cs723/papers/Megatron.pdf.
     @functools.partial(
         shard_map.shard_map,
         mesh=self.mesh,
@@ -395,15 +396,13 @@ class MoeBlock(nn.Module):
     def wrapper(x, logits, w0, w1, wo):
       x, sorted_selected_experts, weights, group_sizes = self.permute(x, logits)
       layer_w0 = gmm(x, w0, group_sizes)
-      tensor_parallelism = self.config.ici_tensor_parallelism * self.config.dcn_tensor_parallelism
-      if tensor_parallelism > 1:
-        layer_w0 = jax.lax.psum(layer_w0, 'tensor')
       layer_w1 = gmm(x, w1, group_sizes)
-      if tensor_parallelism > 1:
-        layer_w1 = jax.lax.psum(layer_w1, 'tensor')
       layer_act = _convert_to_activation_function(self.config.mlp_activations[0])(layer_w0)
       intermediate_layer = jnp.multiply(layer_act, layer_w1)
       intermediate_output = gmm(intermediate_layer, wo, group_sizes)
+      tensor_parallelism = self.config.ici_tensor_parallelism * self.config.dcn_tensor_parallelism
+      if tensor_parallelism > 1:
+        intermediate_output = jax.lax.psum_scatter(intermediate_output, 'tensor', scatter_dimension=1, tiled=True)
       output = self.unpermute(intermediate_output,
                               sorted_selected_experts,
                               weights)
