3b73e86d2ad98f2aac1d035e22a138a1899e8a09
gobbleturk <mattdavidow@google.com>
2024-07-22 17:14:05 +0000

    Gathering weights

--- MaxText/my_flax_partial.py
+++ MaxText/my_flax_partial.py
@@ -55,6 +55,44 @@ class MyMultipleBeast(nn.Module):
         #return body_instance.apply(params, inputs)
       return func_to_vmap
 
+    def shard_dim_by_stages(self, x, dim: int):
+      # Shards a dimension by stages. Currently the sharding of other dimensions are left up the compiler, alternatively
+      # we may want to copy over the sharding from the other input axes
+      dims_mapping = [jax.sharding.PartitionSpec.UNCONSTRAINED] * x.ndim
+      dims_mapping[dim] = "stage"
+      dims_mapping = tuple(dims_mapping)
+      sharding = jax.sharding.NamedSharding(mesh, jax.sharding.PartitionSpec(*dims_mapping))
+      return jax.lax.with_sharding_constraint(x, sharding)
+
+    def vmap_parallel_gather(self, weights, repeat_ids, repeat_dim_in_weights, stages_dim_in_weights):
+      """Use vmap to implement a sharded parallel gather.
+      Parallel gather means each stage has its own weights, and gets one slice from it.
+      Args:
+        weights: Per-stage data to be gathered from.
+        repeat_ids: Integer tensor of shape [num_stages], the repeats of the stages.
+        repeat_dim_in_weights: The dimension in weights where repeat_ids are applied. The output will not
+          have this dimension.
+        stages_dim_in_weights: The dimension in weights that represents parallel stages.
+      Returns:
+        The per-stage gathered values. The shape is weights.shape but with repeat_dim_in_weights
+          removed.
+      """
+      def _gather_one(x, repeat_id):
+        return jnp.squeeze(jax.lax.dynamic_slice_in_dim(x, repeat_id, 1, repeat_dim_in_weights), repeat_dim_in_weights)
+
+      gathered_weights_stage_dim = 0
+      repeat_ids = self.shard_dim_by_stages(repeat_ids, 0)
+      weights = self.shard_dim_by_stages(weights, stages_dim_in_weights)
+      stage_weights = jax.vmap(_gather_one, in_axes=(stages_dim_in_weights, 0), out_axes=gathered_weights_stage_dim)(weights, repeat_ids)
+      stage_weights = self.shard_dim_by_stages(stage_weights, gathered_weights_stage_dim)
+      # may need to remove axis here = meta.remove_axis(weights, 0, circular_metadata_params)
+      return stage_weights
+
+    def gather_weights(self, repeat_ids):
+       def gather_leaf(weights):
+        return self.vmap_parallel_gather(weights, repeat_ids, 0, 1)
+       return jax.tree.map(gather_leaf, self.variables)
+       
     @nn.compact
     def __call__(self, stage_inputs: jnp.ndarray):
        # Inputs should be [stage, batch, embed]
@@ -62,7 +100,7 @@ class MyMultipleBeast(nn.Module):
        if 1:
         func_to_vmap = self.get_main_vmap_func() # sdl
 
-        if self.is_initializing() or True:
+        if self.is_initializing():
           vmap_sdl = nn.vmap(
               func_to_vmap,
               in_axes=(0),
@@ -96,12 +134,18 @@ class MyMultipleBeast(nn.Module):
             }
           ) 
 
-        example_inputs = jax.lax.broadcast(stage_inputs, [REPEATS])
-        # We only need to run one set of stages to initialize the variables, instead of looping over all microbatches for the full total_iterations.
-        stage_outputs = circ_vmap_sdl(self.layers, example_inputs)
-        stage_outputs = stage_outputs[0] # Remove extra dimension created for the circular vmap
-        broadcasted_stage_outpus = jax.lax.broadcast(stage_outputs[0], [GLOBAL_BATCH // BATCH])
-        return jnp.reshape(broadcasted_stage_outpus, [GLOBAL_BATCH, EMBED])
+          example_inputs = jax.lax.broadcast(stage_inputs, [REPEATS])
+          # We only need to run one set of stages to initialize the variables, instead of looping over all microbatches for the full total_iterations.
+          stage_outputs = circ_vmap_sdl(self.layers, example_inputs)
+          stage_outputs = stage_outputs[0] # Remove extra dimension created for the circular vmap
+          broadcasted_stage_outpus = jax.lax.broadcast(stage_outputs[0], [GLOBAL_BATCH // BATCH])
+          return jnp.reshape(broadcasted_stage_outpus, [GLOBAL_BATCH, EMBED])
+       
+       if not self.is_initializing():
+          repeat_ids = jnp.array([2,0])
+          #breakpoint()
+          weights = self.gather_weights(repeat_ids)
+          breakpoint()
 
        
        
