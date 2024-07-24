27636e39b67e58d79ef5ffdb43d3c5ebc28383a2
khatwanimohit <mohitkhatwani@google.com>
2024-07-24 17:42:58 +0000

    Convert tiktoken to SentencePiece

---
+++ MaxText/tests/convert_tokenizer.py
@@ -0,0 +1,9 @@
+
+from tiktoken.load import load_tiktoken_bpe
+
+
+# Example initialization (replace with your actual tokenizer model)
+mergeable_ranks = load_tiktoken_bpe("assets/tokenizer_llama3.tiktoken")
+
+
+breakpoint()
