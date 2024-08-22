a011d1bb4f9e5cf15f87418bcda6704e6db6ddd8
Zhaoyue Cheng <joychengzhaoyue@gmail.com>
2024-08-22 20:45:35 +0000

    add gemma2 27b configs to Maxtext

--- end_to_end/tpu/gemma2/27b/1_test_gemma.sh
+++ end_to_end/tpu/gemma2/27b/1_test_gemma.sh
@@ -7,13 +7,13 @@
 # 1. Pull the checkpoint from a GCS bucket and uploads the new MaxText compatible checkpoint to destination GCS bucket.
 # 2. Convert the scanned checkpoint from step 1 into unscanned checkpoint format and run more efficient decoding.
 
-# Example Usage: export BASE_OUTPUT_PATH=/path/to/GCS/bucket; bash end_to_end/tpu/gemma2/9b/1_test_gemma.sh
-# Use the same BASE_OUTPUT_PATH as end_to_end/tpu/gemma2/9b/2_test_gemma.sh.
+# Example Usage: export BASE_OUTPUT_PATH=/path/to/GCS/bucket; bash end_to_end/tpu/gemma2/27b/1_test_gemma.sh
+# Use the same BASE_OUTPUT_PATH as end_to_end/tpu/gemma2/27b/2_test_gemma.sh.
 # Please note that in these two scripts (1_test_gemma.sh and 2_test_gemma.sh) BASE_OUTPUT_PATH is assumed to be already a unique path across multiple runs and 
 # the subfolders names aka RUN_NAMEs are static. Please remember to change BASE_OUTPUT_PATH across different runs.
 
 set -ex
-export MODEL_VARIATION='9b'
+export MODEL_VARIATION='27b'
 
 
 # After downloading checkpoints, copy them to GCS bucket at $CHKPT_BUCKET \
@@ -23,7 +23,7 @@ export CHKPT_BUCKET=gs://maxtext-gemma/flax
 
 if [ -z "${BASE_OUTPUT_PATH}" ]; then
     # Non-Googlers please remember to point BASE_OUTPUT_PATH to GCS buckets that you own, this script uses internal buckets for testing.
-    # Use the same BASE_OUTPUT_PATH as end_to_end/tpu/gemma2/9b/2_test_gemma.sh
+    # Use the same BASE_OUTPUT_PATH as end_to_end/tpu/gemma2/27b/2_test_gemma.sh
     export BASE_OUTPUT_PATH=gs://runner-maxtext-logs/$(date +%Y-%m-%d-%H-%M)
     echo "BASE_OUTPUT_PATH is not set, using BASE_OUTPUT_PATH = ${BASE_OUTPUT_PATH}"
 fi
@@ -42,5 +42,5 @@ export RUN_NAME=unscanned_chkpt
 
 export UNSCANNED_CKPT_PATH=${BASE_OUTPUT_PATH}/${RUN_NAME}/checkpoints/0/items
 
-JAX_PLATFORMS=cpu python MaxText/generate_param_only_checkpoint.py MaxText/configs/base.yml async_checkpointing=false base_output_directory=${BASE_OUTPUT_PATH} load_parameters_path=${CONVERTED_CHECKPOINT} run_name=${RUN_NAME} model_name=gemma2-9b force_unroll=true
+JAX_PLATFORMS=cpu python MaxText/generate_param_only_checkpoint.py MaxText/configs/base.yml async_checkpointing=false base_output_directory=${BASE_OUTPUT_PATH} load_parameters_path=${CONVERTED_CHECKPOINT} run_name=${RUN_NAME} model_name=gemma2-27b force_unroll=true
 echo "Written MaxText compatible unscanned checkpoint to ${BASE_OUTPUT_PATH}/${MODEL_VARIATION}/${RUN_NAME}/checkpoints/0/items"
