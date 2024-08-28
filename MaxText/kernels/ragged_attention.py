d0745ced3c0b7c9c8ac3500c824e0954d0463fac
maxtext authors <no-reply@google.com>
2024-08-28 11:44:20 -0700

    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/maxtext/pull/851 from google:mattdavidow-fix-kernel 5cc0f96bcd35a7c3a134ff38309f6dd778b1d3a8
    PiperOrigin-RevId: 668543391

--- MaxText/kernels/ragged_attention.py
+++ MaxText/kernels/ragged_attention.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-"""Kernels for ragged attention."""
+"""Kernels for ragged attention for efficient inference."""
 
 import functools
 
