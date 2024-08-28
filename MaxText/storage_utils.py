634bb21bb57475386feffb7002d31097b829ab2d
Bernard Han <bernardhan@google.com>
2024-08-28 16:06:50 +0000

    Add node attributes to the training benchmark

--- MaxText/storage_utils.py
+++ MaxText/storage_utils.py
@@ -1,6 +1,7 @@
 from google.cloud import storage
 import csv
 import uuid
+import max_logging
 
 def upload_csv(bucket_name, object_name, data):
   tmp_file_name = f"{str(uuid.uuid4())}.csv"
@@ -13,3 +14,12 @@ def upload_csv(bucket_name, object_name, data):
   bucket = storage_client.get_bucket(bucket_name)
   blob = bucket.blob(object_name)
   blob.upload_from_filename(tmp_file_name)
+  # Set if_generation_match to 0 for the object insertion to be idempotent
+  # such that server-side errors will be auto retried. See
+  # https://cloud.google.com/storage/docs/retry-strategy#idempotency-operations and
+  # https://cloud.google.com/storage/docs/retry-strategy#tools.
+  generation_match_precondition = 0
+  try:
+    blob.upload_from_filename(tmp_file_name, if_generation_match=generation_match_precondition)
+  except Exception as e:
+    max_logging.log(f"An error occurred during upload {object_name} to {bucket_name}: {e}")
