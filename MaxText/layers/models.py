c3f85a7a1a073ab4b9f78d099e106dc9897df1b7
gobbleturk <mattdavidow@google.com>
2024-08-30 02:21:38 +0000

    Add simple MLP decoder block

--- MaxText/layers/models.py
+++ MaxText/layers/models.py
@@ -202,11 +202,15 @@ class Decoder(nn.Module):
       from layers import simple_layer
 
       return simple_layer.SimpleDecoderLayer
+    elif self.config.decoder_block == "simple_mlp":
+      from layers import simple_layer
+
+      return simple_layer.SimpleMlpDecoderLayer
     else:
       raise ValueError(f"Incorrect decoder_block name {self.config.decoder_block=}")
 
   def get_norm_layer(self):
-    if self.config.decoder_block in ("default", "llama2", "mistral", "gemma", "gemma2", "simple"):
+    if self.config.decoder_block in ("default", "llama2", "mistral", "gemma", "gemma2", "simple", "simple_mlp"):
       return RMSNorm
     elif self.config.decoder_block == "gpt3":
       from layers import gpt3
