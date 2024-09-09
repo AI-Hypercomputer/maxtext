4b0af367a6912ab61a90636c74bbf1e0699f17a5
ZhiyuLi-goog <lizhiyu@google.com>
2024-09-09 16:31:04 -0700

    fix

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -508,7 +508,7 @@ class MoeBlock(nn.Module):
       layer_multiply = jnp.multiply(layer_w0_act, layer_w1)
       with jax.named_scope("wo"):
         wo_kernel_axes = ("exp", None, None)
-        wo_kernel = nn.with_logical_constraint(wo_kernel, wo_kernel)
+        wo_kernel = nn.with_logical_constraint(wo_kernel, wo_kernel_axes)
         intermediate_layer = self.get_einsum(rhs_mesh_axes=wo_kernel_axes)("BECH,EHM -> BECM", layer_multiply, wo_kernel)
         intermediate_layer = nn.with_logical_constraint(intermediate_layer, ("activation_batch_no_exp", "activation_exp", None, "activation_embed"))
       with jax.named_scope("combine"):
