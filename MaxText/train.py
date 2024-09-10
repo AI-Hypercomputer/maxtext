9cd935216c3d02b0593701c78d1aa246e6df21fb
aireenmei <aireenmei@gmail.com>
2024-09-10 02:15:29 +0000

    Add eval to convergence test

--- MaxText/train.py
+++ MaxText/train.py
@@ -605,7 +605,7 @@ def train_loop(config, state=None):
 
     write_metrics(writer, local_metrics_file, running_gcs_metrics, metrics, step, config)
 
-    if config.eval_interval > 0 and step > start_step and step % config.eval_interval == 0:
+    if config.eval_interval > 0 and step > start_step and (step + 1) % config.eval_interval == 0:
       assert eval_data_iterator
       cumulative_eval_metrics = {"total_loss": 0.0, "total_weights": 0.0}
       eval_step_count = 0
