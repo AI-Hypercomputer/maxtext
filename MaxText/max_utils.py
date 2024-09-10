33f78323c61c0ac57555dc8156e4b39e98949063
aireenmei <aireenmei@gmail.com>
2024-09-10 02:15:29 +0000

    Add eval to convergence test

--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -123,7 +123,7 @@ def _prepare_metrics_for_json(metrics, step, run_name):
   return metrics_dict
 
 
-def write_metrics_locally(metrics, step, config, file):
+def write_metrics_locally(metrics, step, config, file, is_training=True):
   """Writes metrics locally for testing"""
   if step == 0:
     file.truncate(0)
@@ -131,7 +131,7 @@ def write_metrics_locally(metrics, step, config, file):
   metrics_dict = _prepare_metrics_for_json(metrics, step, config.run_name)
   file.write(str(json.dumps(metrics_dict)) + "\n")
 
-  if step == config.steps - 1:
+  if is_training and step == config.steps - 1:
     file.close()
 
 
@@ -148,11 +148,11 @@ def add_text_to_summary_writer(key, value, summary_writer):
     summary_writer.add_text(key, value)
 
 
-def write_metrics_for_gcs(metrics, step, config, running_metrics):
+def write_metrics_for_gcs(metrics, step, config, running_metrics, is_training=True):
   """Writes metrics to gcs"""
   metrics_dict_step = _prepare_metrics_for_json(metrics, step, config.run_name)
   running_metrics.append(metrics_dict_step)
-  if (step + 1) % config.log_period == 0 or step == config.steps - 1:
+  if is_training and (step + 1) % config.log_period == 0 or step == config.steps - 1:
     start_step = (step // config.log_period) * config.log_period
     metrics_filename = f"metrics_step_{start_step:06}_to_step_{step:06}.txt"
     with open(metrics_filename, "w", encoding="utf8") as metrics_for_gcs:
