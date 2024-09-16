53b498009aa4ba6444a91009ab0a27ce608f9757
Alex Shraer <shralex@gmail.com>
2024-09-16 15:17:12 +0000

    Run code-style on changed files in pre-commit.
    Fixes https://github.com/google/maxtext/issues/735

--- code_style.sh
+++ code_style.sh
@@ -15,18 +15,25 @@
 # Clean up Python codes using Pylint & Pyink
 # Googlers: please run `sudo apt install pipx; pipx install pylint --force; pipx install pyink==23.10.0` in advance
 
-set -e
+set -e # Exit immediately if any command fails
 
 FOLDERS_TO_FORMAT=("MaxText" "pedagogical_examples")
 LINE_LENGTH=$(grep -E "^max-line-length=" pylintrc | cut -d '=' -f 2)
 
+# Check for --check flag
+CHECK_ONLY_PYINK_FLAGS=""
+if [[ "$1" == "--check" ]]; then
+  CHECK_ONLY_PYINK_FLAGS="--check --diff --color"
+fi
+
 for folder in "${FOLDERS_TO_FORMAT[@]}"
 do
-  pyink "$folder" --pyink-indentation=2 --line-length=${LINE_LENGTH}
+  pyink "$folder" ${CHECK_ONLY_PYINK_FLAGS} --pyink-indentation=2 --line-length=${LINE_LENGTH}
 done
 
 for folder in "${FOLDERS_TO_FORMAT[@]}"
 do
+  # pylint doesn't change files, only reports errors.
   pylint "./$folder"
 done
 
