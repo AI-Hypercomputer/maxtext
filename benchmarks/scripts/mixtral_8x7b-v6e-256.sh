f59709f9431b3e9acd1d92746f8e8a0e9fc22624
Qinwen Xu <qinwen@google.com>
2024-10-29 23:28:50 +0000

    add test scripts

---
+++ benchmarks/scripts/mixtral_8x7b-v6e-256.sh
@@ -0,0 +1,2 @@
+python3 benchmarks/benchmark_runner.py --project='tpu-prod-env-multipod' --zone='europe-west4' --device_type=v6e-256 --num_slices=1  --cluster_name='mlperf-v6e-256' \
+--model_name="mixtral_8x7b_dropped_int8" --libtpu_version=20241009 --base_docker_image maxtext_base_image
