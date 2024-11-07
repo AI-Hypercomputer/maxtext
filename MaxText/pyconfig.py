e19e9a8deab5886a21fa79fbc5becb7040fc8329
RissyRan <ran.rissy@gmail.com>
2024-11-07 23:51:32 +0000

    Add more flexibility to configs

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -194,6 +194,7 @@ def validate_and_assign_remat_tensors(keys):
       "key_proj",
       "value_proj",
       "out_proj",
+      "qkv_proj",
   ]
   assert keys["decoder_layer_input"] != "remat", "Cannot remeterialize this tensor with scan_layers=True"
   tensors_on_device = []
