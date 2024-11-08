de209d25f55bdb512a619669e5d03d0122b7e3cb
gobbleturk <mattdavidow@google.com>
2024-11-08 17:14:20 +0000

    base unrealistic model

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -562,42 +562,42 @@ class MoeBlock(nn.Module):
             dispatch, ("activation_exp", "activation_batch_no_exp", None, "activation_embed")
         )
       with jax.named_scope("wi_0"):
-        w0_kernel_axes = ("exp", "embed_no_exp", "mlp") #rawr
-        w0_kernel_axes = ("exp", None, None)
+        w0_kernel_axes = ("exp", None, "mlp") #rawr
         w0_kernel = self.maybe_all_gather_kernel_weight_in_expert_parallelism(w0_kernel, w0_kernel_axes)
         layer_w0 = self.get_einsum(rhs_mesh_axes=w0_kernel_axes)(
             "EBCM,EMH -> EBCH", dispatch, w0_kernel, precision=matmul_precision
         )
         if self.config.activations_in_float32:
-          layer_w0 = layer_w0.astype(jnp.float32)
-        layer_w0 = checkpoint_name(layer_w0, "mlpwi_0")
+          layer_w0 = layer_w0.astype(jnp.float32)  
         layer_w0 = nn.with_logical_constraint(
             layer_w0, ("activation_exp", "activation_batch_no_exp", None, "activation_mlp")
         )
+        layer_w0 = checkpoint_name(layer_w0, "mlpwi_0")
       with jax.named_scope("wi_1"):
-        w1_kernel_axes = ("exp", "embed_no_exp", "mlp") #rawr
-        # w1_kernel = self.maybe_all_gather_kernel_weight_in_expert_parallelism(w1_kernel, w1_kernel_axes)
+        w1_kernel_axes = ("exp", None, "mlp") #rawr
+        w1_kernel = self.maybe_all_gather_kernel_weight_in_expert_parallelism(w1_kernel, w1_kernel_axes)
         layer_w1 = self.get_einsum(rhs_mesh_axes=w1_kernel_axes)(
             "EBCM,EMH -> EBCH", dispatch, w1_kernel, precision=matmul_precision
         )
         if self.config.activations_in_float32:
           layer_w1 = layer_w1.astype(jnp.float32)
-        layer_w1 = checkpoint_name(layer_w1, "mlpwi_1")
+        
         layer_w1 = nn.with_logical_constraint(
             layer_w1, ("activation_exp", "activation_batch_no_exp", None, "activation_mlp")
         )
+        layer_w1 = checkpoint_name(layer_w1, "mlpwi_1")
       layer_w0_act = _convert_to_activation_function(self.config.mlp_activations[0])(layer_w0)
       layer_multiply = jnp.multiply(layer_w0_act, layer_w1).astype(self.dtype)
       with jax.named_scope("wo"):
-        wo_kernel_axes = ("exp", "mlp", "embed_no_exp") #rawr
-        # wo_kernel = self.maybe_all_gather_kernel_weight_in_expert_parallelism(wo_kernel, wo_kernel_axes)
+        wo_kernel_axes = ("exp", "mlp", None) #rawr
+        wo_kernel = self.maybe_all_gather_kernel_weight_in_expert_parallelism(wo_kernel, wo_kernel_axes)
         intermediate_layer = self.get_einsum(rhs_mesh_axes=wo_kernel_axes)(
             "EBCH,EHM -> EBCM", layer_multiply, wo_kernel, precision=matmul_precision
         )
-        intermediate_layer = checkpoint_name(intermediate_layer, "mlpwo")
         intermediate_layer = nn.with_logical_constraint(
             intermediate_layer, ("activation_exp", "activation_batch_no_exp", None, "activation_embed")
         )
+        intermediate_layer = checkpoint_name(intermediate_layer, "mlpwo")
       with jax.named_scope("combine"):
         # Matmul & element wise operation
         output = self.get_einsum(rhs_mesh_axes=mask_axes)(
--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -166,6 +166,7 @@ def validate_model_name(s: str) -> bool:
       "gpt3-22b",
       "gpt3-6b",
       "gpt3-52k",
+      "custom-moe-single-matt"
   )
   if s not in valid_model_names:
     raise ValueError(f"Invalid model name was passed. Got {s}, Valid options {valid_model_names}")
