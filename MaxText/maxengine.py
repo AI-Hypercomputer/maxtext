0e59567dac22c2d1a8b45aac6d88575c8f81e129
ZhiyuLi-goog <lizhiyu@google.com>
2024-11-12 22:17:32 +0000

    walk around quantize_params

--- MaxText/maxengine.py
+++ MaxText/maxengine.py
@@ -131,7 +131,8 @@ class MaxEngine(engine_api.Engine):
     )
 
     if self.model.quant and not self.config.checkpoint_is_quantized:
-      params = self.quantize_params(state, rng3)
+      # params = self.quantize_params(state, rng3)
+      params = state.params
     else:
       params = state.params
     max_utils.print_mem_stats("After load_params")
