481c04a25528dc11fe8c922d968110927fe7b323
Lance Wang <lancewang@google.com>
2024-11-13 19:47:18 +0000

    Updates the script.

--- mt_pgle_tp_nv.sh
+++ mt_pgle_tp_nv.sh
@@ -8,7 +8,7 @@ export WORKLOAD_NAME=lancewang-7b-nv-pgle-tp-${RANDOM:0:2}
 export NUM_NODES=1
 
 # Enable PGLE
-export JAX_ENABLE_PGLE=true
+export JAX_ENABLE_PGLE=false
 export STRICT_CHECKER=true
 
 cat <<EOF > env.txt
@@ -30,18 +30,26 @@ XLA_FLAGS=--xla_gpu_enable_latency_hiding_scheduler=true \
 --xla_gpu_enable_pipelined_all_reduce=true \
 --xla_gpu_enable_while_loop_double_buffering=true \
 --xla_disable_hlo_passes=rematerialization \
---xla_gpu_enable_pgle_accuracy_checker=$STRICT_CHECKER \
+--xla_gpu_pgle_accuracy_checker=PGLE_STRICTNESS_LEVEL_ERROR \
 --xla_gpu_enable_triton_softmax_fusion=false \
 --xla_gpu_enable_all_gather_combine_by_dim=false \
 --xla_gpu_enable_reduce_scatter_combine_by_dim=false
 EOF
 
+# Old flag
+# --xla_gpu_enable_pgle_accuracy_checker=$STRICT_CHECKER \
+
+# New flag
+# --xla_gpu_pgle_accuracy_checker=PGLE_STRICTNESS_LEVEL_ERROR \
+# 
+
 export LOCAL_IMAGE_NAME=us-west1-docker.pkg.dev/supercomputer-testing/lancewang/lance-1107-nv
+# export LOCAL_IMAGE_NAME=us-west1-docker.pkg.dev/supercomputer-testing/lancewang/llama2-xprof_1004_nolayers_nightly_lance
 
 COMMAND="python3 MaxText/train.py MaxText/configs/models/gpu/llama2_7b.yml base_config=base.yml hardware=gpu run_name=maxtext_llama2-7b steps=10 max_target_length=4096 model_name=llama2-7b enable_checkpointing=false attention=cudnn_flash_te dataset_type=synthetic async_checkpointing=false base_output_directory=gs://lancewang-dev-supercomputer-testing/maxtext_gpu logits_dot_in_fp32=false use_iota_embed=true scan_layers=true dcn_pipeline_parallelism=1 dcn_fsdp_parallelism=1 per_device_batch_size=1 ici_tensor_parallelism=4 weight_dtype=bfloat16 remat_policy=full profiler=xplane skip_first_n_steps_for_profiler=5 ";
 
 COMMAND='export LD_LIBRARY_PATH=/usr/local/cuda-12.6/compat:$LD_LIBRARY_PATH;'"${COMMAND}"; 
 
-xpk workload delete --cluster $CLUSTER_NAME --workload $WORKLOAD_NAME; 
+python ../xpk/xpk.py workload delete --cluster $CLUSTER_NAME --workload $WORKLOAD_NAME; 
 
-xpk workload create --cluster $CLUSTER_NAME --workload $WORKLOAD_NAME --command "${COMMAND}" --docker-image=$LOCAL_IMAGE_NAME --device-type=$DEVICE_TYPE --num-nodes=$NUM_NODES --scheduler=gke.io/topology-aware-auto --env-file=env.txt ;
+python ../xpk/xpk.py workload create --cluster $CLUSTER_NAME --workload $WORKLOAD_NAME --command "${COMMAND}" --docker-image=$LOCAL_IMAGE_NAME --device-type=$DEVICE_TYPE --num-nodes=$NUM_NODES --scheduler=gke.io/topology-aware-auto --env-file=env.txt ;
