c3729644aacbb27e0104ccd1a14a0c81df1d430c
Doris26 <zouyujun526@gmail.com>
2024-11-20 23:55:18 +0000

    set tolerance a configurable param with default value 0.1

--- MaxText/train.py
+++ MaxText/train.py
@@ -82,6 +82,8 @@ def validate_train_config(config):
   if not config.base_output_directory.startswith("gs://"):
     max_logging.log("WARNING: 'base_output_directory' might be pointing your local file system")
   assert config.steps > 0, "You must set steps or learning_rate_schedule_steps to a positive integer."
+  if config.tolerance > 1.0 or config.tolerance < 0.0:
+    max_logging.log("WARNING: 'tolerance: allowed percentage of non-sharded parameters' should be between 0.0 and 1.0")
   if config.quantization == "fp8":
     # pylint: disable=line-too-long
     assert (
@@ -536,6 +538,7 @@ def setup_train_loop(config):
   record_goodput(recorder, config, recorder.record_tpu_init_end_time if recorder else None)
   record_goodput(recorder, config, recorder.record_training_preparation_start_time if recorder else None)
   data_iterator, eval_data_iterator = create_data_iterator(config, mesh)
+  tolerance = config.tolerance if config.tolerance else 0.1
 
   state, state_mesh_annotations, data_iterator = max_utils.setup_training_state(
       model, data_iterator, tx, config, init_rng, mesh, checkpoint_manager
@@ -543,7 +546,7 @@ def setup_train_loop(config):
 
   if not config.using_pipeline_parallelism:
     # The vocab tensor(s) of shape [vocab, embed] (and transpose) are not sharded by stage
-    maxtext_utils.assert_params_sufficiently_sharded(state.params, mesh, tolerance=0.02)
+    maxtext_utils.assert_params_sufficiently_sharded(state.params, mesh, tolerance)
   record_goodput(recorder, config, recorder.record_training_preparation_end_time if recorder else None)
   return (
       init_rng,
