32ddb44dda47b48ac0be3991a39f566e5448d081
gobbleturk <mattdavidow@google.com>
2024-12-02 21:07:41 +0000

    nstat and qdisc

--- MaxText/print_networking_goodies.sh
+++ MaxText/print_networking_goodies.sh
@@ -17,3 +17,13 @@ ifconfig
 echo "Printing cat /proc/sys/net/ipv4/tcp_mtu_probing"
 echo "########################################"
 cat /proc/sys/net/ipv4/tcp_mtu_probing
+
+echo "Printing nstat"
+echo "########################################"
+nstat_out=$(nstat)
+echo "nstat: ${nstat_out}"
+
+echo "Printing qdisc"
+echo "########################################"
+qdisc_out=$(tc -s -d qdisc sh dev eth0)
+echo "qdisc: ${qdisc_out}"
