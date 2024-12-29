2edd721f4d6d63d42ad13c321e0091c4567b3692
gobbleturk <mattdavidow@google.com>
2024-12-29 18:35:27 +0000

    The debugging continues

--- MaxText/layers/pipeline.py
+++ MaxText/layers/pipeline.py
@@ -364,20 +364,21 @@ class Pipeline(nn.Module):
     return new_loop_state
 
   def maybe_ag_new_bsw(self, bsw, sharding_info, loop_iter):
-    bsw = jax.lax.cond( loop_iter % self.config.num_pipeline_microbatches == 0, lambda: self.ag_new_bsw(bsw, sharding_info, loop_iter), lambda: bsw)
+    # Update at end of iteration before new repeat
+    bsw = jax.lax.cond( (loop_iter + 1) % self.config.num_pipeline_microbatches == 0, lambda: self.ag_new_bsw(bsw, sharding_info, loop_iter), lambda: bsw)
     return bsw
   
   def ag_new_bsw(self, bsw, sharding_info, loop_iter):
-    _, repeat_idx = self.get_microbatch_and_repeat_ids(loop_iter)
-    jax.debug.print("Loop iter {loop_iter} Old bsw {bsw}", loop_iter=loop_iter, bsw=bsw['params']['mlp']['wi_0']['kernel'][:,:,0,0])
-    #jax.debug.print("hello {loop_iter} {repeat_idx}", loop_iter=loop_iter, repeat_idx=repeat_idx)
+    _, repeat_idx = self.get_microbatch_and_repeat_ids(loop_iter + 1)
+    jax.debug.print("loop_iter {loop_iter} repeat_idx {repeat_idx}", loop_iter=loop_iter, repeat_idx=repeat_idx)
+    #jax.debug.print("Loop iter {loop_iter} Old bsw {bsw}", loop_iter=loop_iter, bsw=bsw['params']['mlp']['wi_0']['kernel'][:,:,0,0])
     new_bw_insert = self.grab_bsw(self.layers.variables,repeat_idx[0])
-    jax.debug.print("Loop iter {loop_iter} bsw to insert {new_bw_insert}", loop_iter=loop_iter, new_bw_insert=new_bw_insert['params']['mlp']['wi_0']['kernel'][:,:,0,0])
+    #jax.debug.print("Loop iter {loop_iter} bsw to insert {new_bw_insert}", loop_iter=loop_iter, new_bw_insert=new_bw_insert['params']['mlp']['wi_0']['kernel'][:,:,0,0])
     new_bw_ag = self.force_ag(new_bw_insert, sharding_info)
     grab_idx_1 = self.grab_bsw(bsw, 1)
     new_full_bsw = self.insert_pytree(bsw, grab_idx_1, 0) # bsw[0] = bsw[1]
     new_full_bsw = self.insert_pytree(new_full_bsw, new_bw_ag, 1) # bsw[1] = new_bw_ag
-    jax.debug.print("Loop iter {loop_iter} new bsw {bsw}", loop_iter=loop_iter, bsw=bsw['params']['mlp']['wi_0']['kernel'][:,:,0,0])
+    #jax.debug.print("Loop iter {loop_iter} new bsw {new_full_bsw}", loop_iter=loop_iter, new_full_bsw=new_full_bsw['params']['mlp']['wi_0']['kernel'][:,:,0,0])
     return new_full_bsw
 
   def grab_bsw(self, vars, idx):
