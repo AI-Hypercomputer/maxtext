abf6420a4e19a01b498587c2b7e69f9f6c416f5e
Hengtao Guo <hengtaoguo@google.com>
2025-01-10 21:21:06 +0000

    Allow nsys profile saved to local directory

--- MaxText/profiler.py
+++ MaxText/profiler.py
@@ -72,11 +72,14 @@ class Profiler:
       else:
         max_logging.log("WARNING: library for nsys was not loaded \n" "profiler has no effect")
         return
-      # Popen() instead of run() for non-blocking behavior
-      if shutil.which("gsutil") is not None:
-        subprocess.Popen(["gsutil", "cp", "*nsys-rep", self.output_path])  # pylint: disable=consider-using-with
+      if self.output_path.startswith("gs://"):
+        # Popen() instead of run() for non-blocking behavior
+        if shutil.which("gsutil") is not None:
+          subprocess.Popen(["gsutil", "cp", "*nsys-rep", self.output_path])  # pylint: disable=consider-using-with
+        else:
+          max_logging.log("WARNING: gsutil is not installed or not found in the system's PATH. Skipping upload...")
       else:
-        max_logging.log("WARNING: gsutil is not installed or not found in the system's PATH. Skipping upload...")
+        subprocess.Popen(["cp", "*nsys-rep", self.output_path])
     elif self.mode == "xplane":
       jax.profiler.stop_trace()
 
