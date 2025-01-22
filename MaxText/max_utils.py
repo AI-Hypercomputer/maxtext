3ed19d270947a41614409ff5aa557b203be80212
Matt Irvine <mirvine@google.com>
2025-01-22 22:18:10 +0000

    Initialize distributed IDs on CPU

--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -316,6 +316,8 @@ def initialize_jax_for_cpu(raw_keys):
       num_processes=int(os.environ.get("JAX_PROCESS_COUNT")),
       initialization_timeout=raw_keys["jax_distributed_initialization_timeout"],
   )
+  ocp.multihost.initialize_runtime_to_distributed_ids()
+  ocp.multihost.initialize_distributed_to_device_ids()
 
 
 def initialize_jax_for_tpu_with_emergency_checkpointing(raw_keys):
