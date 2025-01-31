b0a38f09a9ab91ce5ea4a9f802c3953976e21c16
RoshaniN <roshanin@google.com>
2025-01-31 01:54:49 +0000

    New changes to help run tests.

--- docker_build_pathways_containers.sh
+++ docker_build_pathways_containers.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 apt-get install docker-compose-plugin
 docker compose version
 gcloud auth configure-docker us-docker.pkg.dev --quiet
@@ -6,7 +22,7 @@ cd utils_pathways
 docker compose down
 sleep 2
 docker compose up &
-sleep 5
+sleep 15
 docker compose ps
 
 export JAX_PLATFORMS="proxy"
