57743ba875c1002e5547035e14af32db258f7b3f
RoshaniN <roshanin@google.com>
2025-02-06 21:56:03 +0000

    Move installation to script

--- docker_build_pathways_containers.sh
+++ docker_build_pathways_containers.sh
@@ -18,13 +18,13 @@ apt-get install docker-compose-plugin
 docker compose version
 gcloud auth configure-docker us-docker.pkg.dev --quiet
 
-cd utils_pathways
-docker compose down
-sleep 10
-docker compose up &
-sleep 20
-docker compose ps
+# cd utils_pathways
+# docker compose down
+# sleep 10
+# docker compose up &
+# sleep 20
+# docker compose ps
 
-export JAX_PLATFORMS="proxy"
-export JAX_BACKEND_TARGET="grpc://localhost:29000"
-echo "Setup for Pathways containers complete!"
+# export JAX_PLATFORMS="proxy"
+# export JAX_BACKEND_TARGET="grpc://localhost:29000"
+echo "Docker compose installation complete!"
