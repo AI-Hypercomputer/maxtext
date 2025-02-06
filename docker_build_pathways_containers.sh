3c9a8e2f4a9ee309a1272a925caba79cfa1c68da
RoshaniN <roshanin@google.com>
2025-02-06 01:56:49 +0000

    Trial to get Pathways working.

--- docker_build_pathways_containers.sh
+++ docker_build_pathways_containers.sh
@@ -20,9 +20,9 @@ gcloud auth configure-docker us-docker.pkg.dev --quiet
 
 cd utils_pathways
 docker compose down
-sleep 2
+sleep 10
 docker compose up &
-sleep 15
+sleep 20
 docker compose ps
 
 export JAX_PLATFORMS="proxy"
