7f0d045fa16a987b1f223b73081ccc9714099b1d
RoshaniN <roshanin@google.com>
2025-02-06 23:12:36 +0000

    More changes.

--- docker_build_pathways_containers.sh
+++ docker_build_pathways_containers.sh
@@ -18,13 +18,11 @@ apt-get install docker-compose-plugin
 docker compose version
 gcloud auth configure-docker us-docker.pkg.dev --quiet
 
-# cd utils_pathways
-# docker compose down
-# sleep 10
-# docker compose up &
-# sleep 20
-# docker compose ps
+cd utils_pathways
+docker compose down
+sleep 10
+docker compose up &
+sleep 20
+docker compose ps
 
-# export JAX_PLATFORMS="proxy"
-# export JAX_BACKEND_TARGET="grpc://localhost:29000"
 echo "Docker compose installation complete!"
