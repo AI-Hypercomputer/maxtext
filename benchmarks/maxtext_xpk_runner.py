296da869b6b79a948e1cf4b005c217c72b29fdea
Victor Barr <vbarr@google.com>
2025-02-06 15:58:53 -0800

    Upload HLOs

--- benchmarks/maxtext_xpk_runner.py
+++ benchmarks/maxtext_xpk_runner.py
@@ -578,6 +578,8 @@ def generate_xpk_workload_cmd(
     docker_image_flag = f'--base-docker-image="{wl_config.base_docker_image}"'
 
   print(f'User command: {user_command}')
+  HLO_FOLDER='gs://DIR/vbarr_hlos'
+
   return (
       (
           f'{workload_create_command}'
@@ -593,6 +595,7 @@ def generate_xpk_workload_cmd(
           f' --workload={name}'
           f' --priority={wl_config.priority}'
           f' --max-restarts={wl_config.max_restarts}'
+          f' --debug-dump-gcs={HLO_FOLDER}/{model.model_name}_{date}_{num_slices}_{temp_post_fix}',
           # ' --use-vertex-tensorboard'
           # f' --experiment-name={test_purpose_name}'
           f' {additional_flags}'
