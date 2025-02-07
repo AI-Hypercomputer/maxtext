659782e99ce2778dd429f2547108b68530b5b487
Obliviour <vbarr@google.com>
2025-02-07 21:04:08 +0000

    bq draft

--- benchmarks/maxtext_xpk_runner.py
+++ benchmarks/maxtext_xpk_runner.py
@@ -24,17 +24,16 @@
 # Enable hlo dumps.
 
 import dataclasses
-import datetime
 import enum
 import os
 import random
 import string
 import subprocess
-import sys
-import tempfile
 import time
 
 import maxtext_trillium_model_configs as model_configs
+from command_utils import run_command_with_updates
+
 import xla_flags_library as xla_flags
 
 # Assumes you built maxtext dep image.
@@ -79,6 +78,7 @@ class WorkloadConfig:
   xpk_path: str = '~/xpk'
   pathways_config: PathwaysConfig = None
   run_name: str = None
+  generate_metrics_and_upload_to_big_query: bool = True
 
 
 @dataclasses.dataclass
@@ -89,192 +89,6 @@ class XpkClusterConfig:
   project: str
   zone: str
   device_type: str
-
-
-def chunks(lst: list, n: int):
-  """Return a list of n-sized chunks from lst.
-
-  Args:
-    lst: input list to get chunks from.
-    n: size of each chunk.
-
-  Returns:
-    List of n-sized chunks for lst.
-  """
-  return [lst[i : i + n] for i in range(0, len(lst), n)]
-
-
-def make_tmp_files(per_command_name):
-  """Make temporary files for each command.
-
-  Args:
-    per_command_name: list of command names.
-
-  Returns:
-    A list of temporary files for each command.
-  """
-  # Supports removal of spaces from command names before converting to file name.
-  return [
-      tempfile.NamedTemporaryFile(
-          delete=False, prefix=command.replace(' ', '-') + '-'
-      )
-      for command in per_command_name
-  ]
-
-
-def run_commands(commands, jobname, per_command_name, batch=10, dry_run=False):
-  """Run commands in groups of `batch`.
-
-  Args:
-    commands: list of command.
-    jobname: the name of the job.
-    per_command_name: list of command names.
-    batch: number of commands to run in parallel.
-    dry_run: enables dry_run if set to true.
-
-  Returns:
-    0 if successful and 1 otherwise.
-  """
-  temporary_files_batches = chunks(make_tmp_files(per_command_name), batch)
-  commands_batched = chunks(commands, batch)
-  per_command_name_batches = chunks(per_command_name, batch)
-
-  print(
-      f'Breaking up a total of {len(commands)} commands into'
-      f' {len(commands_batched)} batches'
-  )
-  if dry_run:
-    print('Pretending all the jobs succeeded')
-    return 0
-
-  max_return_code = 0
-  for i, _ in enumerate(commands_batched):
-    print(f'Dispatching batch {i}/{len(commands_batched)}')
-    batch_max_return_code, _ = run_command_batch(
-        commands_batched[i],
-        jobname,
-        per_command_name_batches[i],
-        temporary_files_batches[i],
-    )
-    max_return_code = max(max_return_code, batch_max_return_code)
-    if max_return_code > 0:
-      return max_return_code
-  return max_return_code
-
-
-def run_command_batch(commands, jobname, per_command_name, output_logs):
-  """Runs commands in parallel.
-
-  Args:
-    commands: list of n commands, each command is a a list of strings
-    jobname: Useful debugging name for the group of commands
-    per_command_name: specific name per task
-    output_logs: list of n log paths, each command will output to each log.
-
-  Returns:
-    The max return code and a list of all the return codes.
-  """
-
-  children = []
-  start_time = datetime.datetime.now()
-  for i, command in enumerate(commands):
-    children.append(
-        # subprocess managed by list pylint: disable=consider-using-with
-        subprocess.Popen(
-            command, stdout=output_logs[i], stderr=output_logs[i], shell=True
-        )
-    )
-
-  while True:
-    returncodes = [child.poll() for child in children]
-    max_returncode = max([0] + [r for r in returncodes if r is not None])
-    completed = len([r for r in returncodes if r is not None])
-    total = len(returncodes)
-    seconds_elapsed = (datetime.datetime.now() - start_time).total_seconds()
-    if completed < total:
-      slow_worker_index = returncodes.index(None)
-      slow_worker_text = per_command_name[slow_worker_index]
-      slow_str = (
-          f', task {slow_worker_text} still working, logfile'
-          f' {output_logs[slow_worker_index].name}'
-      )
-    else:
-      slow_str = ''
-    print(
-        f'[t={seconds_elapsed:.2f}, {jobname}] Completed'
-        f' {completed}/{total}{slow_str}'
-    )
-    if max_returncode > 0:
-      failing_index = [
-          i for i, x in enumerate(returncodes) if x is not None and x > 0
-      ][0]
-      print(f'Terminating all {jobname} processes since at least one failed.')
-      print(
-          f'Failure is {per_command_name[failing_index]}'
-          f' and logfile {output_logs[failing_index].name}'
-      )
-      for child in children:
-        child.terminate()
-      break
-
-    if completed == total:
-      break
-
-    time.sleep(1)
-  return max_returncode, returncodes
-
-
-def run_command_with_updates(command, task, verbose=True) -> int:
-  """Generic run commands function with updates.
-
-  Args:
-    command: command to execute
-    task: user-facing name of the task
-    global_args: user provided arguments for running the command.
-    verbose: shows stdout and stderr if set to true. Set to True by default.
-
-  Returns:
-    0 if successful and 1 otherwise.
-  """
-
-  if verbose:
-    print(
-        f'Task: `{task}` is implemented by `{command}`, streaming output live.'
-    )
-    with subprocess.Popen(
-        command,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        shell=True,
-    ) as child:
-      i = 0
-      while True:
-        return_code = child.poll()
-        if return_code is None:
-          print(f'Waiting for `{task}`, for {i} seconds')
-          time.sleep(1)
-          i += 1
-        else:
-          print(f'Task: `{task}` terminated with code `{return_code}`')
-          return return_code
-  else:
-    print(
-        f'Task: `{task}` is implemented by `{command}`, hiding output unless'
-        ' there is an error.'
-    )
-    try:
-      subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-      print(
-          f'Task: `{task}` terminated with ERROR `{e.returncode}`, printing'
-          ' logs'
-      )
-      print('*' * 80)
-      print(e.output)
-      print('*' * 80)
-      return e.returncode
-    print(f'Task: `{task}` succeeded.')
-    return 0
 
 
 def _get_config_tuning_params(wl_config: WorkloadConfig):
@@ -376,6 +190,12 @@ def build_user_command(
   else:
     run_name_command=f'run_name={name}'
 
+  enable_metrics_cmd=""
+  if wl_config.generate_metrics_and_upload_to_big_query:
+    # 'metrics_file=metrics.txt
+    # Save metrics to gcs bucket so that we can upload them to bq in post processing.
+    enable_metrics_cmd = 'gcs_metrics=true'
+
   # Construct the command string with proper formatting and line continuations
   command = ' '.join([
       f'{install_libtpu_cmd}',
@@ -390,7 +210,8 @@ def build_user_command(
       f'model_name={wl_config.model.model_type}',
       f'base_output_directory={wl_config.base_output_directory}',
       f'{vertex_tensorboard}',
-      f'{run_name_command}'
+      f'{run_name_command}',
+      f'{enable_metrics_cmd}'
   ])
   return command
 
@@ -581,6 +402,15 @@ def generate_xpk_workload_cmd(
   else:
     docker_image_flag = f'--base-docker-image="{wl_config.base_docker_image}"'
 
+  upload_metrics_to_bq_cmd = ""
+  if wl_config.generate_metrics_and_upload_to_big_query:
+    # TODO pass arguments to bq from the maxtext runner.
+    # TODO only run this command if the previous succeeds using &&
+    # TODO enable generate_metrics_and_upload_to_big_query by default in main of this file
+    # TODO (optionally) make it so that this upload step is done on local device instead of within the workload.
+    upload_metrics_to_bq_cmd = " && python3 benchmarks/upload_metrics_to_bq.py ARGS"
+
+
   print(f'User command: {user_command}')
   return (
       (
@@ -600,6 +430,7 @@ def generate_xpk_workload_cmd(
           # ' --use-vertex-tensorboard'
           # f' --experiment-name={test_purpose_name}'
           f' {additional_flags}'
+          f' {upload_metrics_to_bq_cmd}'
       ),
       name,
   )
@@ -663,7 +494,7 @@ def on_device_benchmark_runner(
 # Run maxtext_xpk_runner.py as a script for executing multiple workloads pythonically!
 def main() -> int:
   # Variables to configure:
-  output_bucket = 'gs://DIR'
+  output_bucket = 'gs://maxtext-experiments-tpem/'
   base_docker_image = _DEFAULT_MAXTEXT_BASE_DOCKER_IMAGE_NAME
 
   # Set up the clusters to run workloads on!
---
+++ benchmarks/upload_metrics_to_bq.py
@@ -0,0 +1,186 @@
+"""
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ """
+
+from typing import Sequence
+from statistics import median
+from command_utils import run_command_with_updates
+from benchmark_db_utils import install_mantaray_locally
+from benchmark_db_utils import write_run
+from benchmark_db_utils import DEFAULT_LOCAL_DIR
+import dataclasses
+import getpass
+import json
+
+
+@dataclasses.dataclass
+class Metrics:
+  avg_tflops_per_sec: float
+  avg_tokens_per_sec: float
+  median_step_time: float
+  e2e_step_time: float
+
+
+
+def download_metrics_file_locally(metrics_gcs_file: str, local_file: str) -> int:
+  command = f"gsutil cp {metrics_gcs_file} {local_file}"
+  return run_command_with_updates(command, f"Download {metrics_gcs_file} in {local_file}")
+
+# Get the last n datapoints for a target metric
+def get_last_n_data(metrics_file, target, n=10):
+  last_n_data = []
+  with open(metrics_file, 'r', encoding='utf8') as file:
+    lines = file.readlines()
+    for line in lines[::-1]:
+      metrics = json.loads(line)
+      if target in metrics:
+        last_n_data.append(metrics[target])
+        if len(last_n_data) >= n:
+          break
+  return last_n_data
+
+# Get the average of the last n datapoints for a specific target metric
+def get_metric_average(metrics_file, target, n=10):
+  last_n_data = get_last_n_data(metrics_file, target, n=n)
+  return sum(last_n_data) / len(last_n_data)
+
+
+def get_metric_median(metrics_file, target, n=10):
+  last_n_data = get_last_n_data(metrics_file, target, n=n)
+  return median(last_n_data)
+
+def get_metrics_sum(metrics_file, target, n=10):
+  last_n_data = get_last_n_data(metrics_file, target, n=n)
+  return sum(last_n_data)
+
+# metric file example:
+# {"learning/grad_norm": 1.0000004768371582, "learning/loss": 10.8693265914917, "learning/moe_lb_loss": 0.0, "learning/param_norm": 8122.166015625, "learning/raw_grad_norm": 5.087409973144531, "learning/total_weights": 79694.0, "perf/step_time_seconds": 4.986376, "perf/per_device_tflops": 172.941153140736, "perf/per_device_tflops_per_sec": 34.6827341421377, "perf/per_device_tokens": 24576.0, "perf/per_device_tokens_per_sec": 4928.629529742643, "learning/current_learning_rate": 2.9999999242136255e-05, "step": 0.0, "run_name": "mattdavidow-train-base"}
+
+# It is important to parse the last n steps avoiding steps that have tracing turned on, by default last_n_steps should be total_steps - 10 to avoid initial tracing settings.
+# TODO() Support avoiding tracing when the user explicitly enables tracing for other steps.
+def parse_metrics(local_metrics_file, total_steps, last_n_steps=10) -> Metrics:
+  avg_tflops = get_metric_average(local_metrics_file, "perf/per_device_tflops_per_sec", n=last_n_steps)
+  avg_tokens_per_second = get_metric_average(local_metrics_file, "perf/per_device_tokens_per_sec", n=last_n_steps)
+  median_step_time = get_metric_median(local_metrics_file, "perf/step_time_seconds", n=last_n_steps)
+  e2e_step_time = get_metrics_sum(local_metrics_file, "perf/step_time_seconds", n=total_steps)
+
+  metrics = Metrics(
+    avg_tflops_per_sec=avg_tflops,
+    avg_tokens_per_sec=avg_tokens_per_second,
+    median_step_time=median_step_time,
+    e2e_step_time=e2e_step_time)
+
+  return metrics
+
+# Args
+# metric gcs file location
+#
+def main(argv: Sequence[str]) -> None:
+  metrics_gcs_file = argv[0]
+  model_id = argv[1]
+  hardware_id = argv[2]
+  software_id = argv[3]
+  number_of_chips = argv[4]
+  container_image_name = argv[5]
+  global_batch_size = argv[6]
+  precision = argv[7]
+  optimizer = argv[8]
+  seq_length = argv[9]
+  number_of_steps = argv[10]
+  xla_flags = argv[11]
+  dataset = argv[12]
+
+  local_dir = DEFAULT_LOCAL_DIR
+
+  # 1.Download metrics from the GCS Bucket
+  local_metrics_file = local_dir
+  rc = download_metrics_file_locally(metrics_gcs_file=metrics_gcs_file, local_file=local_metrics_file)
+  if rc != 0:
+    exit(rc)
+
+  # 2.Install MantaRay locally
+  rc = install_mantaray_locally(local_dir)
+  if rc != 0:
+    exit(rc)
+
+  # 3.Parse Metrics
+  # If there are more than 10 steps, have a buffer to avoid profiling bad perf:
+  if number_of_steps - 10 > 0:
+    compute_metrics_of_n_steps = number_of_steps - 10
+  else:
+    compute_metrics_of_n_steps = number_of_steps
+
+  metrics_from_file = parse_metrics(local_metrics_file, number_of_steps, compute_metrics_of_n_steps)
+
+  # TODO() convert hardware_id to bq acceptable term
+  hardware_id = hardware_id
+  # TODO() convert number of chips to number of nodes (number of vms)
+  # Trillium 4 chips per vm
+  number_of_nodes = number_of_chips / 4
+
+  # TODO() convert tflops to MFU based on hardware_id
+  # Trillium 918
+  avg_mfu = metrics_from_file.avg_tflops_per_sec / 918
+
+  # TODO() determine runtype
+  run_type = "MaxText"
+
+  run_release_status = "local"
+
+  # TODO() support env variables
+  env_vars = ""
+
+  # TODO() support framework config in json
+  framework_config = ""
+
+  # TODO() convert hardware_id to topology
+  topology = "16x16"
+
+
+  # Load metrics to bq
+  write_run(
+    model_id=model_id,
+    hardware_id=hardware_id,
+    software_id=software_id,
+    number_of_nodes=number_of_nodes,
+    number_of_chips=number_of_chips,
+    container_image_name=container_image_name,
+    global_batch_size=global_batch_size,
+    precision=precision,
+    optimizer=optimizer,
+    seq_length=seq_length,
+    median_step_time=metrics_from_file.median_step_time,
+    e2e_time=metrics_from_file.e2e_step_time,
+    number_of_steps=number_of_steps,
+    mfu=avg_mfu,
+    tokens_per_second=metrics_from_file.avg_tokens_per_sec,
+    writer_path=local_dir,
+    run_success=True,
+    run_type=run_type,
+    run_release_status=run_release_status,
+    other_metrics_in_json="",
+    nccl_driver_nickname=None,
+    env_variables=env_vars,
+    framework_config_in_json=framework_config,
+    xla_flags=xla_flags,
+    topology=topology,
+    dataset=dataset,
+    num_of_superblock=None,
+    update_person_ldap=getpass.getuser(),
+    comment="",
+    is_test=False,
+  )
+
+
