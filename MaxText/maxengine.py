d1f5a6e13880ef2190dc7708783ce14398ef7952
Lihao Ran <ranlihao@google.com>
2025-02-14 22:16:34 +0000

    test multisampling

--- MaxText/maxengine.py
+++ MaxText/maxengine.py
@@ -368,6 +368,7 @@ class MaxEngine(engine_api.Engine):
         nucleus_topp=self.config.decode_sampling_nucleus_p,
         temperature=self.config.decode_sampling_temperature,
     )
+    print(f"{first_generated_token.shape=}")
 
     all_valid = jnp.ones(first_generated_token.shape, dtype=jnp.int8)
     result = engine_api.ResultTokens(
@@ -544,9 +545,10 @@ class MaxEngine(engine_api.Engine):
     out_logits = jax.lax.with_sharding_constraint(out_logits, self.replicated_sharding)
     new_cache = jax.lax.with_sharding_constraint(new_vars["cache"], self.kv_cache_shardings)
 
-    logging.warning(f"{self.config.decode_sampling_strategy=}")
-    logging.warning(f"{self.config.decode_sampling_top_k=}")
+    print(f"{self.config.decode_sampling_strategy=}")
+    print(f"{self.config.decode_sampling_top_k=}")
     # sampling tokens
+    print(f"{out_logits.shape=}")
     new_token = inference_utils.sampling(
         out_logits,
         rng,
@@ -555,7 +557,7 @@ class MaxEngine(engine_api.Engine):
         nucleus_topp=self.config.decode_sampling_nucleus_p,
         temperature=self.config.decode_sampling_temperature,
     )
-    logging.warning(f"{new_token.shape=}")
+    print(f"{new_token.shape=}")
 
     all_valid = jnp.ones(new_token.shape, dtype=jnp.int8)
     result = engine_api.ResultTokens(
