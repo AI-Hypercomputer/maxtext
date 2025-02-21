5f65fc158458a285eadd09ca9ad2459e5f2ac4d0
wang2yn84 <lancewang@google.com>
2025-02-21 20:33:52 +0000

    Create a new test for checkpoint conversion.

--- MaxText/tests/hf_checkpoint_conversion_checker.py
+++ MaxText/tests/hf_checkpoint_conversion_checker.py
@@ -1,5 +1,5 @@
 """
-Copyright 2023 Google LLC
+Copyright 2025 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,8 +21,6 @@ from safetensors import safe_open
 import pathlib
 import jax
 import numpy as np
-from MaxText.llama_or_mistral_ckpt import permute_to_match_maxtext_rope
-from MaxText.llama_mistral_mixtral_orbax_to_hf import unpermute_from_match_maxtext_rope
 import argparse
 
 
@@ -98,36 +96,6 @@ def compare_pytrees(tree1, tree2, atol=0.001):
       print(f"The issue is with {key}")
 
   print(f"All {len(named_leaves1.keys())} leaves match within tolerance.")
-
-
-def test_huggingface_to_maxtext_back_to_huggingface_flow():
-  base_num_query_heads = 16
-  head_dim = 32
-  wq = np.arange(base_num_query_heads * head_dim * base_num_query_heads * head_dim, dtype=np.float16).reshape(
-      base_num_query_heads * head_dim, base_num_query_heads * head_dim
-  )
-  wq1 = wq.transpose()
-  wq2 = np.reshape(wq1, [base_num_query_heads * head_dim, base_num_query_heads, head_dim])
-
-  wq3 = permute_to_match_maxtext_rope(wq2)
-  stack_shape = (1,)
-  x = np.zeros(stack_shape + wq3.shape, dtype=np.float16)
-  x[0, ...] = wq3
-  x = np.transpose(x, axes=(1, 0, 2, 3))
-
-  x = x[:, 0, :, :]
-  wq4 = unpermute_from_match_maxtext_rope(x, "llama3.1")
-  wq5 = wq4.reshape(base_num_query_heads * head_dim, base_num_query_heads * head_dim)
-  wq6 = wq5.transpose()
-
-  if not np.array_equal(wq, wq6):
-    print("Test failed: wq does not match wq6")
-
-  if not np.array_equal(wq1, wq5):
-    print("Test failed: wq1 does not match wq5")
-
-  if not np.array_equal(wq2, wq4):
-    print("Test failed: wq2 does not match wq4")
 
 
 def main():
