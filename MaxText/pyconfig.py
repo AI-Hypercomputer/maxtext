6303ff64a524214134157a69a25f9d22a130359a
Qinwen Xu <qinwen@google.com>
2025-02-26 05:33:15 +0000

    impl with all to all

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -186,6 +186,7 @@ def validate_model_name(s: str) -> bool:
       "llama3.1-8b",
       "llama3.1-70b",
       "llama3.1-405b",
+      "subsup",
       "mistral-7b",
       "mixtral-8x7b",
       "mixtral-8x22b",
