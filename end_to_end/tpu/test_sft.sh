99293ef92bfa08906bc27f919e36b7c5d97c03d6
SurbhiJainUSC <sjsurbhi@google.com>
2025-03-03 22:56:20 +0000

    Update test script for SFT

--- end_to_end/tpu/test_sft.sh
+++ end_to_end/tpu/test_sft.sh
@@ -6,17 +6,45 @@
 set -xe
 
 RUN_NAME=sft-$(date +%Y-%m-%d-%H-%M-%S)
+STEPS=100
+LOSS_THRESHOLD=100.0 # Set to large value so test is guaranteed to pass
 
+# Gemma2-2b
 PRE_TRAINED_MODEL=gemma2-2b
 PRE_TRAINED_MODEL_TOKENIZER=google/gemma-2-2b-it
-PRE_TRAINED_MODEL_CKPT_PATH=gs://maxtext-model-checkpoints/gemma2-2b/2025-01-29-19-26/scanned
+PRE_TRAINED_MODEL_CKPT_PATH=$(gcloud storage ls gs://maxtext-model-checkpoints/gemma2-2b | sort -r | head -1)
 BASE_OUTPUT_DIRECTORY=gs://runner-maxtext-logs
 
-# HF pipeline
+# SFT with HF pipeline
 python MaxText/sft_trainer.py MaxText/configs/sft.yml \
     run_name=${RUN_NAME}-hf base_output_directory=${BASE_OUTPUT_DIRECTORY} \
-    model_name=${PRE_TRAINED_MODEL} load_parameters_path=${PRE_TRAINED_MODEL_CKPT_PATH}/0/items \
+    model_name=${PRE_TRAINED_MODEL} load_parameters_path=${PRE_TRAINED_MODEL_CKPT_PATH}/scanned/0/items \
     dataset_type=hf hf_access_token=$HF_TOKEN tokenizer_path=${PRE_TRAINED_MODEL_TOKENIZER} \
-    per_device_batch_size=0.5 allow_split_physical_axes=True \
-    ici_data_parallelism=2 ici_tensor_parallelism=2 ici_fsdp_parallelism=1 \
-    steps=10 max_target_length=1024
+    per_device_batch_size=1 \
+    steps=${STEPS} max_target_length=1024 \
+    metrics_file=sft-hf-metrics.txt
+
+# Assert training loss is smaller than input LOSS_THRESHOLD
+python3 end_to_end/tpu/eval_assert.py final_loss sft-hf-metrics.txt $LOSS_THRESHOLD
+
+# Decode
+python MaxText/decode_sft.py MaxText/configs/sft.yml \
+    run_name=${RUN_NAME}-hf-decode \
+    model_name=${PRE_TRAINED_MODEL} tokenizer_path=${PRE_TRAINED_MODEL_TOKENIZER} \
+    load_parameters_path=${BASE_OUTPUT_DIRECTORY}/${RUN_NAME}-hf/checkpoints/0/items \
+    per_device_batch_size=1 max_prefill_predict_length=512 max_target_length=1024 \
+    attention=dot_product decode_sampling_strategy=weighted decode_sampling_temperature=.00001 prompt="Suggest some famous landmarks in London."
+
+python MaxText/decode_sft.py MaxText/configs/sft.yml \
+    run_name=${RUN_NAME}-hf-decode \
+    model_name=${PRE_TRAINED_MODEL} tokenizer_path=${PRE_TRAINED_MODEL_TOKENIZER} \
+    load_parameters_path=${BASE_OUTPUT_DIRECTORY}/${RUN_NAME}-hf/checkpoints/0/items \
+    per_device_batch_size=1 max_prefill_predict_length=512 max_target_length=1024 \
+    attention=dot_product decode_sampling_strategy=weighted decode_sampling_temperature=.00001 prompt="What are the classic cocktails that every bartender should know how to make and what are their ingredients?"
+
+python MaxText/decode_sft.py MaxText/configs/sft.yml \
+    run_name=${RUN_NAME}-hf-decode \
+    model_name=${PRE_TRAINED_MODEL} tokenizer_path=${PRE_TRAINED_MODEL_TOKENIZER} \
+    load_parameters_path=${BASE_OUTPUT_DIRECTORY}/${RUN_NAME}-hf/checkpoints/0/items \
+    per_device_batch_size=1 max_prefill_predict_length=512 max_target_length=1024 \
+    attention=dot_product decode_sampling_strategy=weighted decode_sampling_temperature=.00001 prompt="Which famous landmarks should I visit in London?"
