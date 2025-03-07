b72707d7ac6bb6c4d25eb83e289dec31e849597e
wang2yn84 <lancewang@google.com>
2025-03-07 23:12:42 +0000

    Update script.

--- mt_train_1k.sh
+++ mt_train_1k.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# export MODEL_NAME=llama2-7b
+export MODEL_NAME=llama2-7b
 
 # Launch llama2 70b
 # export MODEL_NAME=llama2-70b
 
 # Launch llama3.1 405b
-export MODEL_NAME=llama3.1-405b
+# export MODEL_NAME=llama3.1-405b
 
 MODEL_SIZE=$(echo $MODEL_NAME | grep -o '[0-9]\+b')
 
@@ -114,7 +114,7 @@ EOF
 
 
 
-# export LOCAL_IMAGE_NAME=us-west1-docker.pkg.dev/supercomputer-testing/lancewang/lance-1124-tp-fix2 # with fix2
+export LOCAL_IMAGE_NAME=us-west1-docker.pkg.dev/supercomputer-testing/lancewang/lance-1124-tp-fix2 # with fix2
 
 # export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-0106-pinned
 # export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-0206-nightly
@@ -142,8 +142,22 @@ EOF
 # export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-nv-0220-original-script
 # export LOCAL_IMAGE_NAME=us-central1-docker.pkg.dev/supercomputer-testing/yangyuwei-maxtext/maxtext-stable:latest
 
-# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-1124-tp-fix2-mantaray-te2
-export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_stable_stack_0305
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-1124-tp-fix2-mantaray-te2 # Somehow doesn't work
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_stable_stack_0305:working # cuda 12.8
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_stable_stack_0226:latest # Not build successfully
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/lance-mantaray_maxtext_jsts_gpu_a4_02252025-nv-fix2 # Work on a3u, not a3+
+
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_stable_stack_0206_pure # Pure version, works!
+# export LOCAL_IMAGE_NAME=gcr.io/tpu-prod-env-multipod/maxtext_gpu_stable_stack_nightly_jax:2025-03-05  # Lack of network
+# export LOCAL_IMAGE_NAME=gcr.io/tpu-prod-env-multipod/maxtext_gpu_stable_stack_nightly_jax:2025-02-25 # Lack of network
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_jax_nightly_0225_pure # NCCL connection issue, cuda 12.8
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_jax_nightly_0305_pure # NCCL connection issue, cuda 12.8
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_jax_nightly_0221_pure # NCCL connection issue, cuda 12.6
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_jax_nightly_0206_pure
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_0305_pure
+# export LOCAL_IMAGE_NAME=gcr.io/supercomputer-testing/maxtext_sts_0206_pure
+# tfds_nightly==4.9.7.dev202503040044
+# 4.9.2.dev202308090034
 
 call_config() {
 
@@ -167,6 +181,7 @@ call_config() {
 
     local DCN_FSDP=${args[DCN_FSDP]}
     local DCN_PP=${args[DCN_PP]:-1}
+    PP_MBS=$(($DCN_PP * 2))
     local NUM_LAYERS_PER_PP_STAGE=${args[NUM_LAYERS_PER_PP_STAGE]:-1} # Layers are modified to 128 for short term solution
     # export NUM_LAYERS_PER_PP_STAGE=$(expr 126 / $DCN_PP)
 
@@ -182,10 +197,10 @@ call_config() {
 
     echo 'NUM_NODES' ${NUM_NODES} 'PER_DEVICE_BATCH_SIZE' ${PER_DEVICE_BATCH_SIZE} 'ICI_TP' ${ICI_TP} 'DCN_FSDP' ${DCN_FSDP} 'DCN_PP' ${DCN_PP} 'NUM_LAYERS_PER_PP_STAGE' ${NUM_LAYERS_PER_PP_STAGE} 'REMAT_POLICY' ${REMAT_POLICY} 'ATTENTION' ${ATTENTION} WORKLOAD_NAME ${WORKLOAD_NAME}
 
-    COMMAND="python3 MaxText/train.py MaxText/configs/models/gpu/$CONFIG_NAME.yml hardware=gpu run_name=$RUN_NAME steps=10 max_target_length=4096 model_name=$MODEL_NAME enable_checkpointing=false attention=$ATTENTION dataset_type=synthetic async_checkpointing=false base_output_directory=$OUTPUT_BUCKET logits_dot_in_fp32=false use_iota_embed=true scan_layers=true ici_tensor_parallelism=$ICI_TP dcn_fsdp_parallelism=$DCN_FSDP dcn_pipeline_parallelism=$DCN_PP per_device_batch_size=$PER_DEVICE_BATCH_SIZE num_layers_per_pipeline_stage=$NUM_LAYERS_PER_PP_STAGE weight_dtype=bfloat16 remat_policy=$REMAT_POLICY profiler=xplane skip_first_n_steps_for_profiler=5 quantization=$QUANT";
+    COMMAND="python3 MaxText/train.py MaxText/configs/models/gpu/$CONFIG_NAME.yml hardware=gpu run_name=$RUN_NAME steps=10 max_target_length=4096 model_name=$MODEL_NAME enable_checkpointing=false attention=$ATTENTION dataset_type=synthetic async_checkpointing=false base_output_directory=$OUTPUT_BUCKET logits_dot_in_fp32=false use_iota_embed=true scan_layers=true ici_tensor_parallelism=$ICI_TP dcn_fsdp_parallelism=$DCN_FSDP dcn_pipeline_parallelism=$DCN_PP per_device_batch_size=$PER_DEVICE_BATCH_SIZE num_layers_per_pipeline_stage=$NUM_LAYERS_PER_PP_STAGE weight_dtype=bfloat16 remat_policy=$REMAT_POLICY profiler=xplane skip_first_n_steps_for_profiler=5 num_pipeline_microbatches=$PP_MBS";
     # ici_tensor_sequence_parallelism=$T_SEQ
     # quantization=fp8
-
+    COMMAND='export LD_LIBRARY_PATH=/usr/local/cuda-12.6/compat:$LD_LIBRARY_PATH;'"${COMMAND}";
     COMMAND="${COMMAND};""gsutil -m cp -r /var/log/yy $OUTPUT_BUCKET";
 
     echo 'COMMAND is:' ${COMMAND}
@@ -206,8 +221,24 @@ call_config() {
 # submit 126
 
 # Test run with 2 nodes, cuda kernel fails
-export NODES=64
-call_config --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 1 --ICI_TP 8 --DCN_FSDP $NODES --REMAT_POLICY save_qkv_proj --ATTENTION cudnn_flash_te --QUANT fp8
+
+# 7B
+# export NODES=16
+# call_config --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 1 --ICI_TP 1 --DCN_FSDP $NODES --REMAT_POLICY save_qkv_proj --ATTENTION dot_product
+
+# 405B FSDP
+# export NODES=64
+# call_config --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 0.125 --ICI_TP 8 --DCN_FSDP $NODES --REMAT_POLICY save_qkv_proj --ATTENTION cudnn_flash_te
+
+# 405B PP1
+export NODES=112
+call_config --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 1 --ICI_TP 1 --DCN_FSDP 16 --DCN_PP 7 --NUM_LAYERS_PER_PP_STAGE 6 --REMAT_POLICY full --ATTENTION cudnn_flash_te
+
+# 405B PP2
+# export NODES=96
+# call_config --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 1 --ICI_TP 1 --DCN_FSDP 32 --DCN_PP 3 --NUM_LAYERS_PER_PP_STAGE 6 --REMAT_POLICY full --ATTENTION cudnn_flash_te
+
+
 # call_config --NUM_LAYERS 126  --NUM_NODES $NODES --PER_DEVICE_BATCH_SIZE 1 --T_SEQ 1 --ICI_TP 1 --ICI_FSDP 8 --DCN_FSDP $NODES --DCN_PP 1 --REMAT_POLICY save_qkv_proj --ATTENTION cudnn_flash_te --NUM_LAYERS_PER_PP_STAGE 0
 
 
