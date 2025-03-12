fe952e23b4b4e8c3f75cdd0944741ababce83308
Sujeeth Jinesh <sujinesh@google.com>
2025-03-12 17:57:15 +0000

    Add Disruption Manager & Recipes

--- benchmarks/recipes/pw_mcjax_benchmark_recipe.py
+++ benchmarks/recipes/pw_mcjax_benchmark_recipe.py
@@ -25,6 +25,8 @@ sys.path.append(parent_dir)
 
 import maxtext_trillium_model_configs as model_configs
 import maxtext_xpk_runner as mxr
+from xpk_configs import XpkClusterConfig
+
 
 PROXY_IMAGE = "us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/unsanitized_proxy_server:latest"
 SERVER_IMAGE = "us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/unsanitized_server:latest"
@@ -50,7 +52,7 @@ BENCHMARK_STEPS = 20
 
 def main() -> int:
   # V6e cluster config
-  cluster_config = mxr.XpkClusterConfig(
+  cluster_config = XpkClusterConfig(
       cluster_name=CLUSTER,
       project=PROJECT,
       zone=ZONE,
