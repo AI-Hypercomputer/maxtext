80abfa8814b050d3b0779f86d55e6b7bba790255
Sujeeth Jinesh <sujinesh@google.com>
2025-03-16 17:37:12 +0000

    Add Step Monitor to Disruption Manager & Suspend/Resume Recipe (SIGTERM Path)

--- benchmarks/disruption_management/monitor.py
+++ benchmarks/disruption_management/monitor.py
@@ -13,8 +13,12 @@
 
 import abc
 import os
+import re
+import subprocess
 import sys
 import time
+
+from disruption_management.disruption_utils import wait_for_pod_to_start
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
@@ -22,7 +26,7 @@ sys.path.append(parent_dir)
 from disruption_management.disruption_handler import DisruptionConfig
 from disruption_management.disruption_handler import TriggerType
 
-POLLING_INTERVAL_SECONDS = 10
+STANDARD_STEP_LOG_REGEX = r".*completed step: (\d+), .*"
 
 
 class Monitor(abc.ABC):
@@ -54,18 +58,80 @@ class StepMonitor(Monitor):
       self,
       workload_name: str,
       disruption_config: DisruptionConfig,
-      target_pod_regex: str,
+      step_pod_regex: str,
   ):
     """Initializes StepMonitor."""
     super().__init__(workload_name, disruption_config)
-    self.target_pod_regex = target_pod_regex
-    if not target_pod_regex:
-      raise ValueError("Target pod regex is required for monitoring.")
+    self.step_pod_regex = step_pod_regex
+    if not step_pod_regex:
+      raise ValueError("Step pod regex is required for monitoring.")
 
-  # TODO(sujinesh): Implement step monitor.
   def monitor_and_detect_trigger(self):
-    """Monitors logs and detects step trigger."""
-    raise NotImplementedError("Step monitor not implemented yet.")
+    """Waits for target pod to start and returns when it detects the step trigger."""
+    pod_name = wait_for_pod_to_start(self.workload_name, self.step_pod_regex)
+    if not pod_name:
+      return False
+
+    kubectl_logs_command = [
+        "kubectl",
+        "logs",
+        "-f",  # Follow the logs for real-time updates
+        pod_name,
+    ]
+    kubectl_logs_command_str = " ".join(kubectl_logs_command)
+
+    process = None
+    try:
+      print(
+          f"Workload '{self.workload_name}', Pod '{pod_name}': Tailing logs for"
+          f" real-time step detection (reading in chunks)..."
+      )
+      process = subprocess.Popen(
+          kubectl_logs_command_str,
+          shell=True,
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE,
+          text=True,
+      )
+
+      last_step = -1
+      with process.stdout as pipe:
+        for line in iter(pipe.readline, ""):
+          print(f"Workload '{self.workload_name}', Pod '{pod_name}': {line}")
+          line = line.strip()
+          print(
+              f"Workload '{self.workload_name}', Pod '{pod_name}': {line}"
+          )
+          match = re.search(STANDARD_STEP_LOG_REGEX, line)
+          if match:
+            step_number = int(match.group(1))
+            if step_number > last_step:
+              last_step = step_number  # Update last seen step
+            if step_number >= self.disruption_config.trigger_value:
+              print(
+                  f"Workload '{self.workload_name}', Pod '{pod_name}': STEP"
+                  f" trigger reached! Detected step: {step_number}, Trigger"
+                  f" Value: {self.disruption_config.trigger_value}."
+              )
+              return True
+
+    except subprocess.CalledProcessError as e:
+      print(
+          f"Error getting logs for pod '{pod_name}' of workload"
+          f" '{self.workload_name}': {e.stderr}"
+      )
+      return False
+    except Exception as e:
+      print(f"An error occurred during log tailing: {e}")
+    finally:
+      if process:
+        process.kill()
+
+    print(
+        f"Workload '{self.workload_name}', Pod '{pod_name}': No step trigger"
+        " detected."
+    )
+    return False
 
 
 class TimeMonitor(Monitor):
@@ -85,11 +151,11 @@ class TimeMonitor(Monitor):
     return True
 
 
-def create_monitor(workload_name, disruption_config, target_pod_regex):
+def create_monitor(workload_name, disruption_config, step_pod_regex):
   """Factory function to create the appropriate monitor."""
   if disruption_config.trigger_type == TriggerType.STEP:
     return StepMonitor(
-        workload_name, disruption_config, target_pod_regex=target_pod_regex
+        workload_name, disruption_config, step_pod_regex=step_pod_regex
     )
   elif disruption_config.trigger_type == TriggerType.TIME_SECONDS:
     return TimeMonitor(workload_name, disruption_config)
