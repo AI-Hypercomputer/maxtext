408ed980e81f8eb7f5a3c0c1955940aecdf523b7
gobbleturk <mattdavidow@google.com>
2025-03-20 23:48:12 +0000

    Add option to set mu_dtype differently than weight_dtype

--- MaxText/tests/state_dtypes_test.py
+++ MaxText/tests/state_dtypes_test.py
@@ -31,11 +31,11 @@ import jax.numpy as jnp
 Transformer = models.Transformer
 
 
-class WeightDtypes(unittest.TestCase):
-  """Test that all weights are expected dtype (default float32)"""
+class StateDtypes(unittest.TestCase):
+  """Tests that state has expected dtypes, e.g. weights default to float32"""
 
-  def get_weights(self, argv):
-    """Gets model weights"""
+  def get_state(self, argv):
+    """Gets model state including weights and optimizer state"""
 
     # Setup necessary inputs to build a model state
     config = pyconfig.initialize(argv)
@@ -48,17 +48,33 @@ class WeightDtypes(unittest.TestCase):
     _, example_rng = jax.random.split(jax.random.PRNGKey(0), 2)
 
     abstract_state, _, _ = max_utils.get_abstract_state(model, tx, config, example_rng, mesh)
-    return abstract_state.params
+    return abstract_state
 
-  def assert_weights_are_dtype(self, weights, expected_dtype):
+  def get_weights(self, argv):
+    return self.get_state(argv).params
+
+  def get_mu(self, argv):
+    return self.get_state(argv).opt_state[0].mu
+
+  def assert_pytree_is_dtype(self, weights, expected_dtype):
     jax.tree_util.tree_map_with_path(lambda x, y: self.assertEqual(y.dtype, expected_dtype), weights)
 
   def test_default_float32(self):
     argv = [None, "configs/base.yml", "enable_checkpointing=False"]
     weights = self.get_weights(argv)
-    self.assert_weights_are_dtype(weights, jnp.float32)
+    self.assert_pytree_is_dtype(weights, jnp.float32)
 
   def test_set_bf16(self):
     argv = [None, "configs/base.yml", "enable_checkpointing=False", "weight_dtype=bfloat16"]
     weights = self.get_weights(argv)
-    self.assert_weights_are_dtype(weights, jnp.bfloat16)
+    self.assert_pytree_is_dtype(weights, jnp.bfloat16)
+
+  def test_default_mu_float32(self):
+    argv = [None, "configs/base.yml", "enable_checkpointing=False"]
+    mu = self.get_mu(argv)
+    self.assert_pytree_is_dtype(mu, jnp.float32)
+
+  def test_set_mu_bf16(self):
+    argv = [None, "configs/base.yml", "enable_checkpointing=False", "mu_dtype=bfloat16"]
+    mu = self.get_mu(argv)
+    self.assert_pytree_is_dtype(mu, jnp.bfloat16)
