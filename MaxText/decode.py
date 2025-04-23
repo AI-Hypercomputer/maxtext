ba4451b9f28201c0626197d826e3491557dd6bcc
Pate Motter <patemotter@google.com>
2025-04-23 21:54:05 +0000

    Adding Wangyuan's debug changes

--- MaxText/decode.py
+++ MaxText/decode.py
@@ -29,6 +29,10 @@ from MaxText import multimodal_utils
 
 # Number of text sequences to process in a single batch.
 _NUM_STREAMS = 1
+# Number of steps
+_NUM_STEPS = 256
+# Number of iterations to run
+_NUM_ITERS = 8
 
 
 def _batch_first_result_token(first_tokens: list[engine_api.ResultTokens], batch_size: int):
@@ -115,42 +119,48 @@ def main(argv: Sequence[str]) -> None:
   batch_size = int(config.per_device_batch_size * jax.device_count())
   assert 0 < _NUM_STREAMS <= batch_size, f"The number of streams {_NUM_STREAMS} must be > 0 and <= batch size {batch_size}"
 
-  prefill_result_list = []
-  first_token_list = []
-  sampled_tokens_list = []
+  for iter in range(_NUM_ITERS):
+    prefill_result_list = []
+    first_token_list = []
+    sampled_tokens_list = []
+    slot_id_base = (iter * _NUM_STREAMS) % batch_size
+    # Prefill
+    rng, rng_prefill = jax.random.split(rng)  # Split RNG before calling prefill
+    for i in range(_NUM_STREAMS):
+      prefill_result, first_token = engine.prefill(
+          params=params, padded_tokens=tokens, true_length=true_length, rng=rng_prefill, slot=slot_id_base+i
+      )
+      prefill_result_list.append(prefill_result)
+      first_token_list.append(first_token)
 
-  # Prefill
-  rng, rng_prefill = jax.random.split(rng)  # Split RNG before calling prefill
-  for i in range(_NUM_STREAMS):
-    prefill_result, first_token = engine.prefill(
-        params=params, padded_tokens=tokens, images=images, true_length=true_length, rng=rng_prefill, slot=i
-    )
-    prefill_result_list.append(prefill_result)
-    first_token_list.append(first_token)
+    # Insert
+    if iter == 0:
+      rng, rng_init_decode = jax.random.split(rng)
+      decode_state = engine.init_decode_state(rng_init_decode)
+    for i in range(_NUM_STREAMS):
+      decode_state = engine.insert(prefill_result_list[i], decode_state, slot=slot_id_base+i)
 
-  # Insert
-  rng, rng_init_decode = jax.random.split(rng)
-  decode_state = engine.init_decode_state(rng_init_decode)
-  for i in range(_NUM_STREAMS):
-    decode_state = engine.insert(prefill_result_list[i], decode_state, slot=i)
+    # Generate
+    # steps = range(config.max_prefill_predict_length, config.max_target_length)
+    steps = _NUM_STEPS
+    sampled_tokens_list.append(_batch_first_result_token(first_token_list, batch_size))
+    for _ in range(steps):
+      rng, rng_generate = jax.random.split(rng)
+      # print(f'wyzhangd: before generate decode state {decode_state}')
+      decode_state, sampled_tokens = engine.generate(params, decode_state, rng=rng_generate)
+      # print(f'wyzhangd: after generate decode state {decode_state}')
+      # print(f'wyzhangd: generated sample tokens {sampled_tokens}')
+      sampled_tokens_list.append(sampled_tokens)
 
-  # Generate
-  steps = range(config.max_prefill_predict_length, config.max_target_length)
-  sampled_tokens_list.append(_batch_first_result_token(first_token_list, batch_size))
-  for _ in steps:
-    rng, rng_generate = jax.random.split(rng)
-    decode_state, sampled_tokens = engine.generate(params, decode_state, rng=rng_generate)
-    sampled_tokens_list.append(sampled_tokens)
+    # Get results
+    for i in range(_NUM_STREAMS):
+      results = [t.get_result_at_slot(slot_id_base+i).tokens.item() for t in sampled_tokens_list]
+      output = tokenizer_model.decode(results)
+      print(f"Input `{text}` -> `{output}`")
 
-  # Get results
-  for i in range(_NUM_STREAMS):
-    results = [t.get_result_at_slot(i).tokens.item() for t in sampled_tokens_list]
-    output = tokenizer_model.decode(results)
-    print(f"Input `{text}` -> `{output}`")
-
-  assert output.startswith(
-      config.autoregressive_decode_assert
-  ), f"generated text mismatch {output=}, {config.autoregressive_decode_assert=}"
+    # assert output.startswith(
+    #     config.autoregressive_decode_assert
+    # ), f"generated text mismatch {output=}, {config.autoregressive_decode_assert=}"
 
 
 def _validate_config(config):
