25161cf53e6ce0551149c1d654f9d690db2575b1
Rissy Ran <ran.rissy@gmail.com>
2025-05-07 18:53:23 +0000

    local changes

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -287,6 +287,7 @@ def validate_model_name(s: str) -> bool:
       "mistral-7b",
       "mixtral-8x7b",
       "mixtral-8x22b",
+      "custom-moe",
       "deepseek2-16b",
       "deepseek2-236b",
       "deepseek3-671b",
