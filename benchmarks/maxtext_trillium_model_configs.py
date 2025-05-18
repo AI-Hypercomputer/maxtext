b7c49066b2a308a74b4b2bdfaee88309a6b0054f
gobbleturk <mattdavidow@google.com>
2025-05-18 18:38:51 +0000

    reasonable perf on custom model lets go

--- benchmarks/maxtext_trillium_model_configs.py
+++ benchmarks/maxtext_trillium_model_configs.py
@@ -1755,4 +1755,56 @@ llama3_1_70b_131072 = _add_to_model_dictionary(
   )
 )
 
+matt_dream_v1 = _add_to_model_dictionary(
+  trillium_model_dict,
+  MaxTextModel(
+    model_name="matt_dream_v1",
+    model_type="default",
+    tuning_params={
+        "steps": 20,
+        "per_device_batch_size": 1,
+        "remat_policy": "full",
+        "max_target_length": 4096,
+        "enable_checkpointing": False,
+        "dataset_type": "synthetic",
+        "base_output_directory": "gs://maxtext-experiments-multipod",
+        "decoder_block": "mistral",
+        "ici_expert_parallelism": 64,
+        "ici_tensor_parallelism": 4,
+        "allow_split_physical_axes": True,
+        "custom_mesh": "hybrid_ring_64x4",
+        "num_experts": 64, # 256
+        "num_experts_per_tok": 2,
+        "base_emb_dim": 10240, #7168
+        "base_mlp_dim": 40960,
+        "base_num_query_heads": 64,
+        "base_num_kv_heads": 16,
+        "head_dim": 256,
+        "skip_first_n_steps_for_profiler": 12,
+        "allow_split_physical_axes": False,
+        "sparse_matmul": False, # False
+        "megablox": False, # True
+        "capacity_factor": 1,
+        "profiler": "xplane",
+        "opt_type": "sgd",
+        #"weight_dtype": "bfloat16",
+        
+        # PP
+        "base_num_decoder_layers": 8, # PP * 8
+        # "dcn_pipeline_parallelism": 2,
+        # "num_pipeline_microbatches": 2, # PP * 2 or since we are sad PP * 1
+        # "num_layers_per_pipeline_stage": 2,
+        # "scan_layers": False,
+    },
+    xla_flags=(
+        xla_flags_library.CUSTOM_VMEM_LIMIT_FLAG(81920)
+        + xla_flags_library.REDUCE_SCATTER_FUSION
+        + xla_flags_library.CF_FOR_ALL_GATHER
+        + xla_flags_library.LAYOUT_FOR_ALL_REDUCE_SCATTER
+        + xla_flags_library.PIPELINING_FLAGS
+        + xla_flags_library.PP_MORE_FLAGS
+    ),
+  )
+)
+
 
