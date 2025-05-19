20fba63a73271b6cfd07a0137e6ef534af0871a0
Abheesht Sharma <abheesht@google.com>
2025-05-19 12:02:22 -0700

    Rename gemma2 to gemma
    PiperOrigin-RevId: 760717432

--- tunix/models/gemma/gemma.py
+++ tunix/models/gemma/gemma.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Gemma transformer."""
 
 import dataclasses
@@ -9,7 +23,7 @@ from jax import numpy as jnp
 from jax.interpreters import pxla
 import jax.sharding as shd
 import jaxtyping
-from tunix.models.gemma2 import params as params_lib
+from tunix.models.gemma import params as params_lib
 
 
 LayerCache = dict[str, jaxtyping.Array]
