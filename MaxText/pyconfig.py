dc34a198caaae2b846aaf70c416896e48431fe69
Qinwen Xu <qinwen@google.com>
2025-05-20 00:54:31 +0000

    add example model

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -272,6 +272,7 @@ def validate_model_name(s: str) -> bool:
   # currently supported models
   valid_model_names = (
       "default",
+      "llama2-3.02b",
       "llama2-7b",
       "llama2-13b",
       "llama2-70b",
