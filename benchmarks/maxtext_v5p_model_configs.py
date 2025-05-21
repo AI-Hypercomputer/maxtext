676885e73f4815b079d19fede230ad0d9f07069b
Rishabh Baghel <rishabhbaghel@google.com>
2025-05-21 18:08:19 +0000

    fix tensor parallelism

--- benchmarks/maxtext_v5p_model_configs.py
+++ benchmarks/maxtext_v5p_model_configs.py
@@ -162,7 +162,7 @@ gpt_3_175b_v5p_8 = _add_to_model_dictionary(
     model_type="gpt3-175b",
     tuning_params={
         "ici_fsdp_parallelism": -1,
-        "ici_tensor_parallelism": 16,
+        "ici_tensor_parallelism": 2,
         "per_device_batch_size": 0.5,
         "remat_policy": "full",
         "max_target_length": 2048,
