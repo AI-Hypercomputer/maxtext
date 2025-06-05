42157212447e3ea27f15cd001c7b9d89f6e4cb7b
gobbleturk <mattdavidow@google.com>
2025-06-05 17:14:19 +0000

    gogogo

--- MaxText/pipeline_train.py
+++ MaxText/pipeline_train.py
@@ -10,9 +10,10 @@ from jax.sharding import Mesh
 import jax.numpy as jnp
 
 from flax.core import meta
-from flax import linen as nn
+from flax import linen as nnx
 
 from MaxText import maxtext_utils
+from MaxText import max_utils
 from MaxText import pyconfig
 from MaxText.common_types import MODEL_MODE_TRAIN
 from MaxText.globals import PKG_DIR
@@ -34,8 +35,45 @@ from MaxText.train import (
     setup_mesh_and_model,
     train_step,
     validate_train_config,
+    loss_fn
 )
 from flax.linen import partitioning as nn_partitioning
+
+
+def train_step_grads(model, config, state_mesh_shardings, state, data, dropout_rng):
+  """
+  Args:
+    model: A nn.Module
+    state: A pytree of the current state of the model
+    data: Batch of data to apply to the model
+    dropout_rng: A key to use to generate rng for dropout
+
+  Returns:
+    grads: Same format as state.
+    metrics: Dictionary of model metrics such as loss, training rate, etc.
+  """
+  reference_params, reference_params_sharding, extra_dpo_args, _loss_fn = [], [], [], loss_fn
+  
+  grad_func = jax.value_and_grad(_loss_fn, argnums=4, has_aux=True)
+  (loss, aux), raw_grads = grad_func(model, config, data, dropout_rng, state.params, *extra_dpo_args, is_train=True)
+
+  if config.gradient_clipping_threshold > 0:
+    grads = maxtext_utils.apply_gradient_clipping(raw_grads, state, config.gradient_clipping_threshold)
+  else:
+    grads = raw_grads
+
+  scalar_metrics = {
+      "learning/loss": loss,
+      "learning/moe_lb_loss": aux["moe_lb_loss"],
+      "learning/total_weights": aux["total_weights"],
+      "learning/grad_norm": max_utils.l2norm_pytree(grads)
+  }
+  metrics = {
+      "scalar": scalar_metrics,
+      "scalars": {},
+  }
+  return raw_grads, metrics
+
 
 config = pyconfig.initialize(
     [sys.argv[0], os.path.join(PKG_DIR, "configs", "base.yml")],
@@ -49,16 +87,18 @@ config = pyconfig.initialize(
     num_pipeline_microbatches=8,
     per_device_batch_size=4,
     pipeline_fsdp_ag_once=True,
+    decoder_block="simple",
     dataset_type="synthetic",
 )
 
+def get_state_and_grads(config)
 init_rng, writer, checkpoint_manager, mesh, model, learning_rate_schedule, tx = setup_mesh_and_model(config)
 data_iterator, eval_data_iterator = create_data_iterator(config, mesh)
 state, _, state_mesh_shardings, data_iterator = maxtext_utils.setup_training_state(
     model, data_iterator, tx, config, init_rng, mesh, checkpoint_manager
 )
 
-  # pylint: disable=line-too-long
+
 (
     functional_train,
     in_shard_train,
@@ -67,18 +107,27 @@ state, _, state_mesh_shardings, data_iterator = maxtext_utils.setup_training_sta
     donate_argnums_train,
 ) = maxtext_utils.get_functional_train_with_signature(train_step, mesh, state_mesh_shardings, model, config)
 
+#functional_train=functools.partial(train_step, model, config, state_mesh_shardings)
+functional_train=functools.partial(train_step_grads, model, config, state_mesh_shardings)
+
 p_train_step = jax.jit(
     functional_train,
     in_shardings=in_shard_train,
-    out_shardings=out_shard_train,
+    #out_shardings=out_shard_train,
     static_argnums=static_argnums_train,
     donate_argnums=donate_argnums_train,
 )
 
-example_batch = None
-example_batch = load_next_batch(data_iterator, example_batch, config)
+example_batch = load_next_batch(data_iterator, None, config) # None=example_batch
 with mesh, nn_partitioning.axis_rules(config.logical_axis_rules):
-    state, metrics = p_train_step(state, example_batch, init_rng) #using same rng
+    grads, metrics = p_train_step(state, example_batch, init_rng) #using same rng
 
 loss = metrics['scalar']['learning/loss']
 print(f"{loss=}", flush=True)
+
+
+def convert_pp_params_to_non_pp():
+    return False
+
+
+
