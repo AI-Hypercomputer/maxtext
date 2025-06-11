c87bc0d71a2f2b3bd8555188b5a4b0d5e58bbcda
gobbleturk <mattdavidow@google.com>
2025-06-11 01:12:43 +0000

    go vmap ra2a go

---
+++ MaxText/rv_minimal.py
@@ -0,0 +1,100 @@
+import jax
+import jax.numpy as jnp
+from jax import lax, jit
+from jax.experimental import shard_map
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
+from jax.experimental import mesh_utils
+import functools
+from enum import Enum, auto
+
+
+# Define inputs
+num_devices = len(jax.devices())
+batch = 8
+model = 5
+expert_parallelism = 2
+pipeline_parallelism = num_devices // expert_parallelism
+axis_name = "expert"
+
+ # Define a 1D mesh only EP
+device_mesh_array = mesh_utils.create_device_mesh((expert_parallelism, pipeline_parallelism))
+mesh = Mesh(device_mesh_array, ("expert", "pipeline"))
+
+# create an array x which is [batch, model] and has elements like
+# [[0,0,0],
+#  [1,1,1],
+#  ...
+x = jnp.arange(0.0, batch)
+x = jnp.expand_dims(x, axis=1)
+x = jnp.tile(x, (1, model))
+
+x_partition_spec = jax.sharding.PartitionSpec("expert", None)
+x_sharding = NamedSharding(mesh, x_partition_spec)
+x = jax.device_put(x, x_sharding)
+
+out_partition_spec = x_partition_spec
+output_sharding = x_sharding
+
+@functools.partial(
+    shard_map.shard_map,
+    mesh=mesh,
+    in_specs=(x_partition_spec),
+    out_specs=(out_partition_spec),
+    check_rep=False,
+)
+def wrapper(x):
+    # x is [batch, embed] (we can imagine batch=pdb * seq * exp_per_tok)
+    batch_shard, _ = x.shape
+    batch_per_ep_shard = batch_shard / expert_parallelism
+
+    # output_shape is worst case [batch * EP, embed]
+    output_shape = jnp.tile(x, (expert_parallelism, 1))
+    output_shape = x # This is best case, which our test case achieves
+
+    # input_offsets,
+    input_offsets = jnp.array([i * batch_per_ep_shard for i in range(expert_parallelism)], dtype=jnp.int32)
+
+    # send_sizes is [EP]
+    send_sizes =jnp.array([batch_per_ep_shard for _ in range(expert_parallelism)], dtype=jnp.int32)
+
+    # output_offsets is [EP]
+    #output_offsets = jnp.array([i * batch_per_ep_shard for i in range(expert_parallelism)], dtype=jnp.int32)
+    local_id = jax.lax.axis_index("expert")
+    output_offsets = local_id * batch_per_ep_shard * jnp.ones((expert_parallelism,), dtype=jnp.int32)
+    output_offsets = output_offsets.astype(jnp.int32)
+
+    # recv_sizes is [EP]
+    recv_sizes=jnp.array([batch_per_ep_shard for _ in range(expert_parallelism)], dtype=jnp.int32)
+    
+    output = jax.lax.ragged_all_to_all(
+        x,
+        output_shape,
+        input_offsets,
+        send_sizes,
+        output_offsets,
+        recv_sizes,
+        axis_name=axis_name,
+    )
+    print(f"{output.shape=}\n")
+    return output
+
+
+
+vmap_func = jax.vmap(
+    wrapper,
+    spmd_axis_name="pipeline",
+)
+jit_vmap_func = jax.jit(vmap_func)
+x_vmap = jnp.expand_dims(x, axis=0)
+x_vmap = jnp.tile(x_vmap, (pipeline_parallelism, 1, 1))
+x_vmap = jax.device_put(x_vmap, NamedSharding(mesh, jax.sharding.PartitionSpec("pipeline", "expert", None)))
+x_vmap_a2a = jit_vmap_func(x_vmap)
+print(x_vmap_a2a.shape)
+print("Successfully ran vmapped wrapper!", flush=True)
+breakpoint()
+
+# jit_wrapper = jax.jit(wrapper)
+# print(f"{x.shape=}", flush=True)
+# x_a2a = jit_wrapper(x)
+# print("Successfully ran wrapper (non - vmap)")
+# print(x_a2a)
