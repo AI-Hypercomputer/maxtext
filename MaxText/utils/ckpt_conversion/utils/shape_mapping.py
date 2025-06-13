db07d6b6085c10361f8c02a0676fbf48104b5b9d
Yixuan Wang <yixuannwang@google.com>
2025-06-13 23:17:06 +0000

    enable gemma3 conversion from maxtext to huggingface

--- MaxText/utils/ckpt_conversion/utils/shape_mapping.py
+++ MaxText/utils/ckpt_conversion/utils/shape_mapping.py
@@ -70,8 +70,60 @@ def GEMMA2_HF_WEIGHTS_TO_SHAPE_MAPPING(config):
   return mapping
 
 
+def GEMMA3TEXT_HF_WEIGHTS_TO_SHAPE_MAPPING(config):
+  """Returns mapping between HuggingFace Gemma3Text weights path and weights shape.
+
+  Args:
+      config (dict): Model configuration dictionary (from HF Gemma3TextConfig.to_dict())
+
+  Returns:
+      dict: A mapping where:
+          - Keys are HuggingFace model parameter paths
+          - Values are parameter shape as a List
+  """
+  head_dim = config["head_dim"]
+
+  mapping = {
+      "model.embed_tokens.weight": [config["vocab_size"], config["hidden_size"]],
+      "model.norm.weight": [config["hidden_size"]],
+  }
+  for layer_idx in range(config["num_hidden_layers"]):
+    layer_mapping = {
+        f"model.layers.{layer_idx}.input_layernorm.weight": [config["hidden_size"]],
+        f"model.layers.{layer_idx}.self_attn.q_proj.weight": [
+            config["num_attention_heads"] * head_dim,
+            config["hidden_size"],
+        ],
+        f"model.layers.{layer_idx}.self_attn.k_proj.weight": [
+            config["num_key_value_heads"] * head_dim,
+            config["hidden_size"],
+        ],
+        f"model.layers.{layer_idx}.self_attn.v_proj.weight": [
+            config["num_key_value_heads"] * head_dim,
+            config["hidden_size"],
+        ],
+        f"model.layers.{layer_idx}.self_attn.o_proj.weight": [
+            config["hidden_size"],
+            config["num_attention_heads"] * head_dim,
+        ],
+        f"model.layers.{layer_idx}.self_attn.q_norm.weight": [head_dim],
+        f"model.layers.{layer_idx}.self_attn.k_norm.weight": [head_dim],
+        f"model.layers.{layer_idx}.post_attention_layernorm.weight": [config["hidden_size"]],
+        f"model.layers.{layer_idx}.pre_feedforward_layernorm.weight": [config["hidden_size"]],
+        f"model.layers.{layer_idx}.mlp.gate_proj.weight": [config["intermediate_size"], config["hidden_size"]],
+        f"model.layers.{layer_idx}.mlp.up_proj.weight": [config["intermediate_size"], config["hidden_size"]],
+        f"model.layers.{layer_idx}.mlp.down_proj.weight": [config["hidden_size"], config["intermediate_size"]],
+        f"model.layers.{layer_idx}.post_feedforward_layernorm.weight": [config["hidden_size"]],
+    }
+    mapping.update(layer_mapping)
+  return mapping
+
+
 SHAPE_MAPPING = {
     "gemma2-2b": GEMMA2_HF_WEIGHTS_TO_SHAPE_MAPPING,
     "gemma2-9b": GEMMA2_HF_WEIGHTS_TO_SHAPE_MAPPING,
     "gemma2-27b": GEMMA2_HF_WEIGHTS_TO_SHAPE_MAPPING,
+    "gemma3-4b": GEMMA3TEXT_HF_WEIGHTS_TO_SHAPE_MAPPING,
+    "gemma3-12b": GEMMA3TEXT_HF_WEIGHTS_TO_SHAPE_MAPPING,
+    "gemma3-27b": GEMMA3TEXT_HF_WEIGHTS_TO_SHAPE_MAPPING,
 }
