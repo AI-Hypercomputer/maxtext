ffb4783503f4c5ac2a2f38af6814e8d0c7fa8fb5
wenxindongwork <wenxindong@google.com>
2025-06-13 04:25:52 +0000

    fix

--- MaxText/layers/deepseek.py
+++ MaxText/layers/deepseek.py
@@ -225,7 +225,7 @@ class DeepSeekMoELayer(nn.Module):
       slot: Optional[int] = None,
   ):
     cfg = self.config
-    if model_mode == models.ModelMode.PREFILL:
+    if model_mode == MODEL_MODE_PREFILL:
       axis_names = ("activation_batch", "prefill_activation_norm_length", "activation_embed")
     else:
       axis_names = ("activation_batch", "activation_norm_length", "activation_embed")
