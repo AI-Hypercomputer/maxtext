68a44da61d0ba549861a11ef41361a1ef9e6442b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-06-19 10:39:29 -0600

    [requirements_with_jax_ai_image.txt,MaxText/__init__.py] Correct versions

--- MaxText/__init__.py
+++ MaxText/__init__.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 __author__ = "Google LLC"
-__version__ = "2025.06.17"
+__version__ = "2025.06.19"
 __description__ = (
     "MaxText is a high performance, highly scalable, open-source LLM written in pure Python/Jax and "
     "targeting Google Cloud TPUs and GPUs for training and **inference."
