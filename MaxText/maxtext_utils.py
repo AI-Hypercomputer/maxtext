52382cbfb474e6e4a087a0827a78d564bf6bea93
Zach Meyers <zachmeyers@google.com>
2025-07-22 12:49:04 -0700

    Update `checkpointing.py` with support for other checkpointing formats, specifically MaxText.
    PiperOrigin-RevId: 785961175

--- MaxText/maxtext_utils.py
+++ MaxText/maxtext_utils.py
@@ -795,6 +795,9 @@ def setup_initial_state(
         config.dataset_type,
         use_ocdbt=config.checkpoint_storage_use_ocdbt,
         use_zarr3=config.checkpoint_storage_use_zarr3,
+        enable_orbax_v1=config.enable_orbax_v1,
+        checkpoint_conversion_fn=config.checkpoint_conversion_fn,
+        checkpoint_context=config.checkpoint_context,
     )
 
     if restored:
