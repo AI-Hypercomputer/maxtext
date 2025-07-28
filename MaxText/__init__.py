e316da1cf75fc01fd59505df922f7ae7273a138c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-07-27 22:49:23 -0500

    [mkdocs.yml] Add documentation generation ; [README.md] Add license details ; [pyproject.toml] Fix setuptools find ; [docs,scripts] Add necessary files for mkdocs ; [__init__.py] Add missing files for module hierarchy ; [requirements-docs.txt] Add doc-specific dependencies (could be placed in pyproject.toml)

--- MaxText/__init__.py
+++ MaxText/__init__.py
@@ -14,6 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+"""
+MaxText is a high performance, highly scalable, open-source LLM written in pure Python/Jax and
+targeting Google Cloud TPUs and GPUs for training and **inference.
+"""
+
 __author__ = "Google LLC"
 __version__ = "2025.04.25"
 __description__ = (
