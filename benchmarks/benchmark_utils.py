0bf7c4c1a2375c2b566a8077c28ac3a1eda155ef
Ranuka Manamendra <ranuka@google.com>
2025-07-28 19:10:08 +0000

    Added bash script/documentation to run maxtest models not via XPK

--- benchmarks/benchmark_utils.py
+++ benchmarks/benchmark_utils.py
@@ -35,9 +35,6 @@ class MaxTextModel:
 
 
 # Run this for new definitions that should be part of the library.
-def _add_to_model_dictionary(
-    model_dictionary: dict[str, MaxTextModel], maxtext_model: MaxTextModel
-) -> MaxTextModel:
-  print(maxtext_model.model_name.replace("-", "_"))
+def _add_to_model_dictionary(model_dictionary: dict[str, MaxTextModel], maxtext_model: MaxTextModel) -> MaxTextModel:
   model_dictionary[maxtext_model.model_name.replace("-", "_")] = maxtext_model
   return maxtext_model
