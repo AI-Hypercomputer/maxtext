3189b975b3eaee82cff74ca1cce0bca8e285eb16
Melissa Weber Mendonça <melissawm@gmail.com>
2025-07-29 11:18:22 -0300

    Reorganize documentation

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "MaxText"
+copyright = "2025, MaxText developers"
+author = "MaxText developers"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_nb",
+    "sphinx_design",
+]
+
+templates_path = ["_templates"]
+source_suffix = [".rst", ".ipynb", ".md"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = []
+# html_logo = "_static/flax.png"
+
+# -- Options for myst ----------------------------------------------
+myst_heading_anchors = 3  # auto-generate 3 levels of heading anchors
+myst_enable_extensions = [
+    "dollarmath",
+    "linkify",
+    "colon_fence",
+]
+myst_linkify_fuzzy_links = False
---
+++ docs/explanations.md
@@ -0,0 +1,9 @@
+# Explanations
+
+```{toctree}
+:maxdepth: 1
+
+explanations/steps_model.md
+explanations/quantization.md
+explanations/sharding.md
+```
