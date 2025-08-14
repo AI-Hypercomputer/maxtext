70268cfcc1d8a041b6773bb2b14b9fc417609d7a
Param Bole <parambole@google.com>
2025-08-14 22:47:03 +0000

    Enhance Checkpoint Converter for MoE Models

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -239,16 +239,11 @@ def validate_constant_bound(keys):
 
 
 def validate_quantization_methods(keys):
-  """Validate quantization methods
-  """
-  valid_quant_methods = (
-    "", "int8", "fp8", "fp8_full", "fp8_gpu", "fp8_nanoo"
-  )
+  """Validate quantization methods"""
+  valid_quant_methods = ("", "int8", "fp8", "fp8_full", "fp8_gpu", "fp8_nanoo")
   if keys["use_qwix_quantization"]:
     if keys["quantization"] not in valid_quant_methods:
-      raise ValueError(
-          f"Invalid quantization method {keys['quantization']}. Valid options are {valid_quant_methods}"
-      )
+      raise ValueError(f"Invalid quantization method {keys['quantization']}. Valid options are {valid_quant_methods}")
 
 
 def validate_data_input(keys):
@@ -351,6 +346,7 @@ def validate_model_name(s: str) -> bool:
       "qwen3-14b",
       "qwen3-32b",
       "qwen3-235b-a22b",
+      "qwen3-30b-a3b",
       "gpt3-175b",
       "gpt3-22b",
       "gpt3-6b",
