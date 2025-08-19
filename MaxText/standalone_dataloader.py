3b23be523472f75c570a5d829e0cd5f11abb8f55
Vladimir Suvorov <suvorovv@google.com>
2025-08-19 21:12:04 +0400

    Nit fix of import
    Signed-off-by: Vladimir Suvorov <suvorovv@google.com>

--- MaxText/standalone_dataloader.py
+++ MaxText/standalone_dataloader.py
@@ -31,8 +31,7 @@ from MaxText import max_logging
 from MaxText import pyconfig
 from MaxText.data_loader import DataLoader
 from MaxText.train import get_first_step
-from MaxText.train_utils import validate_train_config
-from MaxText.train_utils import setup_train_loop
+from MaxText.train_utils import validate_train_config,setup_train_loop
 
 
 def data_load_loop(config, state=None):
