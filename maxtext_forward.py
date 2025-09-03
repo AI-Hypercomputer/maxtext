f38d9f3800bbb6b5d6de5d299eb151d25bab7c17
Shuning Jin <shuningjin@google.com>
2025-09-03 00:02:14 +0000

    clean

--- maxtext_forward.py
+++ maxtext_forward.py
@@ -1,4 +1,7 @@
 """
+# {"prompt": "I love to sleep", "tokens": [40, 3047, 316, 8746], "logits": …}
+# gcloud storage cp gs://shuningjin-multipod-dev/golden-logits/openai/gpt-oss-20b/golden_bf16_v2_debug.jsonl /tmp/gpt-oss-20b-golden_bf16_v2_debug.jsonl
+
 # tpu
 export LIBTPU_INIT_ARGS='--xla_tpu_scoped_vmem_limit_kib=81920'
 python maxtext_forward.py
@@ -35,40 +38,7 @@ class MockConfigForForwardPass:
   def __init__(self):
     self.global_batch_size_to_train_on = 1
     self.max_target_length = 4
-    self.use_multimodal=False
-
-
-model_args = [
-    "something.py",
-    "MaxText/configs/base.yml",
-    # "hardware=cpu",
-    "base_output_directory=test",
-    "run_name=temp-testing-only",
-    "skip_jax_distributed_system=true",
-    # model specific
-    "model_name=gpt-oss-20b",
-    "scan_layers=false",
-    "attention=dot_product",
-    "load_parameters_path=gs://shuningjin-multipod-dev/gpt-oss-20b/unscan-bf16-v2-2025-09-02-01-16-00/0/items",
-    # high precision flags
-    "weight_dtype=float32",
-    "dtype=float32",
-    "activations_in_float32=true",
-    "matmul_precision=high",
-]
-
-
-# model_args = ['/mnt/disks/jacobplatin/code/maxtext/llama4_maverick_check_weight.py', 'MaxText/configs/base.yml', 'hardware=cpu', 'scan_layers=false', 'base_output_directory=llama4', 'run_name=temp-testing-only', 'model_name=llama4-17b-128e', 'skip_jax_distributed_system=true', 'load_parameters_path=/mnt/disks/jacobplatin/models/llama4/maverick/4-layer-unscanned/0/items/']
-config = pyconfig.initialize(model_args)
-
-init_rng = jax.random.PRNGKey(config.init_weights_seed)
-init_rng, rng1 = jax.random.split(init_rng)
-devices_array = maxtext_utils.create_device_mesh(config)
-mesh = jax.sharding.Mesh(devices_array, config.mesh_axes)
-quant = quantizations.configure_quantization(config)
-# model = models.Transformer(config, mesh=mesh, quant=quant)
-model = models.transformer_as_linen(config, mesh, quant=quant, model_mode=MODEL_MODE_TRAIN)
-state, _ = maxtext_utils.setup_decode_state(model, config, rng1, mesh, None)
+    self.use_multimodal = False
 
 
 def print_nested_keys(data, prefix=""):
@@ -84,40 +54,6 @@ def print_nested_keys(data, prefix=""):
       print_nested_keys(value, current_path)
   else:
     print(f"{prefix} | {data.shape} | {data.mean()}")
-
-
-print_nested_keys(state.params)
-
-
-# init_rng = jax.random.PRNGKey(config.init_weights_seed)
-# init_rng, rng1 = jax.random.split(init_rng)
-# devices_array = maxtext_utils.create_device_mesh(config)
-# mesh = jax.sharding.Mesh(devices_array, config.mesh_axes)
-# quant = quantizations.configure_quantization(config)
-# maxtext_model = models.transformer_as_linen(config, mesh, quant=quant, model_mode=MODEL_MODE_TRAIN)
-# maxtext_state, _ = maxtext_utils.setup_decode_state(maxtext_model, config, rng1, mesh, None)
-
-
-# def get_data(golden_data, golden_data_index, config):
-#   """Get the golden data for the test indexed at golden_data_index"""
-
-#   max_logging.log(f"Comparing forward pass for golden data index = {golden_data_index} ")
-#   max_logging.log(f"config.global_batch_size_to_train_on={config.global_batch_size_to_train_on}")
-#   s = (config.global_batch_size_to_train_on, config.max_target_length)
-#   ids = np.asarray(golden_data[golden_data_index]["tokens"], dtype=np.int32)
-
-#   logits = np.asarray(golden_data[golden_data_index]["logits"], dtype=np.float32)
-#   max_logging.log(f" prompt=\"{golden_data[golden_data_index]['prompt']}\" raw ids={ids}, logits.shape = {logits.shape}")
-
-#   decoder_segment_ids = np.zeros(s) + DECODING_ACTIVE_SEQUENCE_INDICATOR
-#   decoder_positions = np.stack(
-#       [np.arange(config.max_target_length, dtype=np.int32) for _ in range(config.global_batch_size_to_train_on)]
-#   )
-
-#   ids = np.stack([ids for _ in range(config.global_batch_size_to_train_on)])
-#   max_logging.log(f"ids={ids}, decoder_segment_ids = {decoder_segment_ids}, decoder_positions= {decoder_positions}")
-
-#   return ids, decoder_segment_ids, decoder_positions, logits
 
 
 def get_data(golden_data, golden_data_index, config):
@@ -151,7 +87,7 @@ def get_data(golden_data, golden_data_index, config):
       [np.arange(config.max_target_length, dtype=np.int32) for _ in range(config.global_batch_size_to_train_on)]
   )
 
-  return ids, decoder_segment_ids, decoder_positions, logits#, seq_len
+  return ids, decoder_segment_ids, decoder_positions, logits  # , seq_len
 
 
 def setup_golden_data(input_golden_data_path):
@@ -186,16 +122,51 @@ def setup_golden_data(input_golden_data_path):
   return ids, decoder_segment_ids, decoder_positions, golden_logits
 
 
-ids, decoder_segment_ids, decoder_positions, golden_logits = setup_golden_data("/tmp/gpt-oss-20b-golden_bf16_v2_debug.jsonl")
-# ids, decoder_segment_ids, decoder_positions, golden_logits = setup_golden_data("golden_scout_4layer.jsonl")
+if __name__ == "__main__":
 
-full_train_logits = model.apply(
-    state.params,
-    ids,
-    decoder_positions,
-    decoder_segment_ids,
-    enable_dropout=False,
-    rngs={"aqt": init_rng},
-)
+  golden_data_path = "/tmp/gpt-oss-20b-golden_bf16_v2_debug.jsonl"
 
-print(full_train_logits)
+  model_args = [
+      "something.py",
+      "MaxText/configs/base.yml",
+      # "hardware=cpu",
+      "base_output_directory=test",
+      "run_name=temp-testing-only",
+      "skip_jax_distributed_system=true",
+      # model specific
+      "model_name=gpt-oss-20b",
+      "scan_layers=false",
+      "attention=dot_product",
+      "load_parameters_path=gs://shuningjin-multipod-dev/gpt-oss-20b/unscan-bf16-v2-2025-09-02-01-16-00/0/items",
+      # high precision flags
+      "weight_dtype=float32",
+      "dtype=float32",
+      "activations_in_float32=true",
+      "matmul_precision=high",
+  ]
+
+  # model_args = ['/mnt/disks/jacobplatin/code/maxtext/llama4_maverick_check_weight.py', 'MaxText/configs/base.yml', 'hardware=cpu', 'scan_layers=false', 'base_output_directory=llama4', 'run_name=temp-testing-only', 'model_name=llama4-17b-128e', 'skip_jax_distributed_system=true', 'load_parameters_path=/mnt/disks/jacobplatin/models/llama4/maverick/4-layer-unscanned/0/items/']
+  config = pyconfig.initialize(model_args)
+  init_rng = jax.random.PRNGKey(config.init_weights_seed)
+  init_rng, rng1 = jax.random.split(init_rng)
+  devices_array = maxtext_utils.create_device_mesh(config)
+  mesh = jax.sharding.Mesh(devices_array, config.mesh_axes)
+  quant = quantizations.configure_quantization(config)
+  # model = models.Transformer(config, mesh=mesh, quant=quant)
+  model = models.transformer_as_linen(config, mesh, quant=quant, model_mode=MODEL_MODE_TRAIN)
+  state, _ = maxtext_utils.setup_decode_state(model, config, rng1, mesh, None)
+
+  print_nested_keys(state.params)
+
+  ids, decoder_segment_ids, decoder_positions, golden_logits = setup_golden_data(golden_data_path)
+
+  full_train_logits = model.apply(
+      state.params,
+      ids,
+      decoder_positions,
+      decoder_segment_ids,
+      enable_dropout=False,
+      rngs={"aqt": init_rng},
+  )
+
+  print(full_train_logits)
