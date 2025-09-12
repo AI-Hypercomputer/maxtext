47e97742e4d609da7b5ca0747868bc8452e0c0fe
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-09-12 09:13:57 -0500

    [docs/conf.py] Generate API docs for sphinx ; `pyink` ; imports at top ; [docs/reference/api.rst] Placeholder for generated API docs ; [pyproject.toml] Uncomment docs deps ; [docs/guides.md] Remove redundant extensions

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+import os.path
+import sys
+
+REPO_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+sys.path.insert(0, os.path.join(REPO_ROOT, "src"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -31,6 +39,10 @@ author = "MaxText developers"
 extensions = [
     "myst_nb",
     "sphinx_design",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ["_templates"]
@@ -52,9 +64,47 @@ myst_enable_extensions = [
 ]
 myst_linkify_fuzzy_links = False
 
+# -- Options for autodoc ----------------------------------------------------
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
+autosummary_generate = True
+
 # Remove specific documents from ToC
 exclude_patterns = [
-    "guides/run_maxtext_via_multihost_job.md",
-    "guides/run_maxtext_via_multihost_runner.md",
-    "guides/llm_calculator.ipynb",
+    os.path.join("guides", "run_maxtext_via_multihost_job.md"),
+    os.path.join("guides", "run_maxtext_via_multihost_runner.md"),
+    os.path.join("guides", "llm_calculator.ipynb"),
 ]
+
+
+# -- Autogenerate API documentation ------------------------------------------
+# This function automatically runs sphinx-apidoc to generate API documentation
+# from the docstrings in the source code.
+def run_apidoc(_):
+  from sphinx.ext import apidoc
+
+  pkg_path = os.path.join(REPO_ROOT, "src", "MaxText")
+  # The path where the generated RST files will be stored
+  output_path = os.path.join(REPO_ROOT, "docs", "reference", "api_generated")
+
+  # sphinx-apidoc options
+  # --module-first: Put module documentation before submodule list
+  # --force: Overwrite existing files
+  # --no-toc: Don't create a table of contents file (modules.rst)
+  # --separate: Create a separate file for each module
+  options = [
+      "--module-first",
+      "--force",
+      "--separate",
+      "--output-dir",
+      output_path,
+      pkg_path,
+      os.path.join("*", "tests", "*"),
+      os.path.join("*", "MaxText", "experimental", "*"),
+  ]
+  apidoc.main(options)
+
+
+# Connect the apidoc generation to the Sphinx build process
+def setup(app):
+  app.connect("builder-inited", run_apidoc)
