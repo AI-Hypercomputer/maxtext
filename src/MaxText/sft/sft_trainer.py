08830db585ad9ea5b8ceff85b5b3fa53ae7513b4
hengtaoguo <hengtaoguo@google.com>
2025-09-23 21:51:06 +0000

    decode using vllm

--- src/MaxText/sft/sft_trainer.py
+++ src/MaxText/sft/sft_trainer.py
@@ -162,6 +162,8 @@ def train(mt_config, goodput_recorder=None):
   with mesh, nn_partitioning.axis_rules(mt_config.logical_axis_rules):
     trainer.train(data_hooks.train_data_iterator, data_hooks.eval_data_iterator)
 
+  return trainer, mesh
+
 
 def main(argv: Sequence[str]) -> None:
   """Main function to run SFT training.
