c1140be2014dbb4aaca0f3ab427b521afae10ccf
Qinwen Xu <qinwen@google.com>
2025-09-29 22:18:53 +0000

    update

--- src/MaxText/layers/moe.py
+++ src/MaxText/layers/moe.py
@@ -1032,24 +1032,24 @@ class RoutedMoE(nnx.Module):
           group_sizes=group_sizes,
           expert_assignments=selected_experts,
       )
-      w0_tile_size = (
+      wi_tile_size = (
           self.config.tile_batch_seq,
           self.config.tile_activation_dim,
           self.config.tile_weight_dim,
       )
-      w_out_tile_size = (
+      wo_out_tile_size = (
           self.config.tile_batch_seq,
           self.config.tile_weight_dim,
           self.config.tile_activation_dim,
       )
-      layer_w0 = gmm_fn(x, w0,tiling=w0_tile_size)
+      layer_w0 = gmm_fn(x, w0,tiling=wi_tile_size)
       if self.get_tensor_transpose_parallelism_size() > 1:
         layer_w0 = jax.lax.psum(layer_w0, "tensor_transpose")
       if self.config.mlp_bias:
         layer_w0 = layer_w0 + w0_bias
       layer_w0 = adc.checkpoint_name(layer_w0, "mlpwi_0")
 
-      layer_w1 = gmm_fn(x, w1, tiling=w0_tile_size)
+      layer_w1 = gmm_fn(x, w1, tiling=wi_tile_size)
       if self.get_tensor_transpose_parallelism_size() > 1:
         layer_w1 = jax.lax.psum(layer_w1, "tensor_transpose")
       if self.config.mlp_bias:
@@ -1057,7 +1057,7 @@ class RoutedMoE(nnx.Module):
       layer_w1 = adc.checkpoint_name(layer_w1, "mlpwi_1")
       intermediate_layer = self.apply_ffn_activation(layer_w0, layer_w1)
 
-      intermediate_output = gmm_fn(intermediate_layer, wo, tiling=w_out_tile_size)
+      intermediate_output = gmm_fn(intermediate_layer, wo, tiling=wo_out_tile_size)
       if self.get_tensor_parallelism_size() > 1:
         intermediate_output = jax.lax.psum_scatter(intermediate_output, "tensor", scatter_dimension=1, tiled=True)
       if self.config.mlp_bias:
