20641fb9d7953cf8267f52913f76752ea0de2862
Mohit Khatwani <mohitkhatwani@google.com>
2025-10-14 00:56:02 +0000

    new resharding API

--- src/MaxText/experimental/rl/grpo_utils.py
+++ src/MaxText/experimental/rl/grpo_utils.py
@@ -14,13 +14,29 @@
 
 """Utility functions for GRPO (Generative Rejection-based Policy Optimization)."""
 
+import math
+import time
+import numpy as np
 import jax
 import jax.numpy as jnp
-import numpy as np
+import jaxtyping
+from typing import Any, Callable
+
 from MaxText import max_logging
 from MaxText import max_utils
 from MaxText.common_types import DecoderBlockType
 from MaxText.inference.offline_engine import InputData
+
+from pathwaysutils.experimental import reshard as experimental_reshard
+from pathwaysutils.experimental import split_by_mesh_axis
+
+
+def _identity(x):
+  return x
+
+
+INTERMEDIATE_SPLIT_SUFFIX = "_intermediate_split"
+INTERMEDIATE_REPLICA_SUFFIX = "_intermediate_replica"
 
 
 def compute_log_probs(
@@ -127,7 +143,6 @@ def generate_offline_completions(config, tokenizer_model, inference_engine, data
     )
 
   results = inference_engine.batch_inference(input_data)
-
   prompt_completions_segmentation = []
   completion_segmentation = []
   prompt_completions = []
@@ -189,9 +204,12 @@ def pathways_reshard(config, inference_engine, params, source_shardings, source_
         params, source_shardings, source_mesh, scan_axis=config.param_scan_axis, layer_groups=layer_groups
     )
 
-  inference_engine.update_params(
-      params, jax.tree_util.tree_map(lambda x: x.spec, destination_shardings.params), is_pw_reshard=True
-  )
+  with (
+      jax.transfer_guard_device_to_host("disallow_explicit"),
+      jax.transfer_guard_host_to_device("disallow_explicit"),
+  ):
+    resharded_params = reshard_pytree(params, destination_shardings)
+  inference_engine.update_params(resharded_params)
 
   if not config.scan_layers:
     max_utils.rescan_train_state_params(
@@ -349,3 +367,243 @@ def filter_and_split(config, example_batch, num_groups, global_batch_size_per_gr
     list_of_output_batches.append(current_group_dict)
 
   return list_of_output_batches
+
+
+def _maybe_find_intermediate_sharding(source_sharding, target_sharding):
+  """Maybe finds an intermediate sharding to reshard to before target sharding."""
+  if not isinstance(source_sharding, jax.sharding.NamedSharding) or not isinstance(
+      target_sharding, jax.sharding.NamedSharding
+  ):
+    max_logging.log(
+        "None-NamedSharding does not need intermediate sharding." f" {source_sharding=}, {target_sharding=}",
+    )
+    return None
+  src_mesh = source_sharding.mesh
+  dst_mesh = target_sharding.mesh
+
+  def _get_sharding_dims(sharding, mesh):
+    sharding_dims = {}
+    used_mesh_axis_names = set()
+    for i, axis_name in enumerate(sharding.spec):
+      if axis_name is None:
+        sharding_dims[(i, None)] = 1
+      else:
+        if isinstance(axis_name, tuple):
+          used_mesh_axis_names |= set(axis_name)
+          shard_size = math.prod([mesh.shape[name] for name in axis_name])
+          first_axis_name = axis_name[0]
+          sharding_dims[(i, mesh.axis_names.index(first_axis_name))] = shard_size
+        else:
+          assert isinstance(axis_name, str), "axis_name expected to be a string or a tuple of strings."
+          used_mesh_axis_names.add(axis_name)
+          sharding_dims[(i, mesh.axis_names.index(axis_name))] = mesh.shape[axis_name]
+    largest_shards = max(sharding_dims.values()) if len(sharding_dims) else 1
+    if len(sharding_dims) < len(mesh.shape):
+      for mi, mesh_axis in enumerate(mesh.axis_names):
+        if mesh_axis not in used_mesh_axis_names:
+          sharding_dims[(None, mi)] = 1
+    return sharding_dims, largest_shards
+
+  src_sharding_dims, src_largest_shards = _get_sharding_dims(source_sharding, src_mesh)
+  dst_sharding_dims, dst_largest_shards = _get_sharding_dims(target_sharding, dst_mesh)
+  # Not able to handle resharding with undividable shardings.
+  if src_largest_shards % dst_largest_shards != 0:
+    return None
+
+  total_source_sharding_dims = math.prod(list(src_sharding_dims.values()))
+  total_dst_sharding_dims = math.prod(list(dst_sharding_dims.values()))
+
+  if total_source_sharding_dims <= total_dst_sharding_dims or total_source_sharding_dims % total_dst_sharding_dims != 0:
+    return None
+
+  new_split_dim_shards = None
+  new_split_axis = None
+  replicas = src_largest_shards // dst_largest_shards
+
+  # Find gcd(src_dim_shards, dst_dim_shards),
+  # If all of them are 1s, an all-gather is needed as the single replica of
+  # the source cannot be presented by any sharded form on the target devices.
+  gcd_shards = []
+  for (sharding_mesh_axis_idx, src_dim_shards), (_, dst_dim_shards) in zip(
+      src_sharding_dims.items(), dst_sharding_dims.items()
+  ):
+    gcd_dim_shards = math.gcd(src_dim_shards, dst_dim_shards)
+    if gcd_dim_shards == 1:
+      if src_dim_shards > dst_dim_shards and src_dim_shards == src_largest_shards:
+        new_split_axis = sharding_mesh_axis_idx
+        new_split_dim_shards = (src_dim_shards // replicas, replicas)
+    gcd_shards.append(gcd_dim_shards)
+  if math.prod(gcd_shards) != 1 or new_split_axis is None:
+    return None
+
+  # Generate the intermediate sharding.
+  new_split_mesh_axis_name = src_mesh.axis_names[new_split_axis[1]] + INTERMEDIATE_SPLIT_SUFFIX
+  new_split_mesh_replica_axis_name = src_mesh.axis_names[new_split_axis[1]] + INTERMEDIATE_REPLICA_SUFFIX
+  intermediate_mesh = jax.sharding.Mesh(
+      src_mesh.devices.reshape(
+          tuple(
+              list(src_mesh.devices.shape[: new_split_axis[1]])
+              + [new_split_dim_shards[0], new_split_dim_shards[1]]
+              + list(src_mesh.devices.shape[new_split_axis[1] + 1 :])
+          )
+      ),
+      axis_names=tuple(
+          list(src_mesh.axis_names[: new_split_axis[1]])
+          + [new_split_mesh_axis_name, new_split_mesh_replica_axis_name]
+          + list(src_mesh.axis_names[new_split_axis[1] + 1 :])
+      ),
+  )
+
+  intermediate_spec = tuple(
+      list(source_sharding.spec[: new_split_axis[0]])
+      + [new_split_mesh_axis_name]
+      + list(source_sharding.spec[new_split_axis[0] + 1 :])
+  )
+  intermediate_sharding = jax.sharding.NamedSharding(
+      intermediate_mesh,
+      jax.sharding.PartitionSpec(*intermediate_spec),
+      memory_kind=source_sharding.memory_kind,
+  )
+
+  return intermediate_sharding
+
+
+def _experimental_pre_reshard(splitfn, src_pytree, target_shardings):
+  """Simple heuristic to determine if resharding with replicated all-gather is needed."""
+  src_shardings = jax.tree_util.tree_map(
+      lambda x: x.sharding,
+      src_pytree,
+  )
+  intermediate_shardings = jax.tree_util.tree_map(
+      _maybe_find_intermediate_sharding,
+      src_shardings,
+      target_shardings,
+  )
+
+  src_leaves_with_path, src_treedef = jax.tree_util.tree_flatten_with_path(src_pytree)
+  intermediate_sharding_leaves_with_path, _ = jax.tree_util.tree_flatten_with_path(intermediate_shardings)
+  intermediate_sharding_leaves_with_path = dict(intermediate_sharding_leaves_with_path)
+
+  to_split_src_pytree_leaves = []
+  to_split_src_pytree_leaves_indexes = []
+  to_split_intermediate_sharding_leaves = []
+
+  intermediate_mesh = None
+  to_update_src_pytree_leaves = []
+
+  for i, (path, src) in enumerate(src_leaves_with_path):
+    to_update_src_pytree_leaves.append(src)
+    if intermediate_sharding := intermediate_sharding_leaves_with_path.get(path, None):
+      if intermediate_mesh is None:
+        intermediate_mesh = intermediate_sharding.mesh
+      to_split_src_pytree_leaves.append(src)
+      to_split_src_pytree_leaves_indexes.append(i)
+      to_split_intermediate_sharding_leaves.append(intermediate_sharding)
+
+  if intermediate_mesh is None:
+    return src_pytree
+
+  to_split_axis = None
+  for axis_name in intermediate_mesh.axis_names:
+    if axis_name.endswith(INTERMEDIATE_REPLICA_SUFFIX):
+      to_split_axis = axis_name
+      break
+  assert to_split_axis is not None, f"No replica axis found in the intermediate mesh {intermediate_mesh}."
+
+  temp_source = jax.jit(
+      _identity,
+      out_shardings=to_split_intermediate_sharding_leaves,
+  )(to_split_src_pytree_leaves)
+
+  to_split_src_pytree_leaves, *_ = splitfn(temp_source, to_split_axis)
+
+  for i, src_pytree_leaf in enumerate(to_split_src_pytree_leaves):
+    to_update_src_pytree_leaves[to_split_src_pytree_leaves_indexes[i]] = src_pytree_leaf
+  updated_src_pytree = jax.tree_util.tree_unflatten(src_treedef, to_update_src_pytree_leaves)
+  return updated_src_pytree
+
+
+def _get_reshard_fn_pathwaysutils(
+    *,
+    cache_resharding_plans: bool,
+    donate: bool,
+    use_experimental_pre_reshard: bool,
+):
+  """Returns a reshard function using pathwaysutils."""
+
+  def reshard_fn(
+      x: Any,
+      sharding: jax.sharding.Sharding | Any,
+  ):
+    if use_experimental_pre_reshard:
+      x = _experimental_pre_reshard(split_by_mesh_axis.split_by_mesh_axis, x, sharding)
+
+    return experimental_reshard.reshard(
+        x,
+        sharding,
+        donate=donate,
+        cache_resharding_plans=cache_resharding_plans,
+    )
+
+  return reshard_fn
+
+
+def _get_reshard_fn(
+    cache_resharding_plans: bool,
+    donate: bool,
+    use_experimental_pre_reshard: bool,
+    get_reshard_fns: list[Callable[..., Any]],
+):
+  """Returns a reshard function."""
+  for get_reshard_fn in get_reshard_fns:
+    try:
+      reshard_fn = get_reshard_fn(
+          cache_resharding_plans=cache_resharding_plans,
+          donate=donate,
+          use_experimental_pre_reshard=use_experimental_pre_reshard,
+      )
+    except (ImportError, EnvironmentError):
+      max_logging.log(f"Could not support {get_reshard_fn=}.")
+    else:
+      return reshard_fn
+
+  raise ValueError(f"Could not find a reshard function from {get_reshard_fns=}.")
+
+
+def reshard_pytree(
+    source: jaxtyping.PyTree,
+    target: jaxtyping.PyTree,
+    cache_plan: bool = True,
+    donate_input: bool = False,
+    use_experimental_pre_reshard: bool = True,
+) -> jaxtyping.PyTree:
+  """Reshard input pytree from source sharding and mesh to target sharding and mesh."""
+
+  def _get_dst_sharding(x):
+    if isinstance(x, jax.sharding.NamedSharding | jax.sharding.SingleDeviceSharding):
+      return x
+    else:
+      return jax.sharding.NamedSharding(
+          x.sharding.mesh,
+          x.sharding.spec,
+          memory_kind=x.sharding.memory_kind,
+      )
+
+  dst_shardings = jax.tree_util.tree_map(
+      _get_dst_sharding,
+      target,
+  )
+  reshard_fn = _get_reshard_fn(
+      cache_resharding_plans=cache_plan,
+      donate=donate_input,
+      use_experimental_pre_reshard=use_experimental_pre_reshard,
+      get_reshard_fns=[
+          _get_reshard_fn_pathwaysutils,
+      ],
+  )
+
+  start = time.time()
+  resharded_array = reshard_fn(source, dst_shardings)
+  resharded_array = jax.block_until_ready(resharded_array)
+  max_logging.log(f"Reshard finished in {time.time() - start:.4f}s")
+  return resharded_array
