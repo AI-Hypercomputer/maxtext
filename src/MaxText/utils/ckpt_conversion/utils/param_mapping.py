5369207abfe2c8a0a51cea57741262cea5f33fbc
Shuning Jin <shuningjin@google.com>
2025-11-14 07:06:43 +0000

    .

--- src/MaxText/utils/ckpt_conversion/utils/param_mapping.py
+++ src/MaxText/utils/ckpt_conversion/utils/param_mapping.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 """Parameter mappings and transformation hooks for checkpoint conversion.
 
 This module defines the necessary components to convert model checkpoints between
