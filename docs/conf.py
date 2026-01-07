48445cf00e73064e87886539714a8ba1794ee019
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-01-07 09:49:20 -0500

    Copyright 2023–2026 Google LLC

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# Copyright 2023–2025 Google LLC
+# Copyright 2023–2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 project = "MaxText"
 # pylint: disable=redefined-builtin
-copyright = "2025, Google LLC"
+copyright = "2023–2026 Google LLC"
 author = "MaxText developers"
 
 # -- General configuration ---------------------------------------------------
