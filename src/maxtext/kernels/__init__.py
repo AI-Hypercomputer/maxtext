30fd86b238631e7d58d71441f4e748856397d029
Dipannita Shaw <dishaw@google.com>
2026-01-30 20:20:23 +0000

    Clean move

--- src/maxtext/kernels/__init__.py
+++ src/maxtext/kernels/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023–2025 Google LLC
+# Copyright 2023–2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Megablox kernel"""
-
-from MaxText.kernels.megablox.ops import gmm
