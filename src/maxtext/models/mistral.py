17732db50a1289cf8664a26d5d19fd97f7624354
Dipannita Shaw <dishaw@google.com>
2026-02-07 00:44:24 +0000

    Fix config import

--- src/maxtext/models/mistral.py
+++ src/maxtext/models/mistral.py
@@ -25,13 +25,12 @@ from flax import nnx
 
 from maxtext.layers import nnx_wrappers, initializers
 from maxtext.layers.linears import Dropout, MlpBlock
-from maxtext.models.models import Config
 from maxtext.layers.attentions import Attention
 from maxtext.layers import quantizations
 from maxtext.layers.quantizations import AqtQuantization as Quant
 from maxtext.layers.normalizations import RMSNorm
 from maxtext.utils import max_utils
-
+from MaxText.common_types import Config
 
 # -----------------------------------------
 # The Decoder Layer for Mistral
