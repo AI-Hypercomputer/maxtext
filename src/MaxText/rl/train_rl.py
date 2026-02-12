199c0711a8a0ee4c00baa7dc06bebf7e7291bc32
Nicolas Grande <nicogrande@google.com>
2026-02-12 22:18:22 +0000

    xremoving duplicate KV cache instance.
    changing config paths.

--- src/MaxText/rl/train_rl.py
+++ src/MaxText/rl/train_rl.py
@@ -74,6 +74,7 @@ from MaxText.globals import MAXTEXT_CONFIGS_DIR
 os.environ["SKIP_JAX_PRECOMPILE"] = "1"
 
 from MaxText import pyconfig
+from MaxText.globals import MAXTEXT_CONFIGS_DIR
 from MaxText.integration.tunix.tunix_adapter import TunixMaxTextAdapter
 from MaxText.rl.evaluate_rl import evaluate
 from MaxText.rl import utils_rl
@@ -500,8 +501,7 @@ def rl_train(trainer_config, sampler_config, trainer_devices, sampler_devices):
           "enable_tunix_perf_metrics is True but tunix.perf modules are not available, skipping Tunix-managed metrics."
       )
 
-  # configs_dir = os.environ.get("MAXTEXT_CONFIGS_DIR", os.path.join(MAXTEXT_PKG_DIR, "configs"))
-  vllm_config_path = os.path.join(MAXTEXT_CONFIGS_DIR, "vllm.yml")
+  vllm_config_path = os.path.join(MAXTEXT_CONFIGS_DIR, "inference", "vllm.yml")
   argv_list = ["", str(vllm_config_path), "log_config=False"]
   vllm_config = pyconfig.initialize(argv_list)
 
