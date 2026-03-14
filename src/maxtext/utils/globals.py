5afca54953d589d472135816b57f52cd62274ca9
A9isha <mazumdera@google.com>
2026-03-14 06:59:27 +0000

    Add option to start test_batch in train_rl from a specific index, also add default tokenizer_path for default model

--- src/maxtext/utils/globals.py
+++ src/maxtext/utils/globals.py
@@ -72,6 +72,8 @@ HF_IDS = {
     "olmo3-7b": "allenai/Olmo-3-7B-Instruct",
     "olmo3-7b-pt": "allenai/Olmo-3-1025-7B",
     "olmo3-32b": "allenai/Olmo-3-32B-Think",
+    # "default" is not HF model, but adding to to avoid confusing warning about tokenizer_path
+    "default": os.path.join(MAXTEXT_ASSETS_ROOT, "tokenizers/tokenizer.llama2"),
 }
 
 __all__ = [
