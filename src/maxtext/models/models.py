c17d5b1645b649e96b082cd9f358c64cb85189de
hsuan-lun-chiang <hsuan-lun.chiang@cienet.com>
2026-03-15 19:44:01 -0700

    PR #2831: Migrate Decoder to NNX
    Imported from GitHub PR https://github.com/AI-Hypercomputer/maxtext/pull/2831
    
    Migrate the Transformer decoder layer into NNX.
    
    Note: The following models are currently not supported:
    
    - DeepSeek
    - Gemma3
    - Llama4
    
    Support for these models will be added in a follow-up PR.
    
    Strategy:
    A `pure_nnx_decoder` flag is added to control whether NNX or Linen decoder shall be used.
    Initial migration doesn't include the pipeline NNX support.
    
    Conducted these tests. Details in the [GDoc file](https://docs.google.com/document/d/1NbUP3g5glgbC6bMyt44pwM_vQA1NR7U2rBUzfbTDwSs/edit?pli=1&resourcekey=0-9EUahtzL-hCycdu7l0grhQ&tab=t.htq5367h8au0)
    1. Test with different model and compare with Linen training
    2. Golden logits comparison
    3. Inference
    4. Checkpoint comparison (Including TreeStructure Comparison)
    5. Sharding comparison
    
    TODOs:
    - NNX version of unit tests (future PRs)
    
    Before submitting this PR, please make sure (put X in square brackets):
    - [x] I have performed a self-review of my code. For an optional AI review, add the `gemini-review` label.
    - [x] I have necessary comments in my code, particularly in hard-to-understand areas.
    - [x] I have run end-to-end tests tests and provided workload links above if applicable.
    - [x] I have made or will make corresponding changes to the doc if needed, including adding new documentation pages to the relevant Table of Contents (toctree directive) as explained in [our documentation](https://maxtext.readthedocs.io/en/latest/development.html#adding-new-documentation-files).
    
    Copybara import of the project:
    
    --
    073e916a5415ef58db91b4c6aea0423d238b09a7 by hsuan-lun-chiang <hsuan-lun.chiang@cienet.com>:
    
    Migrate Decoder to NNX
    
        Adding nnx_decoders.py in parallel with decoders.py
    
        1. Dup and modifiy decoders.py on new file nnx_decoders.py
        2. add new config pure_nnx_decoder to control if model will use NNXDecoder, default false for now
        3. modify relative code to accomodate the change
        4. add/modify unit test
    
    Merging this change closes #2831
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/AI-Hypercomputer/maxtext/pull/2831 from CIeNET-International:feat/Migrate-Decoder-to-NNX 073e916a5415ef58db91b4c6aea0423d238b09a7
    PiperOrigin-RevId: 884170982

--- src/maxtext/models/models.py
+++ src/maxtext/models/models.py
@@ -335,8 +335,8 @@ class Transformer(nnx.Module):
     if cfg.pure_nnx_decoder:
       self.decoder = NNXDecoder(config=cfg, mesh=mesh, quant=self.quant, model_mode=self.model_mode, rngs=rngs)
     else:
-      decoder_linen = Decoder(config=cfg, mesh=mesh, quant=self.quant, model_mode=self.model_mode)
-      self.decoder = nnx_wrappers.ToNNX(decoder_linen, rngs=rngs)
+      self.decoder = Decoder(config=cfg, mesh=mesh, quant=self.quant, model_mode=self.model_mode)
+      self.decoder = nnx_wrappers.ToNNX(self.decoder, rngs=rngs)
     self.hidden_states = None
 
     batch_size, seq_len = max_utils.get_batch_seq_len_for_mode(config=cfg, model_mode=model_mode)
