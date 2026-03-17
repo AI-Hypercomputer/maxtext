2fb977d4d2369cf5fceabe5b929245690ca5cbba
Niyati Chaudhary <niyatic@google.com>
2026-03-17 15:39:36 -0700

    Use Tokamax's representative group sizes.
    PiperOrigin-RevId: 885246948

--- src/maxtext/layers/moe.py
+++ src/maxtext/layers/moe.py
@@ -900,10 +900,7 @@ class RoutedMoE(nnx.Module):
       if self.config.using_pipeline_parallelism and self.config.pipeline_fsdp_ag_per_repeat:
         tokamax_group_sizes = group_sizes
       else:
-        tokamax_group_sizes = tokamax.RaggedDotGroupSizes(
-            group_sizes,
-            max_utils.generate_representative_group_sizes(inputs.shape[0], kernel.shape[0]),
-        )
+        tokamax_group_sizes = tokamax.RaggedDotGroupSizes(group_sizes, self.config.wi_tile_fwd_batch_seq)
       pad_length = self.config.wi_tile_fwd_batch_seq
       hs_shape = inputs.shape
       # pad length is the 1st dimension of tiling size in gmm call
