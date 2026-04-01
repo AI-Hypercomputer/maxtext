725015005eb6e17c2b04a4cdaed5e221bfa649a2
vlad-karp <vladkarp@google.com>
2026-04-01 23:33:14 +0000

    logging fix

--- src/maxtext/trainers/post_train/distillation/train_distill.py
+++ src/maxtext/trainers/post_train/distillation/train_distill.py
@@ -228,7 +228,7 @@ class MaxTextDistillationTrainer(peft_trainer.PeftTrainer):
         if not isinstance(x, base_wrt):
           return False
         freeze = student_freeze_param_filter(path)
-        logging.info("Student model freezing info: Parameter {path}; freeze={freeze}")
+        logging.info(f"Student model freezing info: Parameter {path}; freeze={freeze}")
         return not freeze
 
       self.wrt_filter = wrt_filter
