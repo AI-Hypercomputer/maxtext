301d327dc16070c1f7bd048f0eb1072dce870dd1
github-action-benchmark <github@users.noreply.github.com>
2026-07-08 03:37:03 +0000

    add MaxText Test Execution Times (customSmallerIsBetter) benchmark result for ea7af7995239ea56032449905d1ed3dfa75e032d

---
+++ dev/bench/data.js
@@ -0,0 +1,58 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783481823042,
+  "repoUrl": "https://github.com/AI-Hypercomputer/maxtext",
+  "entries": {
+    "MaxText Test Execution Times": [
+      {
+        "commit": {
+          "author": {
+            "name": "AI-Hypercomputer",
+            "username": "AI-Hypercomputer"
+          },
+          "committer": {
+            "name": "AI-Hypercomputer",
+            "username": "AI-Hypercomputer"
+          },
+          "id": "ea7af7995239ea56032449905d1ed3dfa75e032d",
+          "message": "ci: add test duration tracking workflow",
+          "timestamp": "2026-07-07T23:47:44Z",
+          "url": "https://github.com/AI-Hypercomputer/maxtext/pull/4290/commits/ea7af7995239ea56032449905d1ed3dfa75e032d"
+        },
+        "date": 1783481822487,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total TPU Tests Duration",
+            "value": 1394.216,
+            "unit": "sec"
+          },
+          {
+            "name": "Total TPU Tests Count",
+            "value": 2143,
+            "unit": "count"
+          },
+          {
+            "name": "Total CPU Tests Duration",
+            "value": 1439.5949999999998,
+            "unit": "sec"
+          },
+          {
+            "name": "Total CPU Tests Count",
+            "value": 230,
+            "unit": "count"
+          },
+          {
+            "name": "Total CUDA12 Tests Duration",
+            "value": 873.224,
+            "unit": "sec"
+          },
+          {
+            "name": "Total CUDA12 Tests Count",
+            "value": 2021,
+            "unit": "count"
+          }
+        ]
+      }
+    ]
+  }
+}
