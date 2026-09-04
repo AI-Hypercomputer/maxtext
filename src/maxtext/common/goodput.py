a24348eb9552a14b1446e46c218617526b59530a
Yin <liyinn@google.com>
2026-09-04 23:00:13 +0000

    Use sliding window to track goodput metrics over time. This will allow for more accurate measurement of performance and help identify trends in data throughput.

--- src/maxtext/common/goodput.py
+++ src/maxtext/common/goodput.py
@@ -109,12 +109,28 @@ def maybe_monitor_goodput(config):
 
     goodput_monitor = _construct_goodput_monitor(config, common_kwargs)
     goodput_monitor.start_goodput_uploader()
-    max_logging.log("Started Goodput upload to Tensorboard & GCM in the background!")
-    yield
+    max_logging.log("Started Cumulative Goodput upload to Tensorboard & GCM in the background!")
+    with maybe_monitor_rolling_window_goodput(goodput_monitor, config):
+      yield
   finally:
     if goodput_monitor:
       goodput_monitor.stop_goodput_uploader()
-      max_logging.log("Flushed final metrics and safe exited from Goodput monitoring.")
+      max_logging.log("Flushed final metrics and safe exited from cumulative Goodput monitoring.")
+
+
+@contextlib.contextmanager
+def maybe_monitor_rolling_window_goodput(goodput_monitor, config):
+  """Monitor rolling window goodput if enabled on the lead host."""
+  if _GOODPUT_STUB or not goodput_monitor:
+    yield
+    return
+  try:
+    goodput_monitor.start_rolling_window_goodput_uploader(config.rolling_windows_seconds)
+    max_logging.log("Started Rolling Window Goodput upload to Tensorboard & GCM in the background!")
+    yield
+  finally:
+    goodput_monitor.stop_rolling_window_goodput_uploader()
+    max_logging.log("Flushed final metrics and safe exited from rolling window Goodput monitoring.")
 
 
 @contextlib.contextmanager
