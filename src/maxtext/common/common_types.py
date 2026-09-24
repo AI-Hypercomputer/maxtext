28d3a1e44e8b368e37f2a90f16695dc319641559
gobbleturk <mattdavidow@google.com>
2026-09-24 20:30:16 +0000

    Rename rule to fsdp-transpose-and-ep-as-dp; add fsdp-transpose-as-dp where EP shards attn / shared expert weights

--- src/maxtext/common/common_types.py
+++ src/maxtext/common/common_types.py
@@ -198,4 +198,5 @@ class CustomRule(enum.Enum):
   EP_AS_DP = "ep-as-dp"
   SHARD_EXP_ON_FSDP = "shard-exp-on-fsdp"
   FSDP_AS_DP_FOR_ATTN = "fsdp-as-dp-for-attn"
+  FSDP_TRANSPOSE_AND_EP_AS_DP = "fsdp-transpose-and-ep-as-dp"
   FSDP_TRANSPOSE_AS_DP = "fsdp-transpose-as-dp"
